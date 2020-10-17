9e77aa9d252e11ec543d64668da1619c1942f430
danielpmc <danielpd93@gmail.com>
2020-10-17 15:20:01 +0100

    Update stats command

--- Panel/bot/discord/commands/stats.js
+++ Panel/bot/discord/commands/stats.js
@@ -1,30 +1,18 @@
-exports.run = async (client, message) => {
-
+exports.run = async(client, message) => {
+    
   //Sends message while the json's are being cached
   let embed = new Discord.RichEmbed()
-    .setColor(`RANDOM`)
-    .addField(`__**Please wait...**__`, `Loading all node's stats! (If this takes longer than 5seconds. This command is broken)`, true);
+  .setColor(`RANDOM`)
+  .addField(`__**Please wait...**__`, `Loading all node's stats! (If this takes longer than 5seconds. This command is broken)`, true);
   let msg = await message.channel.send(embed)
 
-  //Data for node 1
-  var N1 = fs.readFileSync('./data/Node1.json', 'utf8');
-  var Node1Data = JSON.parse(N1);
-
-  //Data for node 2
-  var N2 = fs.readFileSync('./data/Node2.json', 'utf8');
-  var Node2Data = JSON.parse(N2);
-
-  //Data for node 3
-  var N3 = fs.readFileSync('./data/vmi450443.contaboserver.net.json', 'utf8');
-  var Node3Data = JSON.parse(N3);
-
   //Edits the message to display the data
-  embed.fields.pop()
-  embed.addField("\u200b", "__**[Node 1 - Discord Bots](https://danbot.host/Node1)**__ \n**CPU LOAD**: " + Node1Data.cpuload + "% \n**RAM (USED/TOTAL)**: " + Node1Data.memused + " / " + Node1Data.memtotal + " \n**STORAGE (USED/TOTAL)**: " + Node1Data.diskused + " / " + Node1Data.disktotal + " \n**UPTIME**: " + Node1Data.osuptime)
-  embed.addField('\u200b', '\u200b')
-  embed.addField("\u200b", "__**[Node 2 - Discord Bots](https://danbot.host/Node2)**__ \n**CPU LOAD**: " + Node2Data.cpuload + "% \n**RAM (USED/TOTAL)**: " + Node2Data.memused + " / " + Node2Data.memtotal + " \n**STORAGE (USED/TOTAL)**: " + Node2Data.diskused + " / " + Node2Data.disktotal + " \n**UPTIME**: " + Node2Data.osuptime)
-  embed.addField('\u200b', '\u200b')
-  embed.addField("\u200b", "__**[Node 3 - Gaming](https://danbot.host/Node3)**__ \n**CPU LOAD**: " + Node3Data.cpuload + "% \n**RAM (USED/TOTAL)**: " + Node3Data.memused + " / " + Node3Data.memtotal + " \n**STORAGE (USED/TOTAL)**: " + Node3Data.diskused + " / " + Node3Data.disktotal + " \n**UPTIME**: " + Node3Data.osuptime)
-  embed.setDescription('Want to view more stats live? [Click Here!](https://danbot.host/stats)')
-  msg.edit(embed);
+      embed.fields.pop()
+          embed.addField("\u200b", "__**[Node 1 - Discord Bots](https://danbot.host/Node1)**__ \n**CPU LOAD**: " + nodeData.fetch("Node1.cpuload") + "% \n**RAM (USED/TOTAL)**: " + nodeData.fetch("Node1.memused") + " / " + nodeData.fetch("Node1.memtotal") + " \n**STORAGE (USED/TOTAL)**: " + nodeData.fetch("Node1.diskused") + " / " + nodeData.fetch("Node1.disktotal") + " \n**UPTIME**: " + nodeData.fetch("Node1.osuptime") + "\n**Servers:** **Total**: " + nodeData.fetch("Node1.dockercontainers") + ", **Running**: " + nodeData.fetch("Node1.dockercontainersrunning") + ", **Stopped**: " + nodeData.fetch("Node1.dockercontainersstopped"))
+          embed.addField('\u200b', '\u200b')
+          embed.addField("\u200b", "__**[Node 2 - Discord Bots](https://danbot.host/Node2)**__ \n**CPU LOAD**: " + nodeData.fetch("Node2.cpuload") + "% \n**RAM (USED/TOTAL)**: " + nodeData.fetch("Node2.memused") + " / " + nodeData.fetch("Node2.memtotal") + " \n**STORAGE (USED/TOTAL)**: " + nodeData.fetch("Node2.diskused") + " / " + nodeData.fetch("Node2.disktotal") + " \n**UPTIME**: " + nodeData.fetch("Node2.osuptime") + "\n**Servers**: **Total**: " + nodeData.fetch("Node2.dockercontainers") + ", **Running**: " + nodeData.fetch("Node2.dockercontainersrunning") + ", **Stopped**: " + nodeData.fetch("Node2.dockercontainersstopped"))
+          embed.addField('\u200b', '\u200b')
+          embed.addField("\u200b", "__**[Node 3 - Gaming](https://danbot.host/Node3)**__ \n**CPU LOAD**: " + nodeData.fetch("Node3.cpuload") + "% \n**RAM (USED/TOTAL)**: " + nodeData.fetch("Node3.memused") + " / " + nodeData.fetch("Node3.memtotal") + " \n**STORAGE (USED/TOTAL)**: " + nodeData.fetch("Node3.diskused") + " / " + nodeData.fetch("Node3.disktotal") + " \n**UPTIME**: " + nodeData.fetch("Node3.osuptime") + "\n**Servers**: **Total**: " + nodeData.fetch("Node3.dockercontainers") + ", **Running**: " + nodeData.fetch("Node3.dockercontainersrunning") + ", **Stopped**: " + nodeData.fetch("Node3.dockercontainersstopped"))
+          embed.setDescription('Want to view more stats live? [Click Here!](https://danbot.host/stats)')
+      msg.edit(embed);
 };
