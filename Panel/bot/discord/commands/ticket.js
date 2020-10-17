76f0c94f24079e60a3927bc3d1b6fa08768a7175
danielpmc <danielpd93@gmail.com>
2020-10-17 06:05:45 +0100

    remove the 2nd @

--- Panel/bot/discord/commands/ticket.js
+++ Panel/bot/discord/commands/ticket.js
@@ -48,7 +48,7 @@ exports.run = async (client, message) => {
         })
 
         if (userData.get(message.author.id) == null) {
-            channel.send('@<@' + message.author.id + '> here is your ticket! Please give as much info as possible about your problem. \n\n *This account is not linked with a console account*')
+            channel.send('<@' + message.author.id + '> here is your ticket! Please give as much info as possible about your problem. \n\n *This account is not linked with a console account*')
         } else {
             let embed = new Discord.RichEmbed()
                 .setColor(`GREEN`)
