884379736bcd546135b78fb922556b20d9c7f7f9
danielpmc <danielpd93@gmail.com>
2020-10-19 04:17:26 +0100

    Change GB to MB

--- Panel/bot/discord/commands/server.js
+++ Panel/bot/discord/commands/server.js
@@ -513,7 +513,7 @@ exports.run = async (client, message, args) => {
                             .addField('**DISK Usage**', pretty(resources.data.attributes.resources.disk_bytes) + '  out of UNLIMITED MB')
                             .addField('**NET Usage**', 'UPLOADED: ' + pretty(resources.data.attributes.resources.network_tx_bytes) + ', DOWNLOADED: ' + pretty(resources.data.attributes.resources.network_rx_bytes))
                             .addField('\u200b', '\u200b')
-                            .addField('**LIMITS (0 = unlimited)**', 'MEMORY: ' + response.data.attributes.limits.memory + 'MB \nDISK: ' + response.data.attributes.limits.disk + 'GB \nCPU: ' + response.data.attributes.limits.cpu)
+                            .addField('**LIMITS (0 = unlimited)**', 'MEMORY: ' + response.data.attributes.limits.memory + 'MB \nDISK: ' + response.data.attributes.limits.disk + 'MB \nCPU: ' + response.data.attributes.limits.cpu)
                             .addField('**MISC LIMITS**', 'DATABASES: ' + response.data.attributes.feature_limits.databases + '\nBACKUPS: ' + response.data.attributes.feature_limits.backups)
                         message.reply(embedstatus)
                 }).catch(
