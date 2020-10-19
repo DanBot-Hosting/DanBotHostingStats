97c84012d5ed59eef2355a1613772c09db77b3b9
danielpmc <danielpd93@gmail.com>
2020-10-19 03:17:50 +0100

    Update server.js

--- Panel/bot/discord/commands/server.js
+++ Panel/bot/discord/commands/server.js
@@ -488,7 +488,7 @@ exports.run = async (client, message, args) => {
                 }
             }).then(response => {
                 //response.data.attributes - identifier - node - limits - feature_limits (LIMITS: limits: { memory: 0, swap: -1, disk: 5000, io: 500, cpu: 0 }) (Feature_Limits:  feature_limits: { databases: 0, allocations: 0, backups: 0 })
-                console.log(response.data.attributes.variables.data)
+                console.log(response.data.attributes.variables)
             });
         }
     };
