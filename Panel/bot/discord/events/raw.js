a51e5b836da83f749fc69aaff2566c1defc5bada
Hadimhz <Hadimhz@users.noreply.github.com>
2020-10-20 02:42:53 +0300

    Update raw.js

--- Panel/bot/discord/events/raw.js
+++ Panel/bot/discord/events/raw.js
@@ -9,29 +9,33 @@ module.exports = async (client, e) => {
         let member = message.guild.members.get(e.d.user_id);
 
         if (member.user.bot || client.reactionRoles[channel.id] == null || client.reactionRoles[channel.id][message.id] == null) return;
+        try {
+            let reactionRoles = client.reactionRoles[channel.id][message.id];
 
-        let reactionRoles = client.reactionRoles[channel.id][message.id];
+            let input = emoji.id != null ? emoji.id : emoji.name;
 
-        let input = emoji.id != null ? emoji.id : emoji.name;
+            if (reactionRoles[input] != null) {
+                if (member.roles.find(x => x.id == reactionRoles[input])) {
+                    await member.removeRole(reactionRoles[input]).catch(client.channels.get('757029522682937354').send("a"));
+                    member.user.send("removed the role: `" + role.name + "`!");
+                } else {
+                    await member.addRole(reactionRoles[input]).catch(client.channels.get('757029522682937354').send("a"));
+                    member.user.send("gave you the role: `" + role.name + "`!");
+                };
 
-        if (reactionRoles[input] != null) {
-            if (member.roles.find(x => x.id == reactionRoles[input])) {
-                await member.removeRole(reactionRoles[input]).catch(console.log(`couldn't find a role with the id ${reactionRoles[input]}`));
-                member.user.send("removed the role: `" + role.name + "`!");
-            } else {
-                await member.addRole(reactionRoles[input]).catch(console.log(`couldn't find a role with the id ${reactionRoles[input]}`));
-                member.user.send("gave you the role: `" + role.name + "`!");
-            };
+                let memberRoles = member.roles.map(x => x.id).concat(Object.values(reactionRoles));
+                if (findDuplicates(memberRoles)) member.addRole('765869330024890378')
+                else member.removeRole('765869330024890378');
+            }
 
-            let memberRoles = member.roles.map(x => x.id).concat(Object.values(reactionRoles));
-            if (findDuplicates(memberRoles)) member.addRole('765869330024890378')
-            else member.removeRole('765869330024890378');
+            message.reactions.forEach(reaction => {
+                if (e.d.user_id == client.user.id) return;
+                else reaction.remove(e.d.user_id)
+            })
+        } catch (error) {
+            client.channels.get('757029522682937354').send(error.name)
         }
-
-        message.reactions.forEach(reaction => {
-            if (e.d.user_id == client.user.id) return;
-            else reaction.remove(e.d.user_id)
-        })
     }
+
 
 }
