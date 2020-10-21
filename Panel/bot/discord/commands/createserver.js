12ee5d3cdbae2083ba04dae0a3e23826e96320fd
danielpmc <danielpd93@gmail.com>
2020-10-21 20:29:55 +0100

    changed a little thing

--- Panel/bot/discord/commands/createserver.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-const {
-    response
-} = require('express');
-exports.run = async (client, message, args) => {
-    message.channel.send('Command has been renamed to `' + config.DiscordBot.Prefix + "server create`")
-
-};
