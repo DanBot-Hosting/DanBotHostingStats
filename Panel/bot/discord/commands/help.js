b805136923ce996f1ea3964cf0eba704aa9194ec
GamerCreator1 <58714825+GamerCreator1@users.noreply.github.com>
2020-10-21 20:44:08 -0400

    Updated Help To Latest
    New command.

--- Panel/bot/discord/commands/help.js
+++ Panel/bot/discord/commands/help.js
@@ -1,7 +1,7 @@
 const Help = {
-    "Users": `${config.DiscordBot.Prefix}user | See help for that command \n${config.DiscordBot.Prefix}server | See help for that command \n${config.DiscordBot.Prefix}stats | Shows the stats of each hosting node. \n${config.DiscordBot.Prefix}ticket | Create a ticket for help from the staff team! \n${config.DiscordBot.Prefix}status | Check the status of your server! \n${config.DiscordBot.Prefix}uptime | Shows the bots uptime \n${config.DiscordBot.Prefix}info | Get a bots info. \n ${config.DiscordBot.Prefix}suggest | Get the link to send in suggestions. \n ${config.DiscordBot.Prefix}apikey | Get a direct message of the api key.`,
-    "Staff": `${config.DiscordBot.Prefix}staff | See help for that command  \n${config.DiscordBot.Prefix}purge | Delete messages in a channel \n${config.DiscordBot.Prefix}access | Gain subuser access to a server`,
-    "Owner": `${config.DiscordBot.Prefix}reload | Reloads all commands on the bot \n${config.DiscordBot.Prefix}restart | Restarts **EVERYTHING**, Bot and Stats website \n${config.DiscordBot.Prefix}say | Says what you want it to say \n${config.DiscordBot.Prefix}eval | Eval some code \n${config.DiscordBot.Prefix}exec | Run some system commands \n${config.DiscordBot.Prefix}giveaway | Launch a giveaway`
+    "Users": `${config.DiscordBot.Prefix}user | See help for that command \n${config.DiscordBot.Prefix}server | See help for that command \n${config.DiscordBot.Prefix}stats | Shows the stats of each hosting node. \n${config.DiscordBot.Prefix}ticket | Create a ticket for help from the staff team! \n${config.DiscordBot.Prefix}status | Check the status of your server! \n${config.DiscordBot.Prefix}uptime | Shows the bots uptime \n${config.DiscordBot.Prefix}info | Get a bots info. \n ${config.DiscordBot.Prefix}suggest | Get the link to send in suggestions.`,
+    "Staff": `${config.DiscordBot.Prefix}staff | See help for that command  \n${config.DiscordBot.Prefix}purge | Delete messages in a channel \n${config.DiscordBot.Prefix}access | Gain subuser access to a server \n${config.DiscordBot.Prefix}mute | Mute da user  \n${config.DiscordBot.Prefix}kick | Kick da user`,
+    "Owner": `${config.DiscordBot.Prefix}reload | Reloads all commands on the bot \n${config.DiscordBot.Prefix}restart | Restarts **EVERYTHING**, Bot and Stats website \n${config.DiscordBot.Prefix}say | Says what you want it to say \n${config.DiscordBot.Prefix}eval | Eval some code \n${config.DiscordBot.Prefix}exec | Run some system commands \n${config.DiscordBot.Prefix}giveaway | Launch a giveaway \n${config.DiscordBot.Prefix}announce | Announce something`
 }
 
 exports.run = async (client, message, args) => {
@@ -25,3 +25,4 @@ exports.run = async (client, message, args) => {
         message.channel.send(embed)
     };
 };
+
