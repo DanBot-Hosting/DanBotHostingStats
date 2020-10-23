0677d2ff4a1b0e6b26933e5e93278d772f4f1167
danielpmc <danielpd93@gmail.com>
2020-10-23 14:09:57 +0100

    Restore ready.js from backup. Broke the bot

--- Panel/bot/discord/events/ready.js
+++ Panel/bot/discord/events/ready.js
@@ -1,8 +1,7 @@
+//let client = require("../../../../index.js").client;
 const axios = require('axios');
 module.exports = async (client, guild, files) => {
     console.log(chalk.magenta('[DISCORD] ') + chalk.green(client.user.username + " has logged in!"));
-
-    let guild = bot.guilds.get('639477525927690240');
 
     //Auto Activities List
     const activities = [{
@@ -38,32 +37,27 @@ module.exports = async (client, guild, files) => {
             invites[g.id] = guildInvites;
         });
     });
-
-    //Voice channel stats
+    
+    //Voice channel stats updator
     setInterval(async () => {
-        let staffCount = await guild.roles.get('748117822370086932').members;
-        client.channels.get("739821419910791348").edit({
-            name: `Staff: ${staffCount.size}`,
-            reason: "Staff count update"
-        });
+        let guild1 = await client.guilds.get("639477525927690240").fetchMembers();
+        let roleID1 = '748117822370086932';
+        let staffCount = guild1.roles.get(roleID1).members.size;
+        client.channels.get("739821419910791348").edit({ name: `Staff: ${staffCount}`, reason: "Staff count update" });
+        
+        let guild2 = await client.guilds.get("639477525927690240").fetchMembers();
+        let roleID2 = '639490038434103306';
+        let memberCount = guild2.roles.get(roleID2).members.size;
+        client.channels.get("739821366991257621").edit({ name: `Members: ${memberCount}`, reason: "Member count update" });
+        
+        let guild3 = await client.guilds.get("639477525927690240").fetchMembers();
+        let roleID3 = '704467807122882562';
+        let botCount = guild3.roles.get(roleID3).members.size;
+        client.channels.get("739821468296413254").edit({ name: `Bots: ${botCount}`, reason: "Bot count update" });
 
-        let memberCount = await guild.roles.get('639490038434103306').members.size;
-        client.channels.get("739821366991257621").edit({
-            name: `Members: ${memberCount.size}`,
-            reason: "Member count update"
-        });
-
-        let botCount = await guild3.roles.get('704467807122882562').members;
-        client.channels.get("739821468296413254").edit({
-            name: `Bots: ${botCount.size}`,
-            reason: "Bot count update"
-        });
-
-        const ticketcount = await guild.channels.filter(x => x.name.endsWith("-ticket"))
-        client.channels.get("739821447924416562").edit({
-            name: `Tickets: ${ticketcount.size}`,
-            reason: "Ticket count update"
-        })
+        let guild4 = await client.guilds.get("639477525927690240")
+        const ticketcount = guild4.channels.filter(x=> x.name.endsWith("-ticket")).size
+        client.channels.get("739821447924416562").edit({ name: `Tickets: ${ticketcount}`, reason: "Ticket count update"})
 
         axios({
             url: config.Pterodactyl.hosturl + "/api/application/servers",
@@ -76,10 +70,7 @@ module.exports = async (client, guild, files) => {
                 'Accept': 'Application/vnd.pterodactyl.v1+json',
             }
         }).then(response => {
-            client.channels.get("757199549977722890").edit({
-                name: `Servers Hosting: ${response.data.meta.pagination.total}`,
-                reason: "Server count update"
-            })
+        client.channels.get("757199549977722890").edit({ name: `Servers Hosting: ${response.data.meta.pagination.total}`, reason: "Server count update"})
         });
 
         axios({
@@ -93,14 +84,10 @@ module.exports = async (client, guild, files) => {
                 'Accept': 'Application/vnd.pterodactyl.v1+json',
             }
         }).then(response => {
-            client.channels.get("757222585015599214").edit({
-                name: `Clients Hosting: ${response.data.meta.pagination.total}`,
-                reason: "Client count update"
-            })
+        client.channels.get("757222585015599214").edit({ name: `Clients Hosting: ${response.data.meta.pagination.total}`, reason: "Client count update"})
         });
-        client.channels.get("758746579636191382").edit({
-            name: `Boosts: ${client.guilds.get("639477525927690240").premiumSubscriptionCount}`,
-            reason: "Boosts count update"
-        })
+        client.channels.get("758746579636191382").edit({ name: `Boosts: ${client.guilds.get("639477525927690240").premiumSubscriptionCount}`, reason: "Boosts count update" })
     }, 30000);
+
+
 };
