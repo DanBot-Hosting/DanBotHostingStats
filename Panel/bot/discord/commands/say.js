f228fc1130f77895971c5c0347e994cff1173c6c
Tea Cup <35646791+TheTeaCup@users.noreply.github.com>
2020-10-24 11:35:25 -0400

    Update say.js

--- Panel/bot/discord/commands/say.js
+++ Panel/bot/discord/commands/say.js
@@ -3,7 +3,7 @@ exports.run = async (client, message) => {
     
     let ids = ["338192747754160138", "137624084572798976", "293841631583535106"];
     if (!ids.includes(message.author.id)) {
-        return message.channel.send('You dont have perms to use this command. Its for my owner.');
+        return message.channel.send("You dont have perms to use this command. Its for my owner.");
     }
 
 
