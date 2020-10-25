144e292d209aa4fc8a8e77ed02ff59b4ab0bfb5c
HyptexPvP <63811460+HyptexPvP@users.noreply.github.com>
2020-10-25 14:18:49 -0400

    Update say.js

--- Panel/bot/discord/commands/say.js
+++ Panel/bot/discord/commands/say.js
@@ -1,10 +1,5 @@
 exports.run = async (client, message) => {
     const args = message.content.split(' ').slice(1).join(' ');
-
-    if (message.author.id == config.DiscordBot.ownerID) {
         message.delete()
         message.channel.send(args)
-    } else {
-        message.channel.send('You dont have perms to use this command. Its for my owner.');
-    }
 };
