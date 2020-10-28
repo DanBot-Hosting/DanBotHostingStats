db956c32959218f8c478fd8326084f4d8bfb701c
danielpmc <danielpd93@gmail.com>
2020-10-28 22:48:35 +0000

    Update eval.js

--- Panel/bot/discord/commands/eval.js
+++ Panel/bot/discord/commands/eval.js
@@ -30,7 +30,7 @@ exports.run = async (client, message) => {
     message.channel.send('Evaluating...').then(msg => {
         try {
             let code = args.join(' ');
-            let evaled = await eval(code);
+            let evaled = eval(code);
 
             if (typeof evaled !== 'string') {
                 evaled = require('util').inspect(evaled);
