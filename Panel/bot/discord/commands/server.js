45e44ff7502ebb8f00e5f2488a48857a31c757e0
danielpmc <danielpd93@gmail.com>
2020-10-30 10:06:33 +0000

    Update server.js

--- Panel/bot/discord/commands/server.js
+++ Panel/bot/discord/commands/server.js
@@ -1227,7 +1227,7 @@ exports.run = async (client, message, args) => {
                         } else {
 
                         if (output.attributes.user == userData.get(message.author.id).consoleID) {
-                            msg.edit('Are you sure you want to delete `' + output.attributes.name + '`?\nPlease type `confirm` to delete this server. You have 1min until this will expire \n\n**You can not restore the server once it has been deleted**')
+                            msg.edit('Are you sure you want to delete `' + output.attributes.name + '`?\nPlease type `confirm` to delete this server. You have 1min until this will expire \n\n**You can not restore the server once it has been deleted and/or its files**')
                             const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 60000, max: 2 });
                             collector.on('collect', message => {
                                 if (message == "confirm") {
