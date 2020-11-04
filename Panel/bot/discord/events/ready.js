7d186f7357b9ef3687325b91060fab5a56fa20a2
Hadimhz <Hadimhz@users.noreply.github.com>
2020-11-04 02:54:14 +0200

    Update ready.js

--- Panel/bot/discord/events/ready.js
+++ Panel/bot/discord/events/ready.js
@@ -37,7 +37,7 @@ module.exports = async (client, guild, files) => {
         if (unmuteIn < 0) unmuteIn = 1000;
 
         setTimeout(() => {
-            client.guilds.first().members.get(x.ID).removeRole('726829710935457872');
+            client.guilds.get('639477525927690240').members.get(x.ID).removeRole('726829710935457872');
             mutesData.delete(x.ID)
         }, unmuteIn)
 
