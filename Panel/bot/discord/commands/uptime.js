b29b0b953484c014490eaaa88a70adff2b35f638
danielpmc <danielpd93@gmail.com>
2020-11-07 02:03:24 +0000

    mb added to ram usage lmao.

--- Panel/bot/discord/commands/uptime.js
+++ Panel/bot/discord/commands/uptime.js
@@ -5,7 +5,7 @@ exports.run = async (client, message, args) => {
     let myDate = new Date(client.readyTimestamp);
     var embed = new Discord.RichEmbed()
         .addField(":white_check_mark: Uptime:", `**${humanizeDuration(client.uptime, {round: true})}**`, true)
-        .addField("Memory usage:", Math.trunc((process.memoryUsage().heapUsed) / 1024 / 1000), true)
+        .addField("Memory usage:", Math.trunc((process.memoryUsage().heapUsed) / 1024 / 1000) + "mb", true)
         .setFooter(`Ready Timestamp: ${myDate.toString()}`)
         .setColor("GREEN")
     message.channel.send(embed);
