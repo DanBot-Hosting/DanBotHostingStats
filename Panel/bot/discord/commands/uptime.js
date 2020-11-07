1d3b86842478de9b0f0e694f33940b542ff429a4
danielpmc <danielpd93@gmail.com>
2020-11-07 02:02:24 +0000

    Added memory usage to uptime cmd

--- Panel/bot/discord/commands/uptime.js
+++ Panel/bot/discord/commands/uptime.js
@@ -4,7 +4,8 @@ exports.run = async (client, message, args) => {
 
     let myDate = new Date(client.readyTimestamp);
     var embed = new Discord.RichEmbed()
-        .addField(":white_check_mark: Uptime:", `**${humanizeDuration(client.uptime, {round: true})}**`)
+        .addField(":white_check_mark: Uptime:", `**${humanizeDuration(client.uptime, {round: true})}**`, true)
+        .addField("Memory usage:", Math.trunc((process.memoryUsage().heapUsed) / 1024 / 1000), true)
         .setFooter(`Ready Timestamp: ${myDate.toString()}`)
         .setColor("GREEN")
     message.channel.send(embed);
