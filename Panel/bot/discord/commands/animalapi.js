cd75e74980d72925bc49e2f7e89f8164fa5dfa35
danielpmc <danielpd93@gmail.com>
2020-11-07 05:05:38 +0000

    Create animalapi.js

---
+++ Panel/bot/discord/commands/animalapi.js
@@ -0,0 +1,15 @@
+const superagent = require("snekfetch");
+exports.run = async (client, message, args) => {
+    if (!args[0]) {
+        superagent.get('https://api.danbot.host/dog')
+            .end((err, response) => {
+                const embed = new Discord.RichEmbed()
+                    .addField('[Dog API](https://api.danbot.host/dog):', response.body.total + " images.")
+            })
+    } else if (args[0] == "dog") {
+        superagent.get('https://api.danbot.host/dog')
+            .end((err, response) => {
+                message.channel.send("Random dog image from the DanBot Hosting api!", { file: `${response.body.image}` });
+        });
+    }
+};
