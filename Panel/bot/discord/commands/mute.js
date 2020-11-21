b536173697b021f924254139fb8d682a9c1fb50b
Hadimhz <Hadimhz@users.noreply.github.com>
2020-11-21 21:44:49 +0200

    Update mute.js

--- Panel/bot/discord/commands/mute.js
+++ Panel/bot/discord/commands/mute.js
@@ -27,7 +27,7 @@ exports.init = (client) => {
                 mutes[x.ID] = setTimeout(() => {
                     delete mutes[member.id];
                     mutesData.delete(x.ID);
-                    if (guild.members.get(x.ID) != null){
+                    if (guild.members.get(x.ID) != null) {
                         console.log("UNMUTE -  timout expired")
                         member.removeRole(config.DiscordBot.roles.mute);
                     }
@@ -47,59 +47,63 @@ exports.init = (client) => {
 
 
 exports.run = async (client, message, args) => {
-    let modlog = message.guild.channels.find(channel => channel.id == config.DiscordBot.mLogs);
+    try {
+        let modlog = message.guild.channels.find(channel => channel.id == config.DiscordBot.mLogs);
 
-    if (message.member.roles.get(config.DiscordBot.roles.staff) == null) return message.reply("sorry, but it looks like you're too much of a boomer to run this command.");
-    if (!message.guild.me.hasPermission('MANAGE_ROLES')) return message.reply('Sorry, i dont have the perms to do this cmd i need MANAGE_ROLES. :x:');
-
-
-    if (args.length < 1) {
-        message.channel.send('', {
-            embed: new Discord.RichEmbed().setColor(0x00A2E8)
-                .setDescription(`Correct usage ${config.DiscordBot.Prefix}mute <@user|userID> [Time : 5m] [Reason : unspecified]`).setFooter('<required> [optional]')
-        })
-        return;
-    }
-    let target = message.guild.members.get(args[0].match(/[0-9]{18}/)[0])
-    let reason = args.slice(2).join(' ') || `unspecified`;
-    let time = ms(args[1]) || 300000;
-
-    if (target == null) return message.reply("please specify a valid user.");
-    if (time > 604800000) time = 604800000;
+        if (message.member.roles.get(config.DiscordBot.roles.staff) == null) return message.reply("sorry, but it looks like you're too much of a boomer to run this command.");
+        if (!message.guild.me.hasPermission('MANAGE_ROLES')) return message.reply('Sorry, i dont have the perms to do this cmd i need MANAGE_ROLES. :x:');
 
 
-    await target.addRole(config.DiscordBot.roles.mute)
+        if (args.length < 1) {
+            message.channel.send('', {
+                embed: new Discord.RichEmbed().setColor(0x00A2E8)
+                    .setDescription(`Correct usage ${config.DiscordBot.Prefix}mute <@user|userID> [Time : 5m] [Reason : unspecified]`).setFooter('<required> [optional]')
+            })
+            return;
+        }
+        let target = message.guild.members.get(args[0].match(/[0-9]{18}/)[0])
+        let reason = args.slice(2).join(' ') || `unspecified`;
+        let time = ms(args[1]) || 300000;
 
-    mutesData.set(target.id, {
-        mutedAt: Date.now(),
-        expiresAt: Date.now() + time
-    });
-
-    message.channel.send(":white_check_mark: ***The user has been successfully muted for " + ms(time, {
-        long: true
-    }) + "!***");
-
-    if (mutes[target.id] != null) clearTimeout(mutes[target.id])
-    mutes[target.id] = setTimeout(() => {
-        delete mutes[target.id];
-        mutesData.delete(target.id);
-        if (message.guild.channels.find(x => x.id == args[0].match(/[0-9]{18}/)[0]) != null)
-            target.removeRole(config.DiscordBot.roles.mute);
-    }, time);
+        if (target == null) return message.reply("please specify a valid user.");
+        if (time > 604800000) time = 604800000;
 
 
-    if (modlog) {
-        modlog.send('', {
-            embed: new Discord.RichEmbed()
-                .setColor(0x00A2E8)
-                .setTitle("Action: Mute")
-                .addField("Moderator", message.author.tag + " (ID: " + message.author.id + ")")
-                .addField("User", target.user.tag + " (ID: " + target.id + ")")
-                .addField("Time", ms(time, {
-                    long: true
-                }), true)
-                .addField("Reason", reason, true)
-                .setFooter("Time used:").setTimestamp()
-        })
+        await target.addRole(config.DiscordBot.roles.mute)
+
+        mutesData.set(target.id, {
+            mutedAt: Date.now(),
+            expiresAt: Date.now() + time
+        });
+
+        message.channel.send(":white_check_mark: ***The user has been successfully muted for " + ms(time, {
+            long: true
+        }) + "!***");
+
+        if (mutes[target.id] != null) clearTimeout(mutes[target.id])
+        mutes[target.id] = setTimeout(() => {
+            delete mutes[target.id];
+            mutesData.delete(target.id);
+            if (message.guild.channels.find(x => x.id == args[0].match(/[0-9]{18}/)[0]) != null)
+                target.removeRole(config.DiscordBot.roles.mute);
+        }, time);
+
+
+        if (modlog) {
+            modlog.send('', {
+                embed: new Discord.RichEmbed()
+                    .setColor(0x00A2E8)
+                    .setTitle("Action: Mute")
+                    .addField("Moderator", message.author.tag + " (ID: " + message.author.id + ")")
+                    .addField("User", target.user.tag + " (ID: " + target.id + ")")
+                    .addField("Time", ms(time, {
+                        long: true
+                    }), true)
+                    .addField("Reason", reason, true)
+                    .setFooter("Time used:").setTimestamp()
+            })
+        }
+    } catch (error) {
+        console.log(error);
     }
 };
