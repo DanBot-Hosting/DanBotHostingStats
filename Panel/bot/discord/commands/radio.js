4a2c75c3f4b08d64ff76172e32c1f37217ce074c
danielpmc <danielpd93@gmail.com>
2020-11-28 11:19:48 +0000

    now playing to radio.js

--- Panel/bot/discord/commands/radio.js
+++ Panel/bot/discord/commands/radio.js
@@ -4,6 +4,15 @@ exports.run = (client, message, args, guild) => {
     if (message.author.id == "137624084572798976") {
         client.channels.get("781918160219668510").join()
         .then(connection => {
+            var internetradio = require('node-internet-radio');
+            var testStream = "http://62.171.128.136:8000/DanBotFM";
+            internetradio.getStationInfo(testStream, function(error, station) {
+                const embed = new Discord.RichEmbed()
+                .setColor("GREEN")
+                .addField("**Now playing**:", "DanBotFM")
+                .addField('**Song now playing**:', station.title)
+                message.channel.send(embed)
+            });
             connection.playStream('http://62.171.128.136:8000/DanBotFM')
         });
 
