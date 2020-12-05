155097df5073966059ae9432e95c97eb85488b03
Hadimhz <Hadimhz@users.noreply.github.com>
2020-12-05 06:36:23 +0200

    Update serverStatus.js

--- Panel/bot/discord/serverStatus.js
+++ Panel/bot/discord/serverStatus.js
@@ -83,7 +83,7 @@ let getEmbed = () => {
     embed.setTitle("Danbot Hosting Status");
 
     for (let [title, d] of Object.entries(data)) {
-        embed.setTitle(`${embed.title || ''}**__${title}:__**\n${d.join('\n')}\n\n`)
+        embed.setDescription(`${embed.description || ''}**__${title}:__**\n${d.join('\n')}\n\n`)
     }
     return embed;
 }
