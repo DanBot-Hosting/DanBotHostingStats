12bf0be38182b254c3fac28b04868a53bbc7b2a6
Hadimhz <Hadimhz@users.noreply.github.com>
2020-12-09 23:57:26 +0200

    Update server.js

--- Panel/bot/discord/commands/server.js
+++ Panel/bot/discord/commands/server.js
@@ -398,8 +398,6 @@ exports.run = async (client, message, args) => {
                 }).then(use => {
                     use = use.data.attributes;
 
-                    console.log(use.relationships);
-
                     if (use.relationships) {
                         let k = Object.keys(use.relationships);
                         use.extras = {};
@@ -412,8 +410,8 @@ exports.run = async (client, message, args) => {
                         delete use.relationships;
                     }
 
-                    console.log(use.extras);
-
+                    console.log(use.extras.servers.find(m => m.identifier == args[1]));
+                    
                     if (use.extras.servers == null || use.extras.servers.find(x => x.identifier == args[1]) == null) {
                         message.channel.send("Couldn't find that server in your server list.")
                         return;
