8f32c7fe764bab015826f375b3ac0b61abea37a0
danielpmc <danielpd93@gmail.com>
2020-12-09 16:14:31 +0000

    Update snipe.js

--- Panel/bot/discord/commands/snipe.js
+++ Panel/bot/discord/commands/snipe.js
@@ -6,7 +6,7 @@ exports.run = async (client, message, args) => {
         if (!snipe || !snipe.content) return message.channel.send(embed3)
 
         const embed = new Discord.RichEmbed()
-            .setAuthor(snipe.author.username, snipe.author.avatarURL({ dynamic: true, format: 'png' }))
+            //.setAuthor(snipe.author.username, snipe.author.avatarURL({ dynamic: true, format: 'png' }))
             .setTimestamp()
             .setTitle(`Message by: ${snipe.author.tag}`)
             .setDescription(snipe.content)
