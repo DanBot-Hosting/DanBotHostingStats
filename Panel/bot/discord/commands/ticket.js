33f3528a58e418478c3463c5fa2ad0da9225ce91
danielpmc <danielpd93@gmail.com>
2020-12-15 18:00:11 +0000

    cleaned and fixed the ticket command

--- Panel/bot/discord/commands/ticket.js
+++ Panel/bot/discord/commands/ticket.js
@@ -2,17 +2,17 @@ exports.run = async (client, message) => {
     const args = message.content.split(' ').slice(1).join(' ');
     const user = message.content.split(' ').slice(2).join(' ');
 
-    if (args == "") {
+    if (args === "") {
         let embed = new Discord.RichEmbed()
             .setColor(`GREEN`)
-            .addField(`__**Tickets**__`, 'You can create a new ticket by typing: `' + 
-            config.DiscordBot.Prefix + 'ticket new` \nYou can download your old tickets by running: `' + 
-            config.DiscordBot.Prefix + 'ticket logs` \nYou can close your ticket by running: `' + 
-            config.DiscordBot.Prefix + 'ticket close` \nYou can upgrade your ticket by running:`' + 
+            .addField(`__**Tickets**__`, 'You can create a new ticket by typing: `' +
+            config.DiscordBot.Prefix + 'ticket new` \nYou can download your old tickets by running: `' +
+            config.DiscordBot.Prefix + 'ticket logs` \nYou can close your ticket by running: `' +
+            config.DiscordBot.Prefix + 'ticket close` \nYou can upgrade your ticket by running:`' +
             config.DiscordBot.Prefix + 'ticket upgrade` \n\nAny problems? Please send a message in <#739231758087880845> and someone will help you.' , true);
-        message.channel.send(embed)
+        await message.channel.send(embed)
 
-    } else if (args == "new") {
+    } else if (args === "new") {
         const server = message.guild
 
         let channel = await server.createChannel(message.author.username + "-Ticket", "text", [{
@@ -33,10 +33,10 @@ exports.run = async (client, message) => {
         ]).catch(console.error);
         message.reply(`Please check <#${channel.id}> for your ticket.`)
 
-        let category = server.channels.find(c => c.id == "738538742603841650" && c.type == "category");
+        let category = server.channels.find(c => c.id === "738538742603841650" && c.type === "category");
         if (!category) throw new Error("Category channel does not exist");
 
-        let categorybackup = server.channels.find(c => c.id == "741082659610034257" && c.type == "category");
+        let categorybackup = server.channels.find(c => c.id === "741082659610034257" && c.type === "category");
         if (!categorybackup) throw new Error("Category channel does not exist");
 
         await channel.setParent(category.id).catch(channel.setParent(categorybackup.id).catch(console.error));
@@ -47,7 +47,7 @@ exports.run = async (client, message) => {
                 SEND_MESSAGES: true,
                 READ_MESSAGE_HISTORY: true
             })
-            
+
         }, 1000)
 
         if (userData.get(message.author.id) == null) {
@@ -61,7 +61,7 @@ exports.run = async (client, message) => {
                 .addField(`__**Time**__`, userData.fetch(message.author.id + ".linkTime"))
             channel.send('<@' + message.author.id + '> here is your ticket! Please give as much info as possible about your problem. \n\n *This account is linked with:* ', embed)
         }
-    } else if (args == "close") {
+    } else if (args === "close") {
         if (message.channel.name.includes('-ticket')) {
             const filter2 = m => m.author.id === message.author.id;
             const warning = await message.channel.send('<@' + message.author.id + '> are you sure you want to close this ticket? please type `confirm` to close the ticket or `cancel` to keep the ticket open.')
@@ -90,13 +90,13 @@ exports.run = async (client, message) => {
             message.channel.send('ERROR: You can only use this command in ticket channels.')
 
         }
-    } else if (args == "add") {
+    } else if (args === "add") {
         if (message.channel.name.includes('-ticket')) {
-            if (args[1] == "") {
+            if (args[1] === "") {
                 message.channel.send('Please run this command again with the users ID')
             } else {
                 console.log(args[1])
-                message.channel.overwritePermissions(args[1], {
+                await message.channel.overwritePermissions(args[1], {
                     VIEW_CHANNEL: true,
                     SEND_MESSAGES: true,
                     READ_MESSAGE_HISTORY: true
@@ -105,10 +105,10 @@ exports.run = async (client, message) => {
         } else {
             message.channel.send('This command is only to be used inside of ticket channels.')
         }
-    } else if (args == "upgrade") {
+    } else if (args === "upgrade") {
         if (message.channel.name.includes('-ticket')) {
             message.reply("Only admins can see this ticket now.")
-                message.channel.overwritePermissions("748117822370086932", {
+                await message.channel.overwritePermissions("748117822370086932", {
                     VIEW_CHANNEL: false,
                     SEND_MESSAGES: false,
                     READ_MESSAGE_HISTORY: false
@@ -116,29 +116,20 @@ exports.run = async (client, message) => {
         } else {
             message.channel.send('This command is only to be used inside of ticket channels.')
         }
-    } else if (args == "remove") {
-        if (args[1] == "") {
+    } else if (args === "remove") {
+        if (args[1] === "") {
             message.channel.send('Please run this command again with the users ID')
         } else {
-            message.channel.overwritePermissions(args[1], {
+            await message.channel.overwritePermissions(args[1], {
                 VIEW_CHANNEL: false,
                 SEND_MESSAGES: false,
                 READ_MESSAGE_HISTORY: false
             })
         }
+    } else if(args === "label") {
+        //This needs reworking. ill get to this soon lets just fix the command for now
     }
-        else if(args == "label") {
-            if(args[1] == "") {
-                message.channel.send('No label inputed');
-            }
-            else {
-                if(message.author.roles.cache.has('748117822370086932') {
-                   message.channel.setTopic(`Label: ${args[1]`);
-                   }
-            }
-            
-        }
+        
 
-    }
-    
-};
+
+}
