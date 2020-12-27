1e89856af1fc434c93691654c0077e31ad4427c9
danielpmc <danielpd93@gmail.com>
2020-12-27 06:01:30 +0000

    Update raw.js

--- Panel/bot/discord/events/raw.js
+++ Panel/bot/discord/events/raw.js
@@ -1,13 +1,13 @@
 let findDuplicates = arr => arr.filter((e, i) => arr.indexOf(e) != i)
 module.exports = async (client, e) => {
     if (e.t === "MESSAGE_REACTION_ADD") {
-        let channel = bot.channels.get(e.d.channel_id);
+        let channel = bot.channels.cache.get(e.d.channel_id);
         /**
          * @type {Discord.Message}
          */
-        let message = await channel.fetchMessage(e.d.message_id);
+        let message = await channel.messages.fetch(e.d.message_id);
 
-        let member = message.guild.members.get(e.d.user_id);
+        let member = message.guild.members.cache.get(e.d.user_id);
         let emoji = e.d.emoji;
 
         let reactionMessage = message.reactions.find(x => x.emoji.id === emoji.id || x.emoji.name === emoji.name);
@@ -16,13 +16,13 @@ module.exports = async (client, e) => {
     }
 
     if (e.t === "MESSAGE_REACTION_REMOVE") {
-        let channel = bot.channels.get(e.d.channel_id);
+        let channel = bot.channels.cache.get(e.d.channel_id);
         /**
          * @type {Discord.Message}
          */
         let message = await channel.fetchMessage(e.d.message_id);
 
-        let member = message.guild.members.get(e.d.user_id);
+        let member = message.guild.members.cache.get(e.d.user_id);
         let emoji = e.d.emoji;
 
         let reactionMessage = message.reactions.find(x => x.emoji.id === emoji.id || x.emoji.name === emoji.name);
