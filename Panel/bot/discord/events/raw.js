6e689a5291a0e91398bb06232fc24dc3dc6699da
Hadimhz <Hadimhz@users.noreply.github.com>
2020-12-30 14:04:07 +0200

    Update raw.js

--- Panel/bot/discord/events/raw.js
+++ Panel/bot/discord/events/raw.js
@@ -9,10 +9,10 @@ module.exports = async (client, e) => {
         let reactionMessage = message.reactions.cache.find(x => (x.emoji.id != null && x.emoji.id == emoji.id) || x.emoji.name == emoji.name);
         bot.emit('reactionAdd', reactionMessage, member)
     }
-
+    
     if (e.t == "MESSAGE_REACTION_REMOVE") {
-        let channel = bot.channels.get(e.d.channel_id);
-        let message = await channel.fetchMessage(e.d.message_id);
+        let channel = bot.channels.cache.get(e.d.channel_id);
+        let message = await channel.messages.fetch(e.d.message_id);
         let member = message.guild.members.cache.get(e.d.user_id);
         let emoji = e.d.emoji;
         let reactionMessage = message.reactions.cache.find(x => (x.emoji.id != null && x.emoji.id == emoji.id) || x.emoji.name == emoji.name);
