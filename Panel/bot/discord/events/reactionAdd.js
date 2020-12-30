ca4f1196f3bbea236c7fef0a45933adfe684949a
Hadimhz <Hadimhz@users.noreply.github.com>
2020-12-30 14:24:15 +0200

    Reaction roles

--- Panel/bot/discord/events/reactionAdd.js
+++ Panel/bot/discord/events/reactionAdd.js
@@ -25,14 +25,16 @@ module.exports = async (client, r, member) => {
     if (member.user.bot == true) return;
     let emoji = r.emoji.id != null ? r.emoji.id : r.emoji.name;
 
-    
+
     // Reaction Roles
     let reactionRole = parse();
     let found = reactionRole.filter(x => x.message == r.message.id && x.reaction == emoji);
     if (found.length > 0) {
         found = found[0];
         let role = member.guild.roles.cache.get(found.role);
-        await member.roles.add([role, '765869330024890378']);
-        member.user.send("gave you the role: `" + role.name + "`!");
+        if (role.members.cache.get(member.id) == null) {
+            await member.roles.add([role, '765869330024890378']);
+            member.user.send("gave you the role: `" + role.name + "`!");
+        }
     }
 }
--- Panel/bot/discord/events/reactionRemove.js
+++ Panel/bot/discord/events/reactionRemove.js
@@ -25,15 +25,17 @@ module.exports = async (client, r, member) => {
     if (member.user.bot == true) return;
     let emoji = r.emoji.id != null ? r.emoji.id : r.emoji.name;
 
-    
+
     // Reaction Roles
     let reactionRole = parse();
     let found = reactionRole.find(x => x.message == r.message.id && x.reaction == emoji);
 
     let role = member.guild.roles.cache.get(found.role);
-    await member.roles.remove(role);
-    member.user.send("removed the role: `" + role.name + "`!");
-    let memberRoles = member.roles.cache.map(x => x.id).concat(reactionRole.map(x => x.role))
-    let dupped = memberRoles.filter((e, i) => memberRoles.indexOf(e) != i);
-    if (dupped.length == 0) member.roles.remove('765869330024890378')
+    if (role.members.cache.get(member.id) == null) {
+        await member.roles.remove(role);
+        member.user.send("removed the role: `" + role.name + "`!");
+        let memberRoles = member.roles.cache.map(x => x.id).concat(reactionRole.map(x => x.role))
+        let dupped = memberRoles.filter((e, i) => memberRoles.indexOf(e) != i);
+        if (dupped.length == 0) member.roles.remove('765869330024890378')
+    }
 }
