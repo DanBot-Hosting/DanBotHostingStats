56785ce224f65804ce77bafdb3160bc39975b5c1
danielpmc <danielpd93@gmail.com>
2021-01-03 04:39:37 +0000

    Added Red-DiscordBot to createserver

--- Panel/createData.js
+++ Panel/createData.js
@@ -11,6 +11,36 @@ var getPassword = () => {
 };
 
 let list = {};
+list.reddiscordbot = (serverName, userID) => ({
+    "name": serverName,
+    "user": userID,
+    "nest": 5,
+    "egg": 47,
+    "docker_image": "quay.io/parkervcp/pterodactyl-images:bot_red",
+    "startup": `PATH=$PATH:/home/container/.local/bin redbot pterodactyl --token {{TOKEN}} --prefix {{PREFIX}}`,
+    "limits": {
+        "memory": 0,
+        "swap": 0,
+        "disk": 0,
+        "io": 500,
+        "cpu": 0
+    },
+    "environment": {
+        "TOKEN": "Your discord bot api token",
+        "PREFIX": "."
+    },
+    "feature_limits": {
+        "databases": 2,
+        "allocations": 1,
+        "backups": 10
+    },
+    "deploy": {
+        "locations": [9, 3, 11],
+        "dedicated_ip": false,
+        "port_range": []
+    },
+    "start_on_completion": false
+});
 list.nodejs = (serverName, userID) => ({
     "name": serverName,
     "user": userID,
@@ -758,6 +788,7 @@ list.postgres = (serverName, userID) => ({
 
 let data = (serverName, userID) => {
     let toReturn = {
+        reddiscordbot: null,
         nodejs: null,
         python: null,
         aio: null,
