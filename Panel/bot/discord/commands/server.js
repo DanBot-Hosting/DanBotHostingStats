05de57b82156d39016946256a90ea66441da1ffd
danielpmc <danielpd93@gmail.com>
2021-01-12 17:24:31 +0000

    am i dumb because im tired or should this work??!?!?!!??!?!?!?!!?

--- Panel/bot/discord/commands/server.js
+++ Panel/bot/discord/commands/server.js
@@ -226,7 +226,7 @@ exports.run = async (client, message, args) => {
 
         })
     } else if (args[0].toLowerCase() === "create-beta") {
-        if (!message.member.roles.cache.find(r => r.id === '793549158417301544' | '710208090741539006' | '788193704014905364')) return message.channel.send('You are not allowed to use this. F')
+        if (message.member.roles.cache.find(r => r.id === "710208090741539006" | "788193704014905364" | "793549158417301544")) {
 
             let bServerCreatesettings = serverCreateSettings_Beta.createParams(serverName, consoleID.consoleID);
 
@@ -291,7 +291,7 @@ exports.run = async (client, message, args) => {
                 .addField(`__**SteamCMD:**__`, "Rust", true)
                 .setFooter("Example: " + config.DiscordBot.Prefix + "server create-beta NodeJS Testing Server"))
 
-
+        }
     } else if (args[0].toLowerCase() === "delete") {
 
         if (cooldown[message.author.id].delete > Date.now()) {
