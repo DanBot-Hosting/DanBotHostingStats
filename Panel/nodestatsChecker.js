dd99ed2539af76bffd43e37666df43baf9e99f97
danielpmc <danielpd93@gmail.com>
2021-01-19 19:55:33 +0000

    updated node 1's ip

--- Panel/nodestatsChecker.js
+++ Panel/nodestatsChecker.js
@@ -5,7 +5,7 @@ const ping2 = require('ping-tcp-js')
 let stats = {
     node1: {
         serverID: '7c740e8c',
-        IP: '154.27.68.105'
+        IP: '142.54.191.91'
     },
     node2: {
         serverID: '4bbbdb0d',
