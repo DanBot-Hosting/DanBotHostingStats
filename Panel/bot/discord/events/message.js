f77dd302467353f5dc553bbf15371fd29e89aa4f
danielpmc <danielpd93@gmail.com>
2021-01-21 22:43:39 +0000

    Typo

--- Panel/bot/discord/events/message.js
+++ Panel/bot/discord/events/message.js
@@ -49,7 +49,7 @@ module.exports = (client, message) => {
     };
 
     if(message.author.bot) return; // to stop bots from creating accounts, tickets and more.
-    if(message.channel.type === "dm") return; //stops commandsworking in dms
+    if(message.channel.type === "dm") return; //stops commands working in dms
     const prefix = config.DiscordBot.Prefix;
     if (message.content.indexOf(prefix) !== 0) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
