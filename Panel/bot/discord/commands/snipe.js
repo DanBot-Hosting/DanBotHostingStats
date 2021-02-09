de4cb6167f323d3d8b88f939a5a9c685b27aadc6
Steel <50340593+Steel-Discord@users.noreply.github.com>
2021-02-09 06:53:40 +0000

    Update snipe.js

--- Panel/bot/discord/commands/snipe.js
+++ Panel/bot/discord/commands/snipe.js
@@ -2,13 +2,17 @@ exports.run = async (client, message, args) => {
     let embed3 = new Discord.MessageEmbed()
             .setDescription(`Theres nothing to snipe`)
 
-        let snipe = snipes.get(message.channel.id)
+
+if(!args.length) return message.channel.send(`There are a total of ${snipes.get(message.channel.id).length} snipes. Provide one to view.`);
+
+        let snipe = snipes.get(message.channel.id)[args[0]];
         if (!snipe || !snipe.content) return message.channel.send(embed3)
 
         const embed = new Discord.MessageEmbed()
-            //.setAuthor(snipe.author.username, snipe.author.avatarURL({ dynamic: true, format: 'png' }))
+            //.setAuthor(snipe.message.author.username, snipe.message.author.avatarURL({ dynamic: true, format: 'png' }))
             .setTimestamp()
-            .setTitle(`Message by: ${snipe.author.tag}`)
-            .setDescription("`" + snipe.content + "`")
+            .setTitle(`Message by: ${snipe.message.author.tag}`)
+            .setDescription(`${snipe.message.content}`);
+            
         message.channel.send(embed);
 }
