9d827f96a17437995a72487511d65be6e1eca190
Steel <50340593+Steel-Discord@users.noreply.github.com>
2021-02-09 07:00:57 +0000

    Update esnipe.js

--- Panel/bot/discord/commands/esnipe.js
+++ Panel/bot/discord/commands/esnipe.js
@@ -3,7 +3,7 @@ exports.run = async (client, message, args) => {
             .setDescription(`Theres nothing to snipe`)
 
 
-if(!args.length) return message.channel.send(`There are a total of ${edits.get(message.channel.id).length} edit snipes. Provide one to view.`);
+if(!args.length) return message.channel.send(`There are a total of **${edits.get(message.channel.id).length}** edit snipes. Provide one to view.`);
 
         let snipe = edits.get(message.channel.id)[args[0]];
         if (!snipe || !snipe.content) return message.channel.send(embed3)
@@ -13,8 +13,8 @@ if(!args.length) return message.channel.send(`There are a total of ${edits.get(m
             .setTimestamp()
             .setTitle(`Message by: ${snipe.message.author.tag}`)
             .addFields(
-            { name: "Old Content", value: `${snipe.message.content}`, inline: true },
-            { name: "New Content", value: `${snipe.editedMessage.content}`, inline: true },
+            { name: "Old Content", value: `\`${snipe.message.content}\``, inline: true },
+            { name: "New Content", value: `\`${snipe.editedMessage.content}\``, inline: true },
             
             );
             
