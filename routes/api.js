b26d354721c389f2353632c9eb4cb0b8c0103e5e
Hadimhz <Hadimhz@users.noreply.github.com>
2021-02-21 16:10:55 +0200

    removed tea's ids and added my ip to the staff list on the website

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ const Router = require("express").Router();
 const db = require("quick.db");
 const isSnowflake = require(process.cwd() + "/util/isSnowflake.js");
 const passport = require("passport");
-let Developers = [ "137624084572798976", "338192747754160138" ];
+let Developers = [ "137624084572798976", "293841631583535106" ];
 const rateLimitt = require('express-rate-limit');
 
 var axios = require("axios")
