ed39205c8f44c55c835f913bbaf6a5defdc40776
danielpmc <danielpd93@gmail.com>
2021-02-23 20:54:06 +0000

    Update mute.js

--- bot/discord/commands/mute.js
+++ bot/discord/commands/mute.js
@@ -81,8 +81,8 @@ exports.run = async (client, message, args) => {
     let time = ms(args[1]) || 300000;
 
     if (target == null) return message.reply("please specify a valid user.");
-    if (time > 604800000) time = 604800000;
-    if (time < 5000) time = 5000;
+    if (time > 2592000000) time = 2592000000; //30days
+    if (time < 5000) time = 5000; //5seconds
 
 
     await target.roles.add(config.DiscordBot.roles.mute)
