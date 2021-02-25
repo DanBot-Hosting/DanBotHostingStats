610017c51e974c3848ef3e3abab64585eee10433
danielpmc <danielpd93@gmail.com>
2021-02-25 11:27:46 +0000

    Remove useless stuff in exec

--- bot/discord/commands/exec.js
+++ bot/discord/commands/exec.js
@@ -2,14 +2,11 @@ const exec = require('child_process').exec;
 exports.run = (client, message, args) => {
   if (message.member.roles.cache.find(r => r.id === "778237595477606440")) {
 
-    const start = process.hrtime();
-
     exec(`${args.join(" ")}`, (error, stdout) => {
 
       let response = (error || stdout);
-      
+
       if (response.length > 1024) console.log(response), response = 'Output too long.';
-      const end = process.hrtime(start);
 
       message.channel.send("", {
         embed: new Discord.MessageEmbed()
