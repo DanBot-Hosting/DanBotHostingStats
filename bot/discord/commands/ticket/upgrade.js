1858d311069b8b73d60dded2369d6219b51a797a
Hadimhz <Hadimhz@users.noreply.github.com>
2021-02-28 20:21:38 +0200

    undo'd a bit too much

--- bot/discord/commands/ticket/upgrade.js
+++ bot/discord/commands/ticket/upgrade.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args) => {
-    if (message.channel.name.cache.includes('-ticket')) {
+    if (message.channel.name.includes('-ticket')) {
         message.reply("Only admins can see this ticket now.")
         await message.channel.overwritePermissions([{
             id: '748117822370086932',
