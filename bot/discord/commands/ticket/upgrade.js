2f97ce365f3406f916d04053b3c1ff41ec7bd050
Hadimhz <Hadimhz@users.noreply.github.com>
2021-02-28 20:26:41 +0200

    i legit hate discord.js v12's permission system

--- bot/discord/commands/ticket/upgrade.js
+++ bot/discord/commands/ticket/upgrade.js
@@ -1,13 +1,9 @@
 exports.run = async (client, message, args) => {
+    let staffRole = message.guild.roles.cache.get('748117822370086932');
+
     if (message.channel.name.includes('-ticket')) {
         message.reply("Only admins can see this ticket now.")
-        await message.channel.overwritePermissions([{
-            id: '748117822370086932',
-            deny: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY']
-        }, {
-            id: message.guild.id,
-            deny: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY']
-        }]);
+        await message.channel.updateOverwrite('748117822370086932', { VIEW_CHANNEL: false, SEND_MESSAGES: false, READ_MESSAGE_HISTORY: false });
     } else {
         message.channel.send('This command is only to be used inside of ticket channels.')
     }
