55560bea69930ff44bbe4b126a1099c50e6bedde
danielpmc <danielpd93@gmail.com>
2021-03-14 21:12:20 +0000

    Update createData.js

--- createData.js
+++ createData.js
@@ -102,7 +102,7 @@ list.nodejs = (serverName, userID) => ({
     "nest": 5,
     "egg": 50,
     "docker_image": "quay.io/parkervcp/pterodactyl-images:debian_nodejs-12",
-    "startup": `if [[ -d .git ]] && [[ {{AUTO_UPDATE}} == "1" ]]; then git pull; fi && /usr/local/bin/npm install --production --python=/usr/bin/python3 && /usr/local/bin/node /home/container/{{BOT_JS_FILE}}`,
+    "startup": `/usr/local/bin/npm i && /usr/local/bin/node /home/container/{{BOT_JS_FILE}}`,
     "limits": {
         "memory": 0,
         "swap": 0,
