d51d6a8b04ed000ce8e9809ebb521a18c641062e
Hadimhz <solo1010.mahfouz@gmail.com>
2021-03-23 16:15:49 +0200

    Update delete.js

--- bot/discord/commands/server/delete.js
+++ bot/discord/commands/server/delete.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 exports.run = async (client, message, args) => {
 
-    if (message.channel.name.includes('ticket')) return message.channel.send('this is currently disabled')
+    if (!message.channel.name.includes('ticket')) return message.channel.send('this is currently disabled')
 
     if (client.cooldown[message.author.id] == null) {
         client.cooldown[message.author.id] = {
