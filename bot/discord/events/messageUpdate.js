409f24048e519bb70eaaf69bda45b17fc6656b4d
danielpmc <danielpd93@gmail.com>
2021-03-23 19:20:03 +0000

    Update messageUpdate.js

--- bot/discord/events/messageUpdate.js
+++ bot/discord/events/messageUpdate.js
@@ -1,6 +1,27 @@
 //let client = require("../../../../index.js").client;
-const fetch = require('node-fetch');
+/* const fetch = require('node-fetch');
 module.exports = (client, message, editedMessage) => {
+  let whitelisted = ['137624084572798976'];
+    if (!whitelisted.includes(message.author.id)) {
+    const inviteREE = new RegExp(/(https?:\/\/)?(www\.)?(discord\.(gg|io|me|li)|discordapp\.com\/invite)\/.+[a-z]/g);
+    if (inviteREE.test(editedMessage.content)) {
+        const msgcontent = editedMessage.content
+        code = msgcontent.replace(/(https:\/\/)?(www\.)?(discord\.gg|discord\.me|discordapp\.com\/invite|discord\.com\/invite)\/?/g, "");
+        console.log(code)
+        fetch(`https://discordapp.com/api/invite/${code}`)
+        .then((res) => res.json())
+        .then((json) => {
+            if (json.message === 'Unknown Invite') {
+               //Do nothing
+               console.log(json.message)
+            } else  {
+                message.delete()
+                console.log('uh oh')
+                console.log(json)
+            }
+        });
+    }
+}
 
   if (message.author.bot) return;
   if (message.channel.type === 'dm') return;
@@ -18,4 +39,4 @@ module.exports = (client, message, editedMessage) => {
   client.channels.cache.get(config.DiscordBot.mLogs).send({
     embed
   });
-};
+};*/
