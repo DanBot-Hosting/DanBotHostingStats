b15fbc9d01d716c568679b12f3e347161507bb63
danielpmc <danielpd93@gmail.com>
2021-03-28 21:35:25 +0100

    finally fixed https://danbot.host/stats

--- routes/stats.js
+++ routes/stats.js
@@ -3,15 +3,26 @@ const db = require("quick.db");
 const isSnowflake = require(process.cwd() + "/util/isSnowflake.js");
 
 Router.get("/", (req, res) => {
-  let items = nodeData.all();
+  let n1 = [ nodeData.fetch('Node1') ]
+  let n2 = [ nodeData.fetch('Node2') ]
+  let n3 = [ nodeData.fetch('Node3') ]
+  let n4 = [ nodeData.fetch('Node4') ]
+  let n5 = [ nodeData.fetch('Node5') ]
+  let n6 = [ nodeData.fetch('Node6') ]
+  let n7 = [ nodeData.fetch('Node7') ]
+  let n8 = [ nodeData.fetch('Node8') ]
+  let n9 = [ nodeData.fetch('Node9') ]
+  let n10 = [ nodeData.fetch('Node10') ]
+  let n11 = [ nodeData.fetch('Node11') ]
+  let n12 = [ nodeData.fetch('Node12') ]
+  let n13 = [ nodeData.fetch('Node13') ]
+  let n14 = [ nodeData.fetch('Node14') ]
+  let data = n1.concat(n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14);
   //console.log(items)
-  var objectValue = JSON.parse(items);
-
-  let filteredItems = objectValue.filter(i => i.ID === "Node");
-  console.log(filteredItems)
-
+  //var objectValue = JSON.parse(data);
+console.log(data)
   res.render('stats.ejs', {
-    table: filteredItems,
+    table: data,
     user: req.isAuthenticated() ? req.user : null
   })
 });
