79f795417377a11131966b828bcd3de0d1509956
Hadimhz <solo1010.mahfouz@gmail.com>
2021-03-29 00:17:17 +0300

    i missed one

--- bot/discord/events/messageUpdate.js
+++ bot/discord/events/messageUpdate.js
@@ -3,14 +3,14 @@ module.exports = (client, oldMessage, newMessage) => {
 
     // Snipe Command:
 
-    snipes.set(message.channel.id, [...snipes.get(oldMessage.channel.id), {
+    snipes.set(oldMessage.channel.id, [...snipes.get(oldMessage.channel.id), {
         message: oldMessage.content,
         author: oldMessage.member,
         timestamp: Date.now(),
         action: "edit"
     }]);
 
-    snipes.set(message.channel.id, snipes.get(oldMessage.channel.id).filter(x => (Date.now() - x.timestamp) < 300000 && x != null));
+    snipes.set(oldMessage.channel.id, snipes.get(oldMessage.channel.id).filter(x => (Date.now() - x.timestamp) < 300000 && x != null));
 
     // --------------
 
