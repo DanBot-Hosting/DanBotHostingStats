8b5e173397ce867efdf84f6a6ed8a73559e6d8b6
danielpmc <danielpd93@gmail.com>
2021-03-28 22:31:09 +0100

    e

--- bot/discord/serverStatus.js
+++ bot/discord/serverStatus.js
@@ -98,7 +98,7 @@ let parse = async () => {
 
             let da = nodeStatus.get(d.data.toLowerCase());
 
-            da = (da.status === true ? (`🟢 Online (${nodeServers.get(d.data.toLowerCase())} / 500)`) : ((da.is_vm_online == null ? "🔴 **Offline**" : ((da.is_vm_online === true ? "🟠 Wings" : "🔴 **VM**") + ' Outage'))))
+            da = (da.status === true ? (`🟢 Online (${nodeServers.get(d.data.toString().toLowerCase())} / 500)`) : ((da.is_vm_online == null ? "🔴 **Offline**" : ((da.is_vm_online === true ? "🟠 Wings" : "🔴 **VM**") + ' Outage'))))
 
 
             // if(nodeStatus.get(d.data).is_vm_online != null && nodeStatus.get('node1').is_vm_online === false && nodeStatus.get('node2').is_vm_online === false && nodeStatus.get('node5').is_vm_online === false, nodeStatus.get('node7').is_vm_online === false)
