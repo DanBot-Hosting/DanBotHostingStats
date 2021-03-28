969cbc1dea4c0a95f11268d992e6c01896111113
danielpmc <danielpd93@gmail.com>
2021-03-28 22:25:47 +0100

    Update serverStatus.js

--- bot/discord/serverStatus.js
+++ bot/discord/serverStatus.js
@@ -98,13 +98,13 @@ let parse = async () => {
 
             let da = nodeStatus.get(d.data.toLowerCase());
 
-            da = (da.status === true ? ('🟢 Online') : ((da.is_vm_online == null ? "🔴 **Offline**" : ((da.is_vm_online === true ? "🟠 Wings" : "🔴 **VM**") + ' Outage'))))
+            da = (da.status === true ? (`🟢 Online (${nodeServers.fetch(d.data.toLowerCase())} / 500)`) : ((da.is_vm_online == null ? "🔴 **Offline**" : ((da.is_vm_online === true ? "🟠 Wings" : "🔴 **VM**") + ' Outage'))))
 
 
             // if(nodeStatus.get(d.data).is_vm_online != null && nodeStatus.get('node1').is_vm_online === false && nodeStatus.get('node2').is_vm_online === false && nodeStatus.get('node5').is_vm_online === false, nodeStatus.get('node7').is_vm_online === false)
             //    da = '🔴 Network Outage'
 
-            temp.push(`**${d.name}:** ${da} (${nodeServers.fetch(d.data)} / 500)`)
+            temp.push(`**${d.name}:** ${da}`)
         }
 
         toRetun[title] = temp;
