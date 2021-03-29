4f9f3e43630a1e266e97927cf2f8ebb2fe2697a9
danielpmc <danielpd93@gmail.com>
2021-03-29 17:34:40 +0100

    Update beta.js

--- routes/beta.js
+++ routes/beta.js
@@ -12,35 +12,35 @@ Router.get("/stats", (req, res) => {
     let data = {
       Node1: nodeData.fetch('node1'),
       Node2: nodeData.fetch('node2'),
-      Node3: nodeData.fetch('node3'),
-      Node4: nodeData.fetch('node4'),
-      Node5: nodeData.fetch('node5'),
-      Node6: nodeData.fetch('node6'),
-      Node7: nodeData.fetch('node7'),
-      Node8: nodeData.fetch('node8'),
-      Node9: nodeData.fetch('node9'),
-      Node10: nodeData.fetch('node10'),
-      Node11: nodeData.fetch('node11'),
-      Node12: nodeData.fetch('node12'),
-      Node13: nodeData.fetch('node13'),
-      Node14: nodeData.fetch('node14')
+      Node3: nodeData.fetch('Node3'),
+      Node4: nodeData.fetch('Node4'),
+      Node5: nodeData.fetch('Node5'),
+      Node6: nodeData.fetch('Node6'),
+      Node7: nodeData.fetch('Node7'),
+      Node8: nodeData.fetch('Node8'),
+      Node9: nodeData.fetch('Node9'),
+      Node10: nodeData.fetch('Node10'),
+      Node11: nodeData.fetch('Node11'),
+      Node12: nodeData.fetch('Node12'),
+      Node13: nodeData.fetch('Node13'),
+      Node14: nodeData.fetch('Node14')
     }
 
     let status = {
-      Node1: nodeStatus.fetch('Node1'),
-      Node2: nodeStatus.fetch('Node2'),
-      Node3: nodeStatus.fetch('Node3'),
-      Node4: nodeStatus.fetch('Node4'),
-      Node5: nodeStatus.fetch('Node5'),
-      Node6: nodeStatus.fetch('Node6'),
-      Node7: nodeStatus.fetch('Node7'),
-      Node8: nodeStatus.fetch('Node8'),
-      Node9: nodeStatus.fetch('Node9'),
-      Node10: nodeStatus.fetch('Node10'),
-      Node11: nodeStatus.fetch('Node11'),
-      Node12: nodeStatus.fetch('Node12'),
-      Node13: nodeStatus.fetch('Node13'),
-      Node14: nodeStatus.fetch('Node14')
+      Node1: nodeStatus.fetch('node1'),
+      Node2: nodeStatus.fetch('node2'),
+      Node3: nodeStatus.fetch('node3'),
+      Node4: nodeStatus.fetch('node4'),
+      Node5: nodeStatus.fetch('node5'),
+      Node6: nodeStatus.fetch('node6'),
+      Node7: nodeStatus.fetch('node7'),
+      Node8: nodeStatus.fetch('node8'),
+      Node9: nodeStatus.fetch('node9'),
+      Node10: nodeStatus.fetch('node10'),
+      Node11: nodeStatus.fetch('node11'),
+      Node12: nodeStatus.fetch('node12'),
+      Node13: nodeStatus.fetch('node13'),
+      Node14: nodeStatus.fetch('node14')
     }
 
     res.json({ error: false, data: data, status: status });
