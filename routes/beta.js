5adbd41d5862f8629ad8283020e28003fdc56fc2
danielpmc <danielpd93@gmail.com>
2021-03-29 17:31:01 +0100

    Update beta.js

--- routes/beta.js
+++ routes/beta.js
@@ -26,7 +26,24 @@ Router.get("/stats", (req, res) => {
       Node14: nodeData.fetch('Node14')
     }
 
-    res.json({ error: false, data: data });
+    let status = {
+      Node1: nodeStatus.fetch('Node1'),
+      Node2: nodeStatus.fetch('Node2'),
+      Node3: nodeStatus.fetch('Node3'),
+      Node4: nodeStatus.fetch('Node4'),
+      Node5: nodeStatus.fetch('Node5'),
+      Node6: nodeStatus.fetch('Node6'),
+      Node7: nodeStatus.fetch('Node7'),
+      Node8: nodeStatus.fetch('Node8'),
+      Node9: nodeStatus.fetch('Node9'),
+      Node10: nodeStatus.fetch('Node10'),
+      Node11: nodeStatus.fetch('Node11'),
+      Node12: nodeStatus.fetch('Node12'),
+      Node13: nodeStatus.fetch('Node13'),
+      Node14: nodeStatus.fetch('Node14')
+    }
+
+    res.json({ error: false, data: data, status: status });
   } catch (e) {
     res.json({ error: true, message: e });
   }
