c592dfd5d5a738a07f33308c66c3db5cdb186b13
Hadimhz <solo1010.mahfouz@gmail.com>
2021-03-29 04:22:38 +0300

    [Snipe] finished?

--- bot/discord/commands/snipe.js
+++ bot/discord/commands/snipe.js
@@ -5,23 +5,27 @@ exports.run = async (client, message, args) => {
 
     if (snipe == null) return message.channel.send(embed3)
 
+    //Reversing the array 
+    snipe.reverse();
 
+    // getting the number
     let number = 0;
 
-    if(args[0] == null) number = 0;
+    if (args[0] == null) number = 0;
     else number = parseInt(args[0] - 1);
 
-    if(number >= snipe.length) number = snipe.length - 1;
-    if(number < 0) number = 0;
+    //setting a min and max
+    if (number >= snipe.length) number = snipe.length - 1;
+    if (number < 0) number = 0;
 
+    // getting the message
     let snipedMessage = snipe[number];
-        
 
-        const embed = new Discord.MessageEmbed()
-            //.setAuthor(snipe.author.username, snipe.author.avatarURL({ dynamic: true, format: 'png' }))
-            .setTimestamp()
-            .setTitle(`Message by: ${snipedMessage.member.user.tag}`)
-            .setDescription("`" + snipedMessage.message + "`")
-            .setFooter(`action: ${snipedMessage.action} at `).setTimestamp(snipedMessage.timestamp)
-        message.channel.send(embed);
+    //sending the message
+    const embed = new Discord.MessageEmbed()
+        .setTitle(`Message ${snipedMessage.action} by ${snipedMessage.member.user.tag}`)
+        .setDescription("`" + snipedMessage.message + "`")
+        .setFooter(`${number + 1}/${snipe.length}`).setTimestamp(snipedMessage.timestamp)
+        .setColor("GREEN");
+    message.channel.send(embed);
 }
