e14863dd1ef6a0dbe5c159b85744cf106799a3bb
danielpmc <danielpd93@gmail.com>
2021-03-30 21:36:30 +0100

    Added Assetto Corsa

--- createData.js
+++ createData.js
@@ -780,11 +780,48 @@ list.mumble = (serverName, userID) => ({
 
 SteamCMD Servers
 
+Assetto Corsa
 Arma 3
 7 Days to die
 Rust
 
 */
+list.assettocorsa = (serverName, userID) => ({
+    "name": serverName,
+    "user": userID,
+    "nest": 4,
+    "egg": 54,
+    "docker_image": "quay.io/parkervcp/pterodactyl-images:ubuntu_source",
+    "startup": './acServer',
+    "limits": {
+        "memory": 2048,
+        "swap": 0,
+        "disk": 0,
+        "io": 500,
+        "cpu": 0
+    },
+    "environment": {
+        "STEAM_USER": null,
+        "STEAM_PASS": null,
+        "STEAM_AUTH": null,
+        "HOSTNAME": "DBH hosted Assetto Corsa server.",
+        "PASSWORD": null,
+        "ADMIN_PASSWORD": getPassword(),
+        "HTTP_PORT": "8081"
+    },
+    "feature_limits": {
+        "databases": 2,
+        "allocations": 1,
+        "backups": 10
+    },
+    "deploy": {
+        "locations": gaming,
+        "dedicated_ip": false,
+        "port_range": []
+    },
+    "start_on_completion": false,
+    "oom_disabled": false
+})
 list.arma = (serverName, userID) => ({
     "name": serverName,
     "user": userID,
