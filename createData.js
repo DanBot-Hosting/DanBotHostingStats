a7029c7b1fdad53ece0ebd3390c0d9ba153890f5
danielpmc <danielpd93@gmail.com>
2021-03-30 21:33:39 +0100

    Added 7 days to die, Arma 3

--- createData.js
+++ createData.js
@@ -780,9 +780,89 @@ list.mumble = (serverName, userID) => ({
 
 SteamCMD Servers
 
+Arma 3
+7 Days to die
 Rust
 
 */
+list.arma = (serverName, userID) => ({
+    "name": serverName,
+    "user": userID,
+    "nest": 4,
+    "egg": 53,
+    "docker_image": "quay.io/parkervcp/pterodactyl-images:game_arma3",
+    "startup": './{{SERVER_BINARY}} -ip=0.0.0.0 -port={{SERVER_PORT}} -profiles=./serverprofile -bepath=./battleye -cfg=\\"{{BASIC}}\\" -config=\\"{{CONFIG}}\\" -mod=\\"{{MODIFICATIONS}}\\" -serverMod=\\"{{SERVERMODS}}\\" {{STARTUP_PARAMS}}',
+    "limits": {
+        "memory": 2048,
+        "swap": 0,
+        "disk": 0,
+        "io": 500,
+        "cpu": 0
+    },
+    "environment": {
+        "STEAMCMD_APPID": "233780",
+        "STEAM_USER": "your_steam_username",
+        "STEAM_PASS": "your_steam_password",
+        "SERVER_BINARY": "arma3server_x64",
+        "STARTUP_PARAMS": "-noLogs",
+        "CONFIG": "server.cfg",
+        "BASIC": "basic.cfg",
+        "MODIFICATIONS": null,
+        "SERVERMODS": null,
+        "UPDATE_SERVER": 0,
+        "UPDATE_WORKSHOP": null,
+        "MODS_LOWERCASE": 0,
+        "STEAMCMD_EXTRA_FLAGS": null,
+        "HC_NUM": "0",
+        "HC_PASSWORD": null,
+    },
+    "feature_limits": {
+        "databases": 2,
+        "allocations": 1,
+        "backups": 10
+    },
+    "deploy": {
+        "locations": gaming,
+        "dedicated_ip": false,
+        "port_range": []
+    },
+    "start_on_completion": false,
+    "oom_disabled": false
+})
+list.daystodie = (serverName, userID) => ({
+    "name": serverName,
+    "user": userID,
+    "nest": 4,
+    "egg": 52,
+    "docker_image": "quay.io/parkervcp/pterodactyl-images:ubuntu_source",
+    "startup": './7DaysToDieServer.x86_64 -configfile=serverconfig.xml -quit -batchmode -nographics -dedicated -ServerPort=${SERVER_PORT} -ServerMaxPlayerCount=${MAX_PLAYERS} -GameDifficulty=${GAME_DIFFICULTY} -ControlPanelEnabled=false -TelnetEnabled=true -TelnetPort=8081 -logfile logs/latest.log & echo -e "Checing on telnet connection" && until nc -z -v -w5 127.0.0.1 8081; do echo "Waiting for telnet connection..."; sleep 5; done && telnet -E 127.0.0.1 8081',
+    "limits": {
+        "memory": 2048,
+        "swap": 0,
+        "disk": 0,
+        "io": 500,
+        "cpu": 0
+    },
+    "environment": {
+        "MAX_PLAYERS": "8",
+        "GAME_DIFFICULTY": "2",
+        "SRCDS_APPID": "294420",
+        "AUTO_UPDATE": "1",
+        "LD_LIBRARY_PATH": "."
+    },
+    "feature_limits": {
+        "databases": 2,
+        "allocations": 1,
+        "backups": 10
+    },
+    "deploy": {
+        "locations": gaming,
+        "dedicated_ip": false,
+        "port_range": []
+    },
+    "start_on_completion": false,
+    "oom_disabled": false
+})
 list.rust = (serverName, userID) => ({
     "name": serverName,
     "user": userID,
