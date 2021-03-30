568b1ae9e8f29e9d36da97ec77833efb47d1767b
Jonfirexbox <66625200+Jonfirexbox@users.noreply.github.com>
2021-03-30 16:38:28 -0400

    Update command.js

--- bot/discord/commands/dan/command.js
+++ bot/discord/commands/dan/command.js
@@ -3,7 +3,7 @@ exports.run = async (client, message, args) => {
 
     const dan = new Discord.MessageEmbed()
         .setTitle('Dan\/s Special Command \n`' +
-        'DAN IS THE BEST! DAN IS THE BEST! DAN IS THE BEST! DAN IS THE BEST! DAN IS THE BEST! DAN IS THE BEST! (Solo is good too)\nDan is Our God and Savior')
+        'DAN IS THE BEST! DAN IS THE BEST! DAN IS THE BEST! DAN IS THE BEST! DAN IS THE BEST! DAN IS THE BEST!\nDan is Our God and Savior\nWith out him, we would never have this amazing Hosting Website/Panel!')
 
     await message.channel.send(dan)
 }
