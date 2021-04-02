5036e6769ae91939a637eb05db88af77b0412b05
danielpmc <danielpd93@gmail.com>
2021-04-02 02:39:41 +0100

    SOLO SUCKS

--- bot/discord/commands/eval.js
+++ bot/discord/commands/eval.js
@@ -18,75 +18,75 @@ exports.run = async (client, message) => {
         })
     } else {
 
-    function clean(text) {
-        if (typeof text !== 'string')
-            text = require('util').inspect(text, {
-                depth: 0
-            })
-        let rege = new RegExp(client.token, "gi");
-        let rege2 = new RegExp("9 + 10", "gi");
-        text = text
-            .replace(/`/g, '`' + String.fromCharCode(8203))
-            .replace(/@/g, '@' + String.fromCharCode(8203))
-            .replace(rege, '(node:800) UnhandledPromiseRejectionWarning: Error: Incorrect login details were provided.')
-            .replace(rege2, '21')
-        return text;
-    };
-    message.channel.send('Evaluating...').then(msg => {
-        try {
-            let code = args.join(' ');
-            let evaled = eval(`(async () => {${code}})()`);
+        function clean(text) {
+            if (typeof text !== 'string')
+                text = require('util').inspect(text, {
+                    depth: 0
+                })
+            let rege = new RegExp(client.token, "gi");
+            let rege2 = new RegExp("9 + 10", "gi");
+            text = text
+                .replace(/`/g, '`' + String.fromCharCode(8203))
+                .replace(/@/g, '@' + String.fromCharCode(8203))
+                .replace(rege, '(node:800) UnhandledPromiseRejectionWarning: Error: Incorrect login details were provided.')
+                .replace(rege2, '21')
+            return text;
+        };
+        message.channel.send('Evaluating...').then(msg => {
+            try {
+                let code = args.join(' ');
+                let evaled = eval(code);
 
-            if (typeof evaled !== 'string') {
-                evaled = require('util').inspect(evaled);
-            }
-            if (evaled.length > 2000) {
-                try {
-                    let evalcode1 = new Discord.MessageEmbed()
-                        .setAuthor(`Eval by ${message.author.tag}`, `https://cdn.discordapp.com/emojis/314405560701419520.png`)
-                        .setDescription(`**Input:**\n\n\`\`\`js\n${cont}\`\`\``, true)
-                        .addField(`\u200b`, `**Output:**\n\n\`\`\`Output too long, logged to eval.txt`, true)
-                        .setColor(0x00FF00)
-                        .setFooter(`Node.js - Time taken: ${Date.now() - message.createdTimestamp} ms`);
-                    msg.edit({
-                        embed: evalcode1
-                    }), fs.writeFile(`eval.txt`, `${clean(evaled)}`), message.channel.send("Eval output", {
-                        files: ["eval.txt"]
-                    });
-                    return fs.writeFile(`eval.txt`, `${clean(evaled)}`);
-                } catch (err) {
-                    let errorcode1 = new Discord.MessageEmbed()
-                        .setAuthor(`Eval by ${message.author.tag}`, `https://cdn.discordapp.com/emojis/314405560701419520.png`)
-                        .setDescription(`**Input:**\n\n\`\`\`js\n${cont}\`\`\``, true)
-                        .addField(`\u200b`, `**Output:**\n\n\`\`\`js\nOutput too long, logged to ${__dirname}\\eval.txt\`\`\``, true)
-                        .setColor(0xFF0000)
-                        .setFooter(`Node.js - Time taken: ${Date.now() - message.createdTimestamp} ms `, `https://images-ext-2.discordapp.net/eyJ1cmwiOiJodHRwczovL2Euc2FmZS5tb2UvVUJFVWwucG5nIn0.LbWCXwiUul3udoS7s20IJYW8xus`);
-                    msg.edit({
-                        embed: errorcode1
-                    });
-                    return fs.writeFile(`eval.txt`, `${clean(err)}`);
+                if (typeof evaled !== 'string') {
+                    evaled = require('util').inspect(evaled);
                 }
+                if (evaled.length > 2000) {
+                    try {
+                        let evalcode1 = new Discord.MessageEmbed()
+                            .setAuthor(`Eval by ${message.author.tag}`, `https://cdn.discordapp.com/emojis/314405560701419520.png`)
+                            .setDescription(`**Input:**\n\n\`\`\`js\n${cont}\`\`\``, true)
+                            .addField(`\u200b`, `**Output:**\n\n\`\`\`Output too long, logged to eval.txt`, true)
+                            .setColor(0x00FF00)
+                            .setFooter(`Node.js - Time taken: ${Date.now() - message.createdTimestamp} ms`);
+                        msg.edit({
+                            embed: evalcode1
+                        }), fs.writeFile(`eval.txt`, `${clean(evaled)}`), message.channel.send("Eval output", {
+                            files: ["eval.txt"]
+                        });
+                        return fs.writeFile(`eval.txt`, `${clean(evaled)}`);
+                    } catch (err) {
+                        let errorcode1 = new Discord.MessageEmbed()
+                            .setAuthor(`Eval by ${message.author.tag}`, `https://cdn.discordapp.com/emojis/314405560701419520.png`)
+                            .setDescription(`**Input:**\n\n\`\`\`js\n${cont}\`\`\``, true)
+                            .addField(`\u200b`, `**Output:**\n\n\`\`\`js\nOutput too long, logged to ${__dirname}\\eval.txt\`\`\``, true)
+                            .setColor(0xFF0000)
+                            .setFooter(`Node.js - Time taken: ${Date.now() - message.createdTimestamp} ms `, `https://images-ext-2.discordapp.net/eyJ1cmwiOiJodHRwczovL2Euc2FmZS5tb2UvVUJFVWwucG5nIn0.LbWCXwiUul3udoS7s20IJYW8xus`);
+                        msg.edit({
+                            embed: errorcode1
+                        });
+                        return fs.writeFile(`eval.txt`, `${clean(err)}`);
+                    }
+                }
+                let evalcode = new Discord.MessageEmbed()
+                    .setAuthor(`Eval by ${message.author.tag}`, `https://cdn.discordapp.com/emojis/314405560701419520.png`)
+                    .setDescription(`**:inbox_tray: Input:**\n\n\`\`\`js\n${cont}\`\`\``, true)
+                    .addField(`\u200b`, `**:outbox_tray: Output:**\n\n\`\`\`js\n${clean(evaled)}\`\`\``, true)
+                    .setColor(0x00FF00)
+                    .setFooter(`Node.js - Time taken: ${Date.now() - message.createdTimestamp} ms`);
+                msg.edit({
+                    embed: evalcode
+                }).catch(e => logger.error(e));
+            } catch (err) {
+                let errorcode = new Discord.MessageEmbed()
+                    .setAuthor(`Eval by ${message.author.tag}`, `https://cdn.discordapp.com/emojis/314405560701419520.png`)
+                    .setDescription(`**:inbox_tray: Input:**\n\n\`\`\`js\n${cont}\`\`\``, true)
+                    .addField(`\u200b`, `**:outbox_tray: Output:**\`\`\`js\n${clean(err)}\`\`\``, true)
+                    .setColor(0xFF0000)
+                    .setFooter(`Node.js - Time taken: ${Date.now() - message.createdTimestamp} `);
+                msg.edit({
+                    embed: errorcode
+                }).catch(e => logger.error(e));
             }
-            let evalcode = new Discord.MessageEmbed()
-                .setAuthor(`Eval by ${message.author.tag}`, `https://cdn.discordapp.com/emojis/314405560701419520.png`)
-                .setDescription(`**:inbox_tray: Input:**\n\n\`\`\`js\n${cont}\`\`\``, true)
-                .addField(`\u200b`, `**:outbox_tray: Output:**\n\n\`\`\`js\n${clean(evaled)}\`\`\``, true)
-                .setColor(0x00FF00)
-                .setFooter(`Node.js - Time taken: ${Date.now() - message.createdTimestamp} ms`);
-            msg.edit({
-                embed: evalcode
-            }).catch(e => logger.error(e));
-        } catch (err) {
-            let errorcode = new Discord.MessageEmbed()
-                .setAuthor(`Eval by ${message.author.tag}`, `https://cdn.discordapp.com/emojis/314405560701419520.png`)
-                .setDescription(`**:inbox_tray: Input:**\n\n\`\`\`js\n${cont}\`\`\``, true)
-                .addField(`\u200b`, `**:outbox_tray: Output:**\`\`\`js\n${clean(err)}\`\`\``, true)
-                .setColor(0xFF0000)
-                .setFooter(`Node.js - Time taken: ${Date.now() - message.createdTimestamp} `);
-            msg.edit({
-                embed: errorcode
-            }).catch(e => logger.error(e));
-        }
-    });
-}
+        });
+    }
 };
