662baf7b039e2511a274efa61205ca3547b2fbbe
Solo <40409775+Hadimhz@users.noreply.github.com>
2021-04-02 16:40:40 +0300

    Update ping.js

--- bot/discord/commands/ping.js
+++ bot/discord/commands/ping.js
@@ -7,7 +7,6 @@ let embed = new Discord.MessageEmbed()
 .setColor("RED")
 .setTitle("DanBot Hosting - Ping")
 .setDescription(`API Latency: ${Math.round(client.ws.ping)}ms`)
-.setFooter("Command Created by Kyro#3400")
 .setTimestamp()
 message.channel.send(embed)
 
