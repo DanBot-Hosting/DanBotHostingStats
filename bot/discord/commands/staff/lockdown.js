d9fa80637ad2777d7b1040a1403db8da045d9c05
danielpmc <danielpd93@gmail.com>
2021-04-28 00:06:20 +0100

    Remove solos ID - no longer staff

--- bot/discord/commands/staff/lockdown.js
+++ bot/discord/commands/staff/lockdown.js
@@ -1,7 +1,7 @@
 exports.run = async (client, message, args) => {
     if (!message.member.roles.cache.find(r => r.id === "639489438036000769")) return;
 
-    if (!['137624084572798976', '293841631583535106'].includes(message.author.id)) return;
+    if (!['137624084572798976'].includes(message.author.id)) return;
     if (!args[1]) {
         message.channel.send('Channel is now locked. Only admins+ can post here \nUse `DBH!staff lockdown unlock` to unlock this channel')
         message.channel.updateOverwrite("639477525927690240", {
