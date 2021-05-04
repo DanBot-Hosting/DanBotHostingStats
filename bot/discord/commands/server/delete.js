cf7cc492820487bc3ebc9b204e88d14379dcf956
danielpmc <danielpd93@gmail.com>
2021-05-04 23:52:06 +0100

    Update delete.js

--- bot/discord/commands/server/delete.js
+++ bot/discord/commands/server/delete.js
@@ -75,7 +75,7 @@ exports.run = async (client, message, args) => {
                                         }).then(response => {
                                             msg.edit('Server deleted!')
 
-                                            if (output.attributes.node === 14 || output.attributes.node === 27)
+                                            if (output.attributes.node === 14 || output.attributes.node === 21)
                                                 userPrem.set(message.author.id + '.used', userPrem.fetch(message.author.id).used - 1);
 
                                             collector.stop()
