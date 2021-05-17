6df5931acd1fceec5807d4db39691afcf5fd4dce
Hadimhz <solo1010.mahfouz@gmail.com>
2021-05-17 17:46:23 +0300

    updated staff premium
    added 3 subcommands (add, remove, set) and removed boosted subcommand

--- bot/discord/commands/staff/premium.js
+++ bot/discord/commands/staff/premium.js
@@ -1,24 +1,37 @@
 exports.run = async (client, message, args) => {
     if (!['137624084572798976', '293841631583535106'].includes(message.author.id)) return;
-    if (args.lenght < 4) return;
 
-    if (args[1].toLowerCase() === 'donated') {
-        let amount = Number.parseInt(args[3])
-        if (isNaN(amount)) return;
-        let userid = args[2]
+    if (args.length < 4) return;
 
+    let setDonations = (userid, amount) => {
         userPrem.set(userid + '.donated', amount)
+    }
+
+    let sendMessage = (userid, amount) => {
         message.channel.send('Thanks <@' + userid + '> for donating! \nYou can now create donator servers using `' + config.DiscordBot.Prefix + 'server create-donator`')
         client.channels.cache.get('795884677688721448').send('Thanks, <@' + userid + '> for donating ' + amount + '$!')
     }
 
-    if (args[1].toLowerCase() === 'boosted') {
-        let amount = Number.parseInt(args[3])
-        if (isNaN(amount)) return;
-        let userid = args[2]
+    let userid = message.guild.members.cache.get(args[2].match(/[0-9]{18}/).length == 0 ? args[2] : args[2].match(/[0-9]{18}/)[0]);
+    let amount = Number.parseInt(args[3])
+    if (isNaN(amount)) return;
 
-        userPrem.set(userid + '.boosted', amount)
-        message.reply('done')
+    let oldBal = userPrem.get(userid + '.donated') || 0
+
+    if (args[1].toLowerCase() === 'add') {
+        setDonations(userid, amount + oldBal);
+        sendMessage(userid, amount + oldBal)
+    }
+
+
+    if (args[1].toLowerCase() === 'set') {
+        setDonations(userid, amount);
+        sendMessage(userid, amount)
+    }
+
+    if (args[1].toLowerCase() === 'remove') {
+        setDonations(userid, Math.max(0, oldBal - amount));
+        sendMessage(userid, Math.max(0, oldBal - amount))
     }
 
 }
