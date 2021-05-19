26cd417f4ec0bd5e88063c9e30a0edb0ef095481
danielpmc <danielpd93@gmail.com>
2021-05-19 14:27:28 +0100

    Update redeem.js

--- bot/discord/commands/server/redeem.js
+++ bot/discord/commands/server/redeem.js
@@ -1,4 +1,9 @@
 exports.run = async (client, message, args) => {
+    let setDonations = (userid, amount) => {
+        userPrem.set(userid + '.donated', amount)
+    }
+    let oldBal = userPrem.get(message.author.id + '.donated') || 0
+
     if (!args[1]) {
         message.channel.send('Usage is: `DBH!server redeem code`')
     } else {
@@ -8,6 +13,7 @@ exports.run = async (client, message, args) => {
             message.channel.send(`You have added ${codes.get(args[1])} premium server(s) to this account!`)
             client.channels.cache.get('795884677688721448').send('<@' + message.author.id + '>, Redeemed code: `' + args[1] + '` it held **' + codes.get(args[1]) + '** premium servers!')
             codes.delete(args[1]);
+            setDonations(message.author.id, codes.get(args[1]) + oldBal);
         }
     }
 }
