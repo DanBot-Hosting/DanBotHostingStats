5a30cac4b8d84d161f1a271b614b403129c1e4e5
danielpmc <danielpd93@gmail.com>
2021-05-23 18:33:13 +0100

    Updated Node status

--- bot/discord/serverStatus.js
+++ bot/discord/serverStatus.js
@@ -12,9 +12,6 @@ let nstatus = {
         name: 'Node 6',
         data: 'Node6'
     }, {
-        name: 'Node 7',
-        data: 'node7'
-    }, {
         name: 'Node 8',
         data: 'Node8'
     }, {
@@ -36,9 +33,6 @@ let nstatus = {
         name: 'Node 14',
         data: 'Node14'
     }, {
-        name: 'Node 15',
-        data: 'Node15'
-    }, {
         name: 'Node 16',
         data: 'Node16'
     }, {
@@ -55,9 +49,12 @@ let nstatus = {
         data: 'Node20'
     }],
 
-    "Storage Servers": [{
-        name: 'Backup Storage',
-        data: 'backups1'
+    "Donator Nodes": [{
+        name: 'Node 7',
+        data: 'node7'
+    }, {
+        name: 'Node 15',
+        data: 'node15'
     }],
 
     "Panel": [{
