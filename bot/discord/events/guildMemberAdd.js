a6cba6796cb5dfdebc236538cb55c830111976e9
danielpmc <danielpd93@gmail.com>
2021-06-03 21:37:51 +0100

    Full Bonk Mode

--- bot/discord/events/guildMemberAdd.js
+++ bot/discord/events/guildMemberAdd.js
@@ -4,12 +4,13 @@ module.exports = async (client, member, guild) => {
     if (enabled.Welcome === true) {
         let welcomeChannel = client.channels.cache.get(config.DiscordBot.welcome);
 
-        if (Date.now() - member.user.createdAt < 863136000) {
+        if (Date.now() - member.user.createdAt < 432000000) {
+            await member.ban() && welcomeChannel.send('Bonk')
+
+        } else if (Date.now() - member.user.createdAt < 863136000) {
             await member.user.send(`Sorry! We only allow accounts over the age of 10days to join. \nYour account was created ${humanizeDuration(Date.now() - member.user.createdAt, {round: true})} ago.\n\nYou are welcome to join again once this account is over 10days old!`)
             await member.kick()
             welcomeChannel.send(member.user.tag + ` has been auto-kicked as account is under 10days old.\nThat account was created ${humanizeDuration(Date.now() - member.user.createdAt, {round: true})}, ago`)
-        } else if (Date.now() - member.user.createdAt < 432000000) {
-            await member.ban({reason: "ALT Account"})
         }
 
         if (member.user.bot) {
