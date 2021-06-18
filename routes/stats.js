cee0eaa3f4ff76c11e7194a79a6dff558944ae1a
danielpmc <danielpd93@gmail.com>
2021-06-18 11:32:19 +0100

    uh

--- routes/stats.js
+++ routes/stats.js
@@ -17,7 +17,27 @@ Router.get("/", (req, res) => {
     let n12 = [nodeData.fetch('Node12')]
     let n13 = [nodeData.fetch('Node13')]
     let n14 = [nodeData.fetch('Node14')]
-    let data = n1.concat(n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14);
+    let n15 = [nodeData.fetch('Node15')]
+    let n16 = [nodeData.fetch('Node16')]
+    let n17 = [nodeData.fetch('Node17')]
+    let n1status = [nodeStatus.fetch('node1')]
+    let n2status = [nodeStatus.fetch('node2')]
+    let n3status = [nodeStatus.fetch('node3')]
+    let n4status = [nodeStatus.fetch('node4')]
+    let n5status = [nodeStatus.fetch('node5')]
+    let n6status = [nodeStatus.fetch('node6')]
+    let n7status = [nodeStatus.fetch('node7')]
+    let n8status = [nodeStatus.fetch('node8')]
+    let n9status = [nodeStatus.fetch('node9')]
+    let n10status = [nodeStatus.fetch('node10')]
+    let n11status = [nodeStatus.fetch('node11')]
+    let n12status = [nodeStatus.fetch('node12')]
+    let n13status = [nodeStatus.fetch('node13')]
+    let n14status = [nodeStatus.fetch('node14')]
+    let n15status = [nodeStatus.fetch('node15')]
+    let n16status = [nodeStatus.fetch('node16')]
+    let n17status = [nodeStatus.fetch('node17')]
+    let data = n1.concat(n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14, n15, n16, n17, n1status, n2status, n3status, n4status, n5status, n6status, n7status, n8status, n9status, n10status, n11status, n12status, n13status, n14status, n15status, n16status, n17status);
     res.render('stats.ejs', {
         table: data,
         user: req.isAuthenticated() ? req.user : null
