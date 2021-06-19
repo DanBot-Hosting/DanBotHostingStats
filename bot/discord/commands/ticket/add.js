1cc55b3840f9e57b621a6d5007c5d6ec0d9a1d3a
Hadimhz <solo1010.mahfouz@gmail.com>
2021-06-19 11:14:15 +0300

    eeee should look better now

--- bot/discord/commands/ticket/add.js
+++ bot/discord/commands/ticket/add.js
@@ -1,15 +1,18 @@
 exports.run = async (client, message, args) => {
-    if (message.channel.name.includes('-ticket')) {
-        if ( !args[1] || !message.guild.members.cache.get(args[1]) ) {
-            message.channel.send('Please run this command again with the users ID')
-        } else {
-            await message.channel.overwritePermissions(args[1], {
-                VIEW_CHANNEL: true,
-                SEND_MESSAGES: true,
-                READ_MESSAGE_HISTORY: true
-            })
-        }
-    } else {
+    if (!message.channel.name.includes('-ticket')) {
         message.channel.send('This command is only to be used inside of ticket channels.')
+        return;
+    }
+
+    if (!args[1] || !message.guild.members.cache.get(args[1])) {
+        message.channel.send('Please run this command again with the users ID')
+    } else {
+        await message.channel.overwritePermissions(args[1], {
+            VIEW_CHANNEL: true,
+            SEND_MESSAGES: true,
+            READ_MESSAGE_HISTORY: true
+        })
+        message.channel.send("Sucess!")
     }
 }
+
