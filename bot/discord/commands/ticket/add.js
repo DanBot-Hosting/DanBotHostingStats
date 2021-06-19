9908cd10f588db36e469557d83bcb0331695b1e4
Ritam Choudhuri <xcyth@xcyth.xyz>
2021-06-19 13:41:23 +0530

    this should work

--- bot/discord/commands/ticket/add.js
+++ bot/discord/commands/ticket/add.js
@@ -1,10 +1,9 @@
 exports.run = async (client, message, args) => {
     if (message.channel.name.includes('-ticket')) {
-        if (args[1] === "") {
+        if ( !args[1] || !message.guild.members.cache.get(args[1]) ) {
             message.channel.send('Please run this command again with the users ID')
         } else {
-            console.log(args[1])
-            await message.channel.cache.overwritePermissions(args[1], {
+            await message.channel.overwritePermissions(args[1], {
                 VIEW_CHANNEL: true,
                 SEND_MESSAGES: true,
                 READ_MESSAGE_HISTORY: true
