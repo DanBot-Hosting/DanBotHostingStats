6ffd2483cb4c61f65f411be2741708daabfeea67
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-07-15 11:56:20 -0500

    Update remove.js
    Made a few better changes and where members can't remove staff from tickets

--- bot/discord/commands/ticket/remove.js
+++ bot/discord/commands/ticket/remove.js
@@ -1,17 +1,18 @@
 exports.run = async (client, message, args) => {
-    if (!message.channel.name.includes('-ticket')) {
-        message.channel.send('This command is only to be used inside of ticket channels.')
-        return;
-    }
-
-    if (!args[1] || !message.guild.members.cache.get(args[1])) {
-        message.channel.send('Please run this command again with the users ID')
-    } else {
-        await message.channel.updateOverwrite(args[1], {
-            VIEW_CHANNEL: false,
-            SEND_MESSAGES: false,
-            READ_MESSAGE_HISTORY: false
-        })
-        message.channel.send("Success!")
-    }
+if (!message.channel.name.includes('-ticket')) {
+    message.channel.send('This command is only to be used inside of ticket channels.')
+    return;
+}
+let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+if(!member){
+    return message.channel.send('Please Mention someone or use their ID to remove the user from the ticket')
+}
+if(member.roles.cache.some(role => role.id === '844027709859823626')) return message.channel.send(`You can't remove staff From tickets`)
+    await message.channel.updateOverwrite(member, {
+        VIEW_CHANNEL: false,
+        SEND_MESSAGES: false,
+        READ_MESSAGE_HISTORY: false
+    })
+    message.channel.send(`Removed ${member} From the ticket`)
+}
 }
