934498396bcc3cd21218c191142a33030bfe47b4
mqrkelich <70708315+mqrkelich@users.noreply.github.com>
2021-07-18 14:38:32 +0200

    Ticket System Rewrite

--- bot/discord/commands/ticket/close.js
+++ bot/discord/commands/ticket/close.js
@@ -1,46 +1,58 @@
 exports.run = async (client, message, args) => {
-    if (message.channel.name.includes('-ticket')) {
-        const filter2 = m => m.author.id === message.author.id;
-        const warning = await message.channel.send('<@' + message.author.id + '> are you sure you want to close this ticket? please type `confirm` to close the ticket or `cancel` to keep the ticket open.')
 
-        let collected1 = await message.channel.createMessageCollector(filter2, {
-            max: 1,
-            time: 30000,
-            errors: ['time'],
-        })
-        collected1.on('collect', m => {
-            if (m.content.toLowerCase() === "confirm") {
-                message.channel.send("**Closing ticket.**", null).then(setTimeout(() => {
-                    message.channel.delete()
-                    message.channel.messages.fetch().then(async (messages) => {
-                        const fs = require('fs')
-                        const script = messages.array().reverse().map(m => `${m.author.tag}: ${m.attachments.size > 0 ? m.attachments.first().proxyURL : m.content}`).join('\n')
-                        fs.writeFile(`script.txt`, `${script}`, (err) => { 
-                            console.log(err)
+        if(!message.channel.name.includes('_ticket')) return message.channel.send(`💡 | You can **only** use this **command** in **ticket channel**!`)
+
+            const embed = new Discord.MessageEmbed()
+            .setAuthor(`${client.user.username} | Tickets`, client.user.avatarURL())
+            .setDescription(`> ❓ | Are you sure you want to close this ticket?\n> 💡 | React with emojis to **open/close** this ticket!`)
+            .setColor(message.guild.me.displayHexColor)
+            .setTimestamp()
+
+            const msg = await message.channel.send(`${message.author}`, embed)
+            await msg.react('✔️').catch((err) => { message.channel.send(err) })
+            await msg.react('❌').catch((err) => { message.channel.send(err) })
+    
+            const filter = (rect, usr) => ['✔️', '❌'].includes(rect.emoji.name) && usr.id === message.author.id
+            const response = await msg.awaitReactions(filter, {
+                max: 1,
+                time: 30000
+            });
+    
+            const emojis = response.first().emoji.name;
+    
+            if (emojis === '✔️') {
+
+                message.channel.send('🚧 | Im **closing** this **ticket**!').then(
+
+                    setTimeout(() => {
+
+                        message.channel.messages.fetch().then(async (messages) => {
+
+                            const script = messages.array().reverse().map(m => `${m.author.tag}: ${m.attachments.size > 0 ? m.attachments.first().proxyURL : m.content}`).join('\n')
+                            fs.writeFile(`script.txt`, script, (err) => { 
+                                console.log(err)
+                            })
                         })
-                    const Discord = require('discord.js') 
-                        const channel = client.channels.cache.get('848714572667682816') //OwO You need a channel ID I suggest making a new one :)
+                        
+                        message.channel.delete()
+
+                        const channel = client.channels.cache.get('848714572667682816')
                         const embed = new Discord.MessageEmbed()
-                        .setColor('RED')
-                        .setTitle(`A Ticket Has Been Closed`)
-                        .setDescription(`User: <@!${message.author.id}>\n\n Ticket Name: **${message.channel.name}**`)
-                        .setFooter(message.author.id)
+                        .setAuthor(`${client.user.username} | Tickets`, client.user.avatarURL())
+                        .setDescription(`> New ticket is closed!`)
+                        .addField(`🚧 | Info`, `> **Closed by:** \`${message.author.tag}\`\n> **Ticket Name:** \`${message.channel.name}\``)
+                        .setThumbnail('https://cdn.discordapp.com/emojis/860696559573663815.png?v=1')
+                        .setColor(message.guild.me.displayHexColor)
                         .setTimestamp()
                         channel.send({ embed, files: ["./script.txt"] })
-    
-            })
-                }, 5000))
-            } else if (m.content.toLowerCase() === "cancel") {
-                message.channel.send('Closing ticket. __**Canceled**__ Ticket staying open.');
-            }
-        });
-        collected1.on('end', collected => {
-            if (!collected) {
-                message.channel.send(`ERROR: User failed to provide an answer. Ticket staying open.`);
-            }
-        });
-    } else if (!message.channel.name.includes('-ticket')) {
-        message.channel.send('ERROR: You can only use this command in ticket channels.')
 
-    }
+                    }, 5000))
+    
+            }
+    
+            if (emojis === '❌') {
+
+                message.channel.send('🚧 | **Ticket** is staying **opened**!');
+    
+            }
 }
