3740d126f409891d8d1034eb80297162ff88afa7
mqrkelich <70708315+mqrkelich@users.noreply.github.com>
2021-07-18 18:05:56 +0200

    Remove Command ReWrite

--- bot/discord/commands/ticket/remove.js
+++ bot/discord/commands/ticket/remove.js
@@ -1,17 +1,16 @@
 exports.run = async (client, message, args) => {
-    if (!message.channel.name.includes('_ticket')) {
-        message.channel.send('This command is only to be used inside of ticket channels.')
-        return;
-    }
-    let member = message.mentions.members.first() || message.guild.members.cache.get(args[1]);
-    if(!member){
-        return message.channel.send('Please Mention someone or use their ID to remove the user from the ticket')
-    }
-    if(member.roles.cache.some(role => role.id === '748117822370086932')) return message.channel.send(`You can't remove staff From tickets`)
-        await message.channel.updateOverwrite(member, {
-            VIEW_CHANNEL: false,
-            SEND_MESSAGES: false,
-            READ_MESSAGE_HISTORY: false
-        })
-        message.channel.send(`Removed ${member} From the ticket`)
-    }
+    if (!message.channel.name.includes('_ticket')) return message.channel.send(`💡 | You can **only** use this **command** in **ticket channel**!`)
+
+    let user = message.mentions.users.first() || message.guild.members.cache.get(args[1])
+    if(!user) return message.channel.send(`💡 | You **need** to mention someone or enter a valid user's **ID** to remove someone from **this ticket**!`)
+
+    if(user.roles.cache.some(role => role.id === '748117822370086932')) return message.channel.send(`💡 | You **can't** remove **staff** from **tickets**`)
+
+    await message.channel.updateOverwrite(user, {
+        VIEW_CHANNEL: false,
+        SEND_MESSAGES: false,
+        READ_MESSAGE_HISTORY: false
+    })
+
+    message.channel.send(`💡 | Succesfully **removed** ${user} from this **ticket**!`)
+}
