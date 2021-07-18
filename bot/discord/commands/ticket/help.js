a114bca1664b5fa50f09c8c2925677bf9179085f
mqrkelich <70708315+mqrkelich@users.noreply.github.com>
2021-07-18 17:47:39 +0200

    New Ticket Help Command

--- bot/discord/commands/ticket/help.js
+++ bot/discord/commands/ticket/help.js
@@ -1,10 +1,11 @@
+const Discord = require('discord.js')
+
 exports.run = async (client, message, args) => {
     let embed = new Discord.MessageEmbed()
-        .setColor(`GREEN`)
-        .addField(`__**Tickets**__`, 'You can create a new ticket by typing: `' +
-            config.DiscordBot.Prefix + 'ticket new` \nYou can download your old tickets by running: `' +
-            config.DiscordBot.Prefix + 'ticket logs` \nYou can close your ticket by running: `' +
-            config.DiscordBot.Prefix + 'ticket close` \nYou can upgrade your ticket by running:`' +
-            config.DiscordBot.Prefix + 'ticket upgrade` \n\nAny problems? Please send a message in <#739231758087880845> and someone will help you.', true);
+    .setAuthor(`${client.user.username} | Tickets Help`, client.user.avatarURL())
+    .addField(`🎫 | Ticket Commands`, `> \`${config.prefix}ticket new\`\n> \`${config.prefix}ticket close\`\n> \`${config.prefix}ticket add\`\n> \`${config.prefix}ticket remove\`\n> \`${config.prefix}ticket upgrade\`\n> \`${config.prefix}ticket downgrade\``)
+    .setThumbnail('https://cdn.discordapp.com/emojis/860696559573663815.png?v=1')
+    .setColor(message.guild.me.displayHexColor)
+    .setTimestamp()
     await message.channel.send(embed)
 }
