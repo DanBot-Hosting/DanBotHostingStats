a820ae62a9eade69660ff9119db20226a9b17bdc
mqrkelich <70708315+mqrkelich@users.noreply.github.com>
2021-07-18 20:09:25 +0200

    Fixes

--- bot/discord/commands/ticket/help.js
+++ bot/discord/commands/ticket/help.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 exports.run = async (client, message, args) => {
     let embed = new Discord.MessageEmbed()
     .setAuthor(`${client.user.username} | Tickets Help`, client.user.avatarURL())
-    .addField(`🎫 | Ticket Commands`, `> \`${config.prefix}ticket new\`\n> \`${config.prefix}ticket close\`\n> \`${config.prefix}ticket add\`\n> \`${config.prefix}ticket remove\`\n> \`${config.prefix}ticket upgrade\`\n> \`${config.prefix}ticket downgrade\``)
+    .addField(`🎫 | Ticket Commands`, `> \`${config.DiscordBot.Prefix}ticket new\`\n> \`${config.DiscordBot.Prefix}ticket close\`\n> \`${config.DiscordBot.Prefix}ticket add\`\n> \`${config.DiscordBot.Prefix}ticket remove\`\n> \`${config.DiscordBot.Prefix}ticket upgrade\`\n> \`${config.DiscordBot.Prefix}ticket downgrade\``)
     .setThumbnail('https://cdn.discordapp.com/emojis/860696559573663815.png?v=1')
     .setColor(message.guild.me.displayHexColor)
     .setTimestamp()
