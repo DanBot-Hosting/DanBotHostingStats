637986b499ad8a165dde98cbb4914efd94b45130
mqrkelich <70708315+mqrkelich@users.noreply.github.com>
2021-07-21 11:22:31 +0200

    Tickets Fixing

--- bot/discord/commands/ticket/help.js
+++ bot/discord/commands/ticket/help.js
@@ -1,11 +1,12 @@
 const Discord = require('discord.js')
 
 exports.run = async (client, message, args) => {
-    let embed = new Discord.MessageEmbed()
-    .setAuthor(`${client.user.username} | Tickets Help`, client.user.avatarURL())
-    .addField(`🎫 | Ticket Commands`, `> \`${config.DiscordBot.Prefix}ticket new\`\n> \`${config.DiscordBot.Prefix}ticket close\`\n> \`${config.DiscordBot.Prefix}ticket add\`\n> \`${config.DiscordBot.Prefix}ticket remove\`\n> \`${config.DiscordBot.Prefix}ticket upgrade\`\n> \`${config.DiscordBot.Prefix}ticket downgrade\``)
-    .setThumbnail('https://cdn.discordapp.com/emojis/860696559573663815.png?v=1')
-    .setColor(message.guild.me.displayHexColor)
-    .setTimestamp()
-    await message.channel.send(embed)
+    
+        let embed = new Discord.MessageEmbed()
+        .setAuthor(`${client.user.username} | Tickets Help`, client.user.avatarURL())
+        .addField(`🎫 | Ticket Commands`, `> \`${config.DiscordBot.Prefix}ticket new\`\n> \`${config.DiscordBot.Prefix}ticket close\`\n> \`${config.DiscordBot.Prefix}ticket add\`\n> \`${config.DiscordBot.Prefix}ticket remove\`\n> \`${config.DiscordBot.Prefix}ticket upgrade\`\n> \`${config.DiscordBot.Prefix}ticket downgrade\``)
+        .setThumbnail('https://cdn.discordapp.com/emojis/860696559573663815.png?v=1')
+        .setColor(message.guild.me.displayHexColor)
+        .setTimestamp()
+        await message.channel.send({ embed: embed })
 }
