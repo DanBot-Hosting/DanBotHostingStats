0b190cf5eee28161e3377f288e78d931a28b0ce0
Hadimhz <solo1010.mahfouz@gmail.com>
2021-07-31 20:40:14 +0300

    Update transfer.js

--- bot/discord/commands/staff/transfer.js
+++ bot/discord/commands/staff/transfer.js
@@ -2,17 +2,26 @@ exports.run = async (client, message, args) => {
     if (!message.member.roles.cache.find(r => r.id === "748117822370086932")) return;
     let modlog = message.guild.channels.cache.find(channel => channel.id === config.DiscordBot.modLogs);
 
-    if (args[1] == null) {
+
+    if (!args.lenght < 3) {
         message.channel.send('usage: DBH!staff transfer <OLDUSERID> <NEWUSERID>.')
     } else {
-        if (userData.get(args[1]) == null) {
+        let old = userData.get(args[1])
+
+        if (old == null) {
             message.channel.send("That account is not linked with a console account :sad:")
         } else {
-
             if (!message.guild.members.cache.get(args[2])) {
                 message.channel.send("Couldn't find a user with the ID: " + args[2]);
                 return;
             }
+
+            let newData = userData.get(args[2]);
+
+            if (!newData || old.consoleID != newData.consoleID) {
+                message.reply('Both accounts should be linked to the same panel account in order for this command to work.')
+            }
+
 
             let { donated, used } = userPrem.get(args[1]);
             let newM = userPrem.get(args[2]) || { donated: 0, used: 0 };
