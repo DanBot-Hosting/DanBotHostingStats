ffe08993ee91e6b8c3dae908ea94c1ae1a458fd0
danielpmc <danielpd93@gmail.com>
2021-08-01 13:52:48 +0100

    revete

--- bot/discord/events/message.js
+++ bot/discord/events/message.js
@@ -71,7 +71,7 @@ module.exports = (client, message) => {
 
     if (message.author.bot) return; // to stop bots from creating accounts, tickets and more.
     if (message.channel.type === "dm") return; //stops commands working in dms
-    const prefix = config.DiscordBot.Prefix.toLowerCase();
+    const prefix = config.DiscordBot.Prefix;
     if (message.content.indexOf(prefix) !== 0) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const commandargs = message.content.split(' ').slice(1).join(' ');
