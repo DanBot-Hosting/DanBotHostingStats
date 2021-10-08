9fe25b262a50cefe3ccdfc680e0af8428c80f314
Sliden <57210425+Sliden101@users.noreply.github.com>
2021-10-08 20:26:14 +0700

    Update help.js

--- bot/discord/commands/server/help.js
+++ bot/discord/commands/server/help.js
@@ -3,9 +3,9 @@ exports.run = async (client, message, args) => {
         .addField('__**Commands**__', 'Create a server: `' +
             config.DiscordBot.Prefix + 'server create type servername` \nServer Types: `' +
             config.DiscordBot.Prefix + 'server create list` \nServer Status: `' +
-            config.DiscordBot.Prefix + 'server status serverid` \nLink Domain`' +
-            config.DiscordBot.Prefix + 'server proxy domainhere serveridhere ` \n Unlink domain: `' +
-            config.DiscordBot.Prefix + 'server unproxy domainhere` \n Delete server: `' +
+            config.DiscordBot.Prefix + 'server status serverid` \nLink Domain `' +
+            config.DiscordBot.Prefix + 'server proxy domainhere serveridhere ` \nUnlink domain: `' +
+            config.DiscordBot.Prefix + 'server unproxy domainhere` \nDelete server: `' +
             config.DiscordBot.Prefix + 'server delete serveridhere`')
     await message.channel.send(embed)
 }
