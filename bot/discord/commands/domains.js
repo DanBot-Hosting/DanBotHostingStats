84ab291aaf8496f1c898003f0ad4591778d9ee70
FC5570 <theamericantrucker20@gmail.com>
2021-12-04 14:00:26 +0530

    style: unlint code

--- bot/discord/commands/domains.js
+++ bot/discord/commands/domains.js
@@ -1,23 +1,13 @@
 const { MessageEmbed } = require('discord.js');
 
 exports.run = (client, message) => {
-	const data = userData.get(message.author.id);
-	if (!data) return message.channel.send('You do not have an account.');
-	if (!data.domains?.length)
-		return message.util.send('You do not have any domains.');
+  const data = userData.get(message.author.id);
+  if (!data) return message.channel.send('You do not have an account.');
+  if (!data.domains?.length) return message.util.send('You do not have any domains.');
 
-	const embed = new MessageEmbed()
-		.setTitle('Your Linked Domains')
-		.setDescription(
-			data.domains
-				.map(
-					(domain, i) =>
-						`${i + 1}) ${domain.domain} - Server ID ${
-							domain.serverID
-						}`
-				)
-				.join('\n')
-		)
-		.setColor('BLUE');
-	message.channel.send(embed);
+    const embed = new MessageEmbed()
+        .setTitle('Your Linked Domains')
+        .setDescription(data.domains.map((domain, i) => `${i + 1}) ${domain.domain} - Server ID ${domain.serverID}`).join('\n'))
+        .setColor('BLUE');
+    message.channel.send(embed);
 };
