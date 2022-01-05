6660d6213a0cb5b2ef108ac70f3e3019906d9d72
vinc3nz <63001507+vinc3nz@users.noreply.github.com>
2022-01-05 19:32:14 +0100

    Updated unproxy command for better help.

--- bot/discord/commands/server/unproxy.js
+++ bot/discord/commands/server/unproxy.js
@@ -20,15 +20,16 @@ async function getNewKey(){
 exports.run = async(client, message, args) => {
     if (!args[1]) {
         const embed = new Discord.MessageEmbed()
-            .setTitle('__**How to remove a domain from a server**__ \nCommand format: ' + config.DiscordBot.Prefix + 'server unproxy domainhere')
+            .setTitle('__**How to remove a domain from a server**__')
+            .setDescription('Command format: `' + config.DiscordBot.Prefix + 'server unproxy <domain>`\nReplace <domain> with your domain. You can find a list with all your proxied domains by running `' + config.DiscordBot.Prefix + 'domains`')
         message.channel.send(embed)
     } else if (args[1]) {
         if (args[2] == "-db") {
             userData.set(message.author.id + '.domains', userData.get(message.author.id).domains.filter(x => x.domain != args[1].toLowerCase()));
-            message.reply('Unlinked from the database')
+            message.reply('Unlinked domain `' + args[1] + '` from the database.')
         } else {
             if (userData.get(message.author.id).domains.find(x => x.domain === args[1].toLowerCase()) == null) {
-                message.channel.send("that domain isnt linked.")
+                message.channel.send("I could not find this domain! Please make sure you have entered a valid domain. A valid domain is `danbot.host`, `https://danbot.host/` is no valid domain!")
                 return;
             }
             config.proxy.authKey = await getNewKey();
@@ -56,7 +57,7 @@ exports.run = async(client, message, args) => {
                     }
                 }).then(response => {
                     userData.set(message.author.id + '.domains', userData.get(message.author.id).domains.filter(x => x.domain != args[1].toLowerCase()));
-                    message.reply('Domain has been unproxied')
+                    message.reply('Unproxied domain `' + args[1] + '`.')
                 })
             }).catch(error => {
                 message.reply('There has been a error. Please contact Dan or try once more, \nIf the bot says its currently linked try adding `-db` to the end of the command.')
