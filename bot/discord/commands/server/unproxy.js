20d1e79fcba8820e128b086ef2df0f2a0b5c5ff3
vinc3nz <63001507+vinc3nz@users.noreply.github.com>
2022-01-06 06:25:36 +0100

    No exploid

--- bot/discord/commands/server/unproxy.js
+++ bot/discord/commands/server/unproxy.js
@@ -26,7 +26,7 @@ exports.run = async(client, message, args) => {
     } else if (args[1]) {
         if (args[2] == "-db") {
             userData.set(message.author.id + '.domains', userData.get(message.author.id).domains.filter(x => x.domain != args[1].toLowerCase()));
-            message.reply('Unlinked domain `' + args[1] + '` from the database.')
+            message.reply('Unlinked domain from the database.')
         } else {
             if (userData.get(message.author.id).domains.find(x => x.domain === args[1].toLowerCase()) == null) {
                 message.channel.send("I could not find this domain! Please make sure you have entered a valid domain. A valid domain is `danbot.host`, `https://danbot.host/` is no valid domain!")
