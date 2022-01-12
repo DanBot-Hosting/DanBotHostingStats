bfb2c716be528b1bfb5a208f64378698c71d133f
danielpmc <danielpd93@gmail.com>
2022-01-12 04:19:23 +0000

    Update nodestatsChecker.js

--- nodestatsChecker.js
+++ nodestatsChecker.js
@@ -108,13 +108,6 @@ if (enabled.nodestatsChecker === true) {
                     })
                 }).catch(err => {})
             }, 800)
-            
-        tcpp.ping({ address: data.IP, port: 22}, function(err, data) {
-            nodePing.set(node, {
-                ip: data.address,
-                ping: data.avg
-            })
-        });
         }
 
         //Server limit
