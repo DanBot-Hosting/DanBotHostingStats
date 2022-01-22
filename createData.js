29d99b3c4bb5fa72836f44a8de68b22a7154f2c6
Freddie <freddiewren@gmail.com>
2022-01-22 16:31:02 +0000

    try again

--- createData.js
+++ createData.js
@@ -402,7 +402,7 @@ list.spigot = (serverName, userID) => ({
     "user": userID,
     "nest": 1,
     "egg": 58,
-    "docker_image": "quay.io/pterodactyl/core:java-11\n",
+    "docker_image": "ghcr.io/pterodactyl/yolks:java_17",
     "startup": 'java -Xms128M -Xmx{{SERVER_MEMORY}}M -jar {{SERVER_JARFILE}}',
     "limits": {
         "memory": 2048,
@@ -467,7 +467,7 @@ list.paper = (serverName, userID) => ({
     "user": userID,
     "nest": 1,
     "egg": 3,
-    "docker_image": "quay.io/pterodactyl/core:java-11",
+    "docker_image": "ghcr.io/pterodactyl/yolks:java_17",
     "startup": "java -Xms128M -Xmx{{SERVER_MEMORY}}M -Dterminal.jline=false -Dterminal.ansi=true -jar {{SERVER_JARFILE}}",
     "limits": {
         "memory": 2048,
