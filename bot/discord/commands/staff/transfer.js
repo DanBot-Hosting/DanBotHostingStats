c51bcc90d94ea5b2e56d6a58b3a938ba988d1c6f
Freddie <freddiewren@gmail.com>
2022-01-26 17:11:43 +0000

    Allow staff to transfer

--- bot/discord/commands/staff/transfer.js
+++ bot/discord/commands/staff/transfer.js
@@ -1,5 +1,5 @@
 exports.run = async(client, message, args) => {
-    if (!message.member.roles.cache.find(r => r.id === "898041747597295667")) return;
+    if (!message.member.roles.cache.find(r => r.id === "898041751099539497")) return;
     let modlog = message.guild.channels.cache.find(channel => channel.id === config.DiscordBot.modLogs);
 
 
