5bf7ccdc7018a867bb5c11aed9172f47cb779de2
FC <68158483+FC5570@users.noreply.github.com>
2022-01-27 23:40:50 +0530

    Update premium.js

--- bot/discord/commands/staff/premium.js
+++ bot/discord/commands/staff/premium.js
@@ -23,14 +23,14 @@ exports.run = async(client, message, args) => {
     if (args[1].toLowerCase() === 'add') {
         setDonations(userid, amount + oldBal);
         sendMessage(userid, amount)
-        message.member.roles.add('788193704014905364');
+        userid.roles.add('788193704014905364');
     }
 
 
     if (args[1].toLowerCase() === 'set') {
         setDonations(userid, amount);
         sendMessage(userid, amount)
-        message.member.roles.add('788193704014905364');
+        userid.roles.add('788193704014905364');
     }
 
     if (args[1].toLowerCase() === 'remove') {
