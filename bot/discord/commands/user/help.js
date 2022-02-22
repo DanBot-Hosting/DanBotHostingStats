4af263cb3683cf07e560cb908d8ac75aa0881b0f
RileCraft <rilecraft@gmail.com>
2022-02-22 15:24:53 +0530

    Add user invites to help page

--- bot/discord/commands/user/help.js
+++ bot/discord/commands/user/help.js
@@ -4,6 +4,7 @@ exports.run = async(client, message, args) => {
             config.DiscordBot.Prefix + "user password` | Reset account password \n`" +
             config.DiscordBot.Prefix + "user link` | Link this account with console account \n`" +
             config.DiscordBot.Prefix + "user unlink` | Unlinks account from the console account \n`" +
+            config.DiscordBot.Prefix + "user invites` | Shows the amount of people you have invited to this server \n`" +
             config.DiscordBot.Prefix + "user premium` | Check your premium server limit")
     await message.channel.send(embed)
 }
