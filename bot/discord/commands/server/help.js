d7b5ad44a9f1d613a8f623e2547a19c27947243c
DIBSTER <76603072+DIBSTERYT@users.noreply.github.com>
2022-02-24 21:51:42 -0500

    Update help.js

--- bot/discord/commands/server/help.js
+++ bot/discord/commands/server/help.js
@@ -1,4 +1,13 @@
 exports.run = async(client, message, args) => {
+        const AllowedChannels = [
+    898041850890440725, //Normal Commands.
+    898041851729305621, //Spam.
+    898041866589700128, //Donator/Booster Commands.
+    898041878447013948, //Beta Commands.
+    ];   
+
+    if(!AllowedChannels.some(c => c == message.channel.id)) return;  
+
     let embed = new Discord.MessageEmbed()
         .addField('__**Commands**__', 'Create a server: `' +
             config.DiscordBot.Prefix + 'server create <type> <servername>` \nCreate a premium server: `' +
