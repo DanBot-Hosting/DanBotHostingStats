26c169864a904ba0aa981ae82e0430c667b48fce
vinc3nz <63001507+vinc3nz@users.noreply.github.com>
2022-04-17 06:48:51 +0200

    Update help.js

--- bot/discord/commands/server/help.js
+++ bot/discord/commands/server/help.js
@@ -9,6 +9,7 @@ exports.run = async(client, message, args) => {
             config.DiscordBot.Prefix + 'server proxy <domainhere> <serveridhere> ` \n Unlink domain: `' +
             config.DiscordBot.Prefix + 'server unproxy <domainhere>` \n Delete server: `' +
             config.DiscordBot.Prefix + 'server delete <serveridhere>` \n List servers: `' +
-            config.DiscordBot.Prefix + 'server list`')
+            config.DiscordBot.Prefix + 'server list` \n See Servercount: `' +
+            config.DiscordBot.Prefix + 'server count`') 
     await message.channel.send(embed)
 }
