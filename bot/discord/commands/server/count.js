293acd136589eba6d03f9873190404ce720bc557
Alex <me@djoh.xyz>
2022-04-18 00:20:09 +0300

    Update count.js

--- bot/discord/commands/server/count.js
+++ bot/discord/commands/server/count.js
@@ -3,12 +3,10 @@ exports.run = async(client, message, args) => {
     message.channel.send('Loading servers...')
     //List servers
     var arr = [];
-    let userid = args[1] == null ? (message.author.id) : (args[1].match(/[0-9]{18}/).length == 0 ? args[1] : args[1].match(/[0-9]{18}/)[0]);
+    let userid = args[1].match(/[0-9]{18}/)?.[0] || message.author.id;
     let user = userPrem.fetch(userid);
-    if (user == null) user = {}
-    // let userID = message.author.id
-    if (message.member.roles.cache.find(r => r.id === "898041747597295667")) userID = args[1] || message.author.id; // Allow devs to lookup a users server list
-        
+    if (!user) user = {};
+
     axios({
         url: "https://panel.danbot.host" + "/api/application/users/" + userData.get(userID).consoleID + "?include=servers",
         method: 'GET',
@@ -27,14 +25,11 @@ exports.run = async(client, message, args) => {
             //console.log(arr.length)
             // console.log(arr)
             setTimeout(() => {
-                // var clean = arr.map(e => "Server Name: `" + e.attributes.name + "`, Server ID: `" + e.attributes.identifier + "`\n");
                 const embed = new Discord.MessageEmbed()
-                            //.addField('**Your Servers:**__', clean)
-			    .setDescription(`Server used: ${arr.length}`);
-                if(user.used) {
-			embed.addField(`Premium Servers`, `You currently have ${(user.used || 0)} Premium servers.`)
-			embed.addField(`Free Servers`, `You currently have ${(arr.length) - (user.used || 0)} Free servers.`)
-		}
+		    .addField(`Free Servers`, `You currently have ${arr.length - user.used} Free servers.`)
+                if (user.used) {
+                    embed.addField(`Premium Servers`, `You currently have ${user.used} Premium servers.`)
+                }
                 message.channel.send(embed)
                 //console.log(output)
             }, 500)
