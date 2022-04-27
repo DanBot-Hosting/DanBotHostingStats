d80063df5b167329271367935b7a7ad82004bd48
Alex <me@djoh.xyz>
2022-04-27 14:27:45 +0300

    Update list.js

--- bot/discord/commands/server/list.js
+++ bot/discord/commands/server/list.js
@@ -1,11 +1,10 @@
 const axios = require('axios')
 exports.run = async(client, message, args) => {
     message.channel.send('Loading servers...')
-    //List servers
     var arr = [];
     let userID = message.author.id
-    if (message.member.roles.cache.find(r => r.id === "898041747597295667")) userID = args[1] || message.author.id; // Allow devs to lookup a users server list
-        
+    if (message.member.roles.cache.find(r => r.id === "898041747597295667")) userID = args[1] || message.author.id;
+
     axios({
         url: "https://panel.danbot.host" + "/api/application/users/" + userData.get(userID).consoleID + "?include=servers",
         method: 'GET',
@@ -18,23 +17,17 @@ exports.run = async(client, message, args) => {
         }
     }).then(response => {
         const preoutput = response.data.attributes.relationships.servers.data
-        //console.log(resources.data.meta)
         arr.push(...preoutput)
-        setTimeout(async() => {
-            //console.log(arr.length)
-            // console.log(arr)
-            setTimeout(() => {
-                var clean = arr.map(e => "Server Name: `" + e.attributes.name + "`, Server ID: `" + e.attributes.identifier + "`\n");
+        setTimeout(() => {
+            var clean = arr.map(e => "Server Name: `" + e.attributes.name + "`, Server ID: `" + e.attributes.identifier + "`\n");
+            const embed = new Discord.MessageEmbed()
+                .addField('__**Your Servers:**__', clean)
+            message.channel.send(embed).catch(e => {
                 const embed = new Discord.MessageEmbed()
-                            .addField('__**Your Servers:**__', clean)
-                message.channel.send(embed).catch(e => {
-                    const embed = new Discord.MessageEmbed()
-                        .setDescription('Your server list is too long so here is a abstracted version!')
-                        .addField('__**Your Servers:**__', arr.map(e => "`" + e.attributes.identifier + "`"))
-                     message.channel.send(embed)
-                })
-                //console.log(output)
-            }, 500)
-        }, 5000)
+                    .setDescription('Your server list is too long so here is a abstracted version!')
+                    .addField('__**Your Servers:**__', arr.map(e => "`" + e.attributes.identifier + "`"))
+                message.channel.send(embed)
+            })
+        }, 500)
     }).catch(err => {});
 }
