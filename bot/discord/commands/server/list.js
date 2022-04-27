1eba8a3f0e6e5e6543472908d8ded302e858fdd4
Alex <me@djoh.xyz>
2022-04-27 14:53:19 +0300

    Update list.js

--- bot/discord/commands/server/list.js
+++ bot/discord/commands/server/list.js
@@ -1,8 +1,8 @@
-const axios = require('axios')
-exports.run = async(client, message, args) => {
-    message.channel.send('Loading servers...')
-    var arr = [];
-    let userID = message.author.id
+const axios = require('axios');
+exports.run = async (client, message, args) => {
+    message.channel.send('Loading servers...');
+    const arr = [];
+    let userID = message.author.id;
     if (message.member.roles.cache.has("898041747597295667")) userID = args[1] || message.author.id;
 
     axios({
@@ -16,18 +16,18 @@ exports.run = async(client, message, args) => {
             'Accept': 'Application/vnd.pterodactyl.v1+json',
         }
     }).then(response => {
-        const preoutput = response.data.attributes.relationships.servers.data
-        arr.push(...preoutput)
+        const preoutput = response.data.attributes.relationships.servers.data;
+        arr.push(...preoutput);
         setTimeout(() => {
-            const clean = arr.map(e => "Server Name: `" + e.attributes.name + "`, Server ID: `" + e.attributes.identifier + "`\n");
+            const clean = arr.map(e => `Server Name: \`${e.attributes.name}\`, Server ID: \`${e.attributes.identifier}\`\n`);
             const embed = new Discord.MessageEmbed()
-                .addField('__**Your Servers:**__', clean)
+                .addField('__**Your Servers:**__', clean);
             message.channel.send(embed).catch(e => {
                 const embed = new Discord.MessageEmbed()
-                    .setDescription('Your server list is too long so here is a abstracted version!')
-                    .addField('__**Your Servers:**__', arr.map(e => "`" + e.attributes.identifier + "`"))
-                message.channel.send(embed)
-            })
-        }, 500)
-    }).catch(err => {});
-}
+                    .setDescription('Your server list is too long so here is an abstracted version!')
+                    .addField('__**Your Servers:**__', arr.map(e => `\`${e.attributes.identifier}\``));
+                message.channel.send(embed);
+            });
+        }, 500);
+    }).catch(err => { });
+};
