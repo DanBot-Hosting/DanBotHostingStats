8f7237d67f50941643800007312abf77193df23e
Alex <me@djoh.xyz>
2022-04-27 14:35:01 +0300

    Update list.js

--- bot/discord/commands/server/list.js
+++ bot/discord/commands/server/list.js
@@ -3,7 +3,7 @@ exports.run = async(client, message, args) => {
     message.channel.send('Loading servers...')
     var arr = [];
     let userID = message.author.id
-    if (message.member.roles.cache.find(r => r.id === "898041747597295667")) userID = args[1] || message.author.id;
+    if (message.member.roles.cache.has("898041747597295667")) userID = args[1] || message.author.id;
 
     axios({
         url: "https://panel.danbot.host" + "/api/application/users/" + userData.get(userID).consoleID + "?include=servers",
@@ -19,7 +19,7 @@ exports.run = async(client, message, args) => {
         const preoutput = response.data.attributes.relationships.servers.data
         arr.push(...preoutput)
         setTimeout(() => {
-            var clean = arr.map(e => "Server Name: `" + e.attributes.name + "`, Server ID: `" + e.attributes.identifier + "`\n");
+            const clean = arr.map(e => "Server Name: `" + e.attributes.name + "`, Server ID: `" + e.attributes.identifier + "`\n");
             const embed = new Discord.MessageEmbed()
                 .addField('__**Your Servers:**__', clean)
             message.channel.send(embed).catch(e => {
