995e6d441dfa1688c905fab4257155797011ce90
yuansheng1549 <75909450+yuansheng1549@users.noreply.github.com>
2022-05-16 14:09:11 +0800

    Removed egg message

--- bot/discord/commands/mute.js
+++ bot/discord/commands/mute.js
@@ -63,8 +63,7 @@ exports.init = (client) => {
 exports.run = async(client, message, args) => {
     let modlog = message.guild.channels.cache.find(channel => channel.id === config.DiscordBot.modLogs);
 
-    if (message.member.roles.cache.find(r => r.id === config.DiscordBot.roles.staff) == null) return message.reply("sorry, but it looks like you're too much of an egg to run this command. 🥚");
-    if (!message.guild.me.hasPermission('MANAGE_ROLES')) return message.reply('Sorry, i dont have the perms to do this cmd i need MANAGE_ROLES. :x:');
+    if (message.member.roles.cache.find(r => r.id === config.DiscordBot.roles.staff) == null) return message.reply("sorry, but it looks like you're too much of a boomer to run this command.");
 
 
     if (args.length < 1) {
