1fe81a65e4edacc0301242efe85d2681b9ed8902
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2022-06-16 02:41:08 +0000

    Merge remote-tracking branch 'refs/remotes/origin/master'

--- bot/discord/commands/staff/settings.js
+++
@@ -1,8 +0,0 @@
-exports.run = async(client, message, args) => {
-    if (message.member.roles.cache.find(r => r.id === "898041743566594049")) {
-        let embed = new Discord.MessageEmbed()
-            .addField('__**Staff Applications Enabled?**__', webSettings.fetch("staff-applications.enabled"), true)
-            .addField('__**Website maintenance enabled?**__', webSettings.fetch("maintenance.enabled"), true)
-        await message.channel.send(embed)
-    }
-}
--- util/isSnowflake.js
+++
@@ -1,4 +0,0 @@
-module.exports = snowflake => {
-    return !isNaN(snowflake) && snowflake.length >= 16;
-};
-// Added to make life easier
