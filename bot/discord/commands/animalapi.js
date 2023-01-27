72a274d9156ee3612aee39e03bc0f3f1e4b2b541
Vincenz <63001507+vinc3nz@users.noreply.github.com>
2023-01-27 06:59:52 +0100

    Return error on Animalapi fail
    Will always fail

--- bot/discord/commands/animalapi.js
+++ bot/discord/commands/animalapi.js
@@ -8,14 +8,16 @@ exports.run = async(client, message, args) => {
                     .addField('Cat API:', response.body.cattotal + " images. \nhttps://api.danbot.host/cat", true)
                     .setFooter('You can get a image from the api using DBH!animalapi animalhere')
                 message.channel.send(embed)
+                if(err) message.channel.send('Error while fetching from API.\n**Currently the API is disabled.**');
             })
     } else if (args[0] === "dog") {
         axios.get('https://api.danbot.host/dog')
-            .then((response) => {
+            .then((err, response) => {
                 console.log(response)
                 message.channel.send("Random dog image from the DanBot Hosting api!", {
                     file: response.data.image
                 });
+                if(err) message.channel.send('Error while fetching from API.\n**Currently the API is disabled.**');
             });
     }
 };
