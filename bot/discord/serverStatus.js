adab597a19ac831ce09380e9dd2731879b3c8bac
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2023-05-20 23:26:43 -0400

    feat: changes to node status

--- bot/discord/serverStatus.js
+++ bot/discord/serverStatus.js
@@ -2,51 +2,63 @@ let nstatus = {
     "Nodes": [{
         name: 'Node 2 ',
         data: 'Node2',
-        location: 'UK'
+        location: 'UK',
+        maxCount: 1200
     }, {
         name: 'Node 3 ',
         data: 'Node3',
-        location: 'UK'
+        location: 'UK',
+        maxCount: 1200
     }, {
         name: 'Node 4 ',
         data: 'Node4',
-        location: 'UK'
+        location: 'UK',
+        maxCount: 1200
     }, {
         name: 'Node 5 ',
         data: 'Node5',
-        location: 'UK'
+        location: 'UK',
+        maxCount: 1200
     }, {
         name: 'Node 6 ',
         data: 'Node6',
-        location: 'UK'
+        location: 'UK',
+        maxCount: 1200
     }, {
         name: 'Node 7 ',
         data: 'Node7',
-        location: 'UK'
+        location: 'UK',
+        maxCount: 1200
     }, {
         name: 'Node 8',
         data: 'Node8',
-        location: 'UK'
+        location: 'UK',
+        maxCount: 600
     }],
 
      "Performance Nodes": [{
         name: 'PNode 1',
-        data: 'pnode1'
+        data: 'pnode1',
+        maxCount: 3000
     }],
     
     "Donator Nodes": [{
         name: 'Dono-01',
         data: 'dono01',
-        location: 'UK'
+        location: 'UK',
+        maxCount: 400
     }, {
         name: 'Dono-02',
-        data: 'dono02'
+        data: 'dono02',
+        maxCount: 602
     }, {
         name: 'Dono-03',
-        data: 'dono03'
+        data: 'dono03',
+        maxCount: 500
     }, {
         name: 'Dono-04',
-        data: 'dono04'
+        data: 'dono04',
+        maxCount: 500
     }],
 
     "VPS Hosting Servers": [{
@@ -76,7 +88,7 @@ let parse = async () => {
             let da = nodeStatus.get(d.data.toLowerCase());
             let nodeData = nodeServers.get(d.data.toLowerCase());
             let ping = nodePing.fetch(d.data.toLowerCase())
-            let serverUsage = d.data.toLowerCase().startsWith('node') ? `(${!nodeData?.servers ? 'N/A' : nodeData.servers} / 1200) [\`${Math.round(Number.isNaN(ping.ping) ? 0 : ping.ping)}ms\`]` : '' || d.data.toLowerCase().includes('dono') ? `(${!nodeData?.servers ? 'N/A' : nodeData.servers} / 500) [\`${Math.round(Number.isNaN(ping.ping) ? 0 : ping.ping)}ms\`]` : '' || d.data.toLowerCase().startsWith('pnode') ? `(${!nodeData?.servers ? 'N/A' : nodeData.servers} / 3000) [\`${Math.round(Number.isNaN(ping.ping) ? 0 : ping.ping)}ms\`]` : '' 
+            let serverUsage = d.data.toLowerCase().startsWith('node') ? `(${!nodeData?.servers ? 'N/A' : nodeData.servers} / ${nodeData.maxCount}) [\`${Math.round(Number.isNaN(ping.ping) ? 0 : ping.ping)}ms\`]` : '' || d.data.toLowerCase().includes('dono') ? `(${!nodeData?.servers ? 'N/A' : nodeData.servers} / ${nodeData.maxCount}) [\`${Math.round(Number.isNaN(ping.ping) ? 0 : ping.ping)}ms\`]` : '' || d.data.toLowerCase().startsWith('pnode') ? `(${!nodeData?.servers ? 'N/A' : nodeData.servers} / ${nodeData.maxCount}) [\`${Math.round(Number.isNaN(ping.ping) ? 0 : ping.ping)}ms\`]` : '' 
 
             da = (da.maintenance === true ? (`🟣 Maintenance Mode!`) : da.status === true ? (`🟢 Online ${serverUsage}`) : ((da.is_vm_online == null ? "🔴 **Offline**" : ((da.is_vm_online === true ? "🟠 Wings" : "🔴 **System**") + ` offline ${serverUsage}`))))
 
