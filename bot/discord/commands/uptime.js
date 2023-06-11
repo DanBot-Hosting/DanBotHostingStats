07e68a8962a0140a1ab286a19061b25908881f44
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2023-06-11 08:06:39 -0400

    feat: removing db ping

--- bot/discord/commands/uptime.js
+++ bot/discord/commands/uptime.js
@@ -5,7 +5,6 @@ exports.run = async(client, message, args) => {
     let myDate = new Date(client.readyTimestamp);
     const embed = new Discord.MessageEmbed()
         .addField(":white_check_mark: Uptime:", `**${humanizeDuration(client.uptime, {round: true})}**`, true)
-        .addField(":white_check_mark: Database Uptime:", `**${await dbUptime()}**`, true)
         .addField("Memory usage:", Math.trunc((process.memoryUsage().heapUsed) / 1024 / 1000) + "mb", true)
         .addField("Bot ping to discord:", client.ws.ping + "ms", true)
         .setFooter(`Ready Timestamp: ${myDate.toString()}`)
