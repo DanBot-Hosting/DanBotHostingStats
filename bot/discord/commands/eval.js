ce27fe01e32aa9b1b1e96c73c30f306783e4301e
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2023-06-10 20:01:49 -0400

    feat: updating eval

--- bot/discord/commands/eval.js
+++ bot/discord/commands/eval.js
@@ -4,7 +4,7 @@ exports.run = async(client, message) => {
     let args = message.content.split(' ').slice(1);
     let cont = message.content.split(' ').slice(1).join(' ');
 
-    if (!message.member.roles.cache.find(r => r.id === "898041741695926282")) {
+    if (!message.member.roles.cache.find(r => r.id === "898041741695926282" || !message.member.roles.cache.find(r => r.id === "1117240787881689250"))) {
 
         message.channel.send('Evaluating...').then(msg => {
             const responses = ["SyntaxError: Unexpected token F in JSON at position 48", "SyntaxError: Unexpected identifier", 'UnhandledPromiseRejectionWarning: DiscordAPIError: Missing Permissions', "TypeError: Cannot read property 'messages' of undefined", "UnhandledPromiseRejectionWarning: MongoError: bad auth : Authentication failed.", `TypeError: Cannot read property '${args.join(' ')}' of undefined`]
