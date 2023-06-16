64023279a5db1acd9239bd96d401d07dec2b1e86
danielpmc <danielpd93@gmail.com>
2023-06-16 03:49:44 +0100

    Who codes like this? wtf..

--- index.js
+++ index.js
@@ -109,63 +109,34 @@ client.login(config.DiscordBot.Token);
 global.Allowed = ["137624084572798976"];
 
 setInterval(async () => {
-
     users.length = 0
-
     servers.length = 0
-
     axios({
-
         url: "https://panel.danbot.host/api/application/users?per_page=9999999999999",
-
         method: 'GET',
-
         followRedirect: true,
-
         maxRedirects: 5,
-
         headers: {
-
             'Authorization': 'Bearer ' + config.Pterodactyl.apikey,
-
             'Content-Type': 'application/json',
-
             'Accept': 'Application/vnd.pterodactyl.v1+json',
-
         }
-
     }).then(resources => {
-
         users.push(...resources.data.data)
-
     }).catch(err => { console.log ('Error fetching users list') });;
-
     axios({
-
         url: "https://panel.danbot.host/api/application/servers?per_page=9999999999999",
-
         method: 'GET',
-
         followRedirect: true,
-
         maxRedirects: 5,
-
         headers: {
-
             'Authorization': 'Bearer ' + config.Pterodactyl.apikey,
-
             'Content-Type': 'application/json',
-
             'Accept': 'Application/vnd.pterodactyl.v1+json',
-
         }
-
     }).then(resources => {
-
         console.log(resources.data)
-
         servers.push(...resources.data.data)
-
     }).catch(err => { console.log ('Error fetching servers list') });;
 
 }, 60000)
