277430b8b299bb68f9ad0df2ab2b12054d70fbf3
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2023-06-20 12:21:33 -0400

    feat: updating guildMemberAdd

--- bot/discord/events/guildMemberAdd.js
+++ bot/discord/events/guildMemberAdd.js
@@ -16,25 +16,26 @@ module.exports = async(client, member, guild) => {
             await member.user.send(`Sorry! We only allow accounts over the age of 10days to join. \nYour account was created ${humanizeDuration(Date.now() - member.user.createdAt, {round: true})} ago.\n\nYou are welcome to join again once this account is over 10days old!`)
             await member.kick()
             welcomeChannel.send(member.user.tag + ` has been auto-kicked as account is under 10days old.\nThat account was created ${humanizeDuration(Date.now() - member.user.createdAt, {round: true})}, ago`)
-        }
-
-        if (userPrem.get(member.id) == null)
-            userPrem.set(member.id, {
-                used: 0,
-                donated: 0
-            })
-
-        member.roles.add(config.DiscordBot.roles.member)
-        if (userData.get(member.id) == null) {
-            welcomeChannel.send("Welcome <@" + member + "> to DanBot Hosting. To get started please read <#898041835002400768> and <#898041837535776788>");
         } else {
-            member.roles.add(config.DiscordBot.roles.client)
-            welcomeChannel.send("Welcome back <@" + member + "> to DanBot Hosting!");
-        }
+            member.roles.add(config.DiscordBot.roles.member)
 
-        if (mutesData.get(member.id) != null) {
-            member.roles.add(config.DiscordBot.roles.mute)
-        }
+            if (userPrem.get(member.id) == null){
+                userPrem.set(member.id, {
+                    used: 0,
+                    donated: 0
+                });
+            }
 
+            if (userData.get(member.id) == null) {
+                welcomeChannel.send("Welcome <@" + member + "> to DanBot Hosting. To get started please read <#898041835002400768> and <#898041837535776788>");
+            } else {
+                member.roles.add(config.DiscordBot.roles.client)
+                welcomeChannel.send("Welcome back <@" + member + "> to DanBot Hosting!");
+            }
+
+            if (mutesData.get(member.id) != null) {
+                member.roles.add(config.DiscordBot.roles.mute);
+            }
+        }
     }
 }
