f9a67aec601c0389b536054efd51aa48bad97fae
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2023-06-30 17:43:13 -0400

    feat: removing ragemp

--- createData_Prem.js
+++ createData_Prem.js
@@ -25,7 +25,6 @@ let data = (serverName, userID) => {
         forge: null,
         altv: null,
         multitheftauto: null,
-        ragemp: null,
         samp: null,
         bedrock: null,
         pocketminemp: null,
