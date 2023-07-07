5fee88588da4d8ceb85e0f2d79d7485469e0ce9d
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2023-07-06 22:00:38 -0400

    feat: Update uptimekuma.js

--- create-premium/uptimekuma.js
+++ create-premium/uptimekuma.js
@@ -4,7 +4,7 @@ createListPrem.uptimekuma = (serverName, userID) => ({
     "nest": 19,
     "egg": 70,
     "docker_image": "ghcr.io/parkervcp/yolks:nodejs_16",
-    "startup": `if [[ -d .git ]] && [[ {{AUTO_UPDATE}} == "1" ]]; then git pull; fi; if [[ ! -z ${NODE_PACKAGES} ]]; then /usr/local/bin/npm install ${NODE_PACKAGES}; fi; if [[ ! -z ${UNNODE_PACKAGES} ]]; then /usr/local/bin/npm uninstall ${UNNODE_PACKAGES}; fi; if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; /usr/local/bin/node /home/container/{{JS_FILE}} --port={{SERVER_PORT}}`,
+    "startup": "if [[ -d .git ]] && [[ {{AUTO_UPDATE}} == \"1\" ]]; then git pull; fi; if [[ ! -z ${NODE_PACKAGES} ]]; then /usr/local/bin/npm install ${NODE_PACKAGES}; fi; if [[ ! -z ${UNNODE_PACKAGES} ]]; then /usr/local/bin/npm uninstall ${UNNODE_PACKAGES}; fi; if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; /usr/local/bin/node /home/container/{{JS_FILE}} --port={{SERVER_PORT}}",
     "limits": {
         "memory": 0,
         "swap": -1,
