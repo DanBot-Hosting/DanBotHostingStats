8eecb0f22c9381e960efc64f9a2344bb1f57467b
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2023-07-08 19:37:03 -0400

    feat: debug

--- bot/discord/commands/server/create.js
+++ bot/discord/commands/server/create.js
@@ -81,7 +81,6 @@ exports.run = async(client, message, args) => {
 
 
         if (args[1].toLowerCase() === "aio" | args[1].toLowerCase() === "java") {
-            console.log(types);
             serverCreateSettings.createServer(types[args[1].toLowerCase()])
                 .then(response => {
                     let embed = new Discord.MessageEmbed()
@@ -117,7 +116,8 @@ exports.run = async(client, message, args) => {
                 }
             })
         } else {
-            console.log(types);
+            console.log(args[1].toLowerCase());
+            console.log(types[args[1].toLowerCase()]);
             serverCreateSettings.createServer(types[args[1].toLowerCase()])
                 .then(response => {
                     let embed = new Discord.MessageEmbed()
