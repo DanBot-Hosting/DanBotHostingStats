62b61986288975ba0e908794e62a966e4cf90cec
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2023-07-08 19:56:58 -0400

    feat: update create.js

--- bot/discord/commands/server/create.js
+++ bot/discord/commands/server/create.js
@@ -7,7 +7,7 @@ exports.run = async(client, message, args) => {
         .setColor("RED").setDescription(`List of servers: (use DBH!server create <type> <name>)\n\n*Please note that some nodes might be having trouble connecting to the bot which may lead into this process giving out an error.*\n`)
         //.addField("__**Minecraft:**__", "Forge \nPaper \nBedrock \nPocketmineMP \nWaterfall \nSpigot", true)
         //.addField("__**Grand Theft Auto:**__", "alt:V \nmultitheftauto \nRage.MP \nSA-MP", true)
-        .addField("__**Bots:**__", "NodeJS \nPython \nJava \naio \nRedDiscordBot", true)
+        .addField("__**Bots:**__", "NodeJS \nPython \nJava \naio \nRedDiscordBot\n Rust (use rustc to create)", true)
         //.addField("__**Source Engine:**__", "GMod \nCS:GO \nARK:SE", true)
         .addField("__**Voice Servers:**__", "TS3 \nMumble", true)
         //.addField("__**SteamCMD:**__", "Rust \nDaystodie \nArma \nAssettocorsa \nAvorion \nBarotrauma", true)
