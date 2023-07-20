0c8534e47a5c8d8d79f5648ac5e0c36ad3a1ac87
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2023-07-19 22:03:09 -0400

    feat: python egg update

--- create-premium/python.js
+++ create-premium/python.js
@@ -3,8 +3,8 @@ createListPrem.python = (serverName, userID) => ({
     "user": userID,
     "nest": 5,
     "egg": 22,
-    "docker_image": "quay.io/parkervcp/pterodactyl-images:debian_python-3.8",
-    "startup": "${STARTUP_CMD}",
+    "docker_image": "ghcr.io/parkervcp/yolks:python_3.9",
+    "startup": "if [[ -d .git ]] && [[ \"{{AUTO_UPDATE}}\" == \"1\" ]]; then git pull; fi; if [[ ! -z \"{{PY_PACKAGES}}\" ]]; then pip install -U --prefix .local {{PY_PACKAGES}}; fi; if [[ -f /home/container/${REQUIREMENTS_FILE} ]]; then pip install -U --prefix .local -r ${REQUIREMENTS_FILE}; fi; /usr/local/bin/python /home/container/{{PY_FILE}}",
     "limits": {
         "memory": 0,
         "swap": -1,
@@ -13,7 +13,10 @@ createListPrem.python = (serverName, userID) => ({
         "cpu": 0
     },
     "environment": {
-        "STARTUP_CMD": "bash"
+        "USER_UPLOAD": "0",
+        "AUTO_UPDATE": "0",
+        "REQUIREMENTS_FILE": "requirements.txt",
+        "PY_FILE": "main.py"
     },
     "feature_limits": {
         "databases": 2,
