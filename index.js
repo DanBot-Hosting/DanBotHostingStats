9d57cedf4433db74379821020edc9468e5ccf8e4
danielpmc <danielpd93@gmail.com>
2023-07-26 23:48:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -126,3 +126,20 @@ setInterval(async () => {
         users.push(...resources.data.data)
     }).catch(err => { console.log ('Error fetching users list') });
 }, 10 * 60 * 1000);
+
+process.on('unhandledRejection', (reason, p) => {
+		console.log(' [antiCrash] :: Unhandled Rejection/Catch');
+		console.log(reason, p);
+	});
+	process.on('uncaughtException', (err, origin) => {
+		console.log(' [antiCrash] :: Uncaught Exception/Catch');
+		console.log(err, origin);
+	});
+	process.on('uncaughtExceptionMonitor', (err, origin) => {
+		console.log(' [antiCrash] :: Uncaught Exception/Catch (MONITOR)');
+		console.log(err, origin);
+	});
+	process.on('multipleResolves', (type, promise, reason) => {
+		console.log(' [antiCrash] :: Multiple Resolves');
+		console.log(type, promise, reason);
+	});
