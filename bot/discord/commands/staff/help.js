c801a12113be72ff4f151c21720d9beba9ae27ee
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2023-08-06 18:58:20 -0400

    feat: updating staff help command

--- bot/discord/commands/staff/help.js
+++ bot/discord/commands/staff/help.js
@@ -1,9 +1,23 @@
 let subcommands = {
     mods: {
-        lockdown: ["Allows the channel to be locked or unlocked", '']
+        premiumfix: ["Fixes a user's premium count if it's incorrect.", ''],
+        proxyfix: ["Attmpts to delete a proxy from one of the servers.", ''],
+        transfer: ["Something idk how this command works.", '']
     },
     admin: {
-        linked: ["Shows if the users account is linked.", '<USERID>']
+        lockdown: ["Allows the channel to be locked or unlocked.", '']
+    },
+    botdev: {
+        linked: ["Shows if the users account is linked.", '<userid>'],
+        sudo: ["Send messages on behalf of another user.", '<userid>'],
+        maintenance: ["Set a Node into maintenance for Node Status.", '<nodeName>'],
+        update: ["Pulls files from GitHub manaully.", '']
+    },        
+    misc: {
+        premium: ["Set, add, or remove premium servers from a user.", '<set||add||remove> <user> <amount>'],
+        code: ["Create a code that is worth premium servers.", '<codename> <amount>'],
+        drop: ["Drops a code within the channel within a certain time.", '<time> <code>'],
+        help: ["Pulls up this help menu.", '']
     }
 }
 
@@ -20,7 +34,10 @@ exports.run = async(client, message, args) => {
     if (!message.member.roles.cache.find(r => r.id === "898041751099539497")) return;
 
     let embed = new Discord.MessageEmbed()
-        .setColor('RANDOM')
-        .addField('**Admin Commands:**', desc(subcommands.admin).join('\n'))
+        .setColor('BLUE')
+        .addField('**Moderator Commands:**', desc(subcommands.mods).join('\n'))
+        .addField('**Administrator Commands:**', desc(subcommands.admin).join('\n'))
+        .addField('**Bot Developer Commands:**', desc(subcommands.botdev).join('\n'))
+        .addField('**Misc Commands:**', desc(subcommands.misc).join('\n'))
     await message.channel.send(embed)
 }
