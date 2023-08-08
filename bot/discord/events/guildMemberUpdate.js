348e517653297f200aad9cbd7175c766fac5ae88
William Harrison <william@williamdavidharrison.com.au>
2023-08-08 09:35:22 +0800

    fix typo

--- bot/discord/events/guildMemberUpdate.js
+++ bot/discord/events/guildMemberUpdate.js
@@ -1,17 +1,13 @@
 module.exports = async(client, oldMember, newMember) => {
     // If user nickname changes.
     if (oldMember.displayName != newMember.displayName) {
-        let displayName = newMember.displayName.toLowerCase();
-
-        //if (displayName.includes("soloisaslut")) {
-        //    return newMember.setNickname(newMember.displayName.replace(/soloisaslut/i, "SoloIsAHottie"), "Naughty...");
-        //}
+        let displayName = newMember.displayName.toLowerCase()
 
         if (displayName.match(/^[a-z0-9]/i) == null) {
             return newMember.setNickname('I love Dan <3');
         }
 
-        if (['hilter', 'jew', 'discord.gg', 'discordapp', 'aids'].some(r => displayName.includes(r))) {
+        if (['hitler', 'jew', 'discord.gg', 'discordapp', 'aids'].some(r => displayName.includes(r))) {
             newMember.setNickname('Moderated Nickname');
         }
 
