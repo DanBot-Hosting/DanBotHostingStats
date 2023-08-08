05ecc6a04cdafefd43fa7782466636935db6f6d9
William Harrison <william@williamdavidharrison.com.au>
2023-08-08 09:37:58 +0800

    feat(ping): add bot latency

--- bot/discord/commands/ping.js
+++ bot/discord/commands/ping.js
@@ -1,12 +1,10 @@
 const Discord = require("discord.js");
 
 exports.run = async(client, message, args) => {
-
-    let embed = new Discord.MessageEmbed()
+    const embed = new Discord.MessageEmbed()
         .setColor("RED")
         .setTitle("DanBot Hosting - Ping")
-        .setDescription(`API Latency: ${Math.round(client.ws.ping)}ms`)
+        .setDescription(`Bot Latency: ${Date.now() - message.createdTimestamp}\nAPI Latency: ${Math.round(client.ws.ping)}ms`)
         .setTimestamp()
     message.channel.send(embed)
-
 };
