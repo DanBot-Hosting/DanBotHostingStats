e25da7f1488ecbdaa1bf6816ec68d386a92371f8
William Harrison <william@williamdavidharrison.com.au>
2023-08-12 07:15:50 +0800

    update links embed

--- bot/discord/commands/links.js
+++ bot/discord/commands/links.js
@@ -3,11 +3,9 @@ const Discord = require("discord.js");
 exports.run = async (client, message, args) => {
     const LinksEmbed = new Discord.MessageEmbed()
         .setColor("BLUE")
-        .setTitle(`DanBot Hosting Links:`)
         .addField("Website", "[danbot.host](https://danbot.host)", true)
         .addField("Panel", "[panel.danbot.host](https://panel.danbot.host)", true)
-        .addField("Portal", "[danbot.app](https://danbot.app)", true)
-        .setTimestamp();
+        .addField("Portal", "[danbot.app](https://danbot.app)", true);
 
     return message.reply(LinksEmbed);
 };
