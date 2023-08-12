463a23a9b9f35db21cf7a9a344a8fb8288b7a4ef
William Harrison <william@williamdavidharrison.com.au>
2023-08-12 08:38:48 +0800

    Update vpn-files.js

--- bot/discord/commands/vpn-files.js
+++ bot/discord/commands/vpn-files.js
@@ -2,12 +2,12 @@ const { MessageEmbed } = require("discord.js");
 
 exports.run = (client, message) => {
     if(!message.member.roles.cache.find((r) => r.id === "898041755419693126")) {
-        return message.reply("You do not have permission to execute this command!")
+        return message.reply("Only beta testers can use this command!");
     }
 
     const embed = new MessageEmbed()
         .setTitle("DBH VPN Files")
-        .setDescription("**URL**: https://cdn.wdh.gg/DBH-VPN/\n**Username**: `dbh`\n**Password**: `betatesters`\n\n**Important Links**:\n[Releases](https://cdn.wdh.gg/DBH-VPN/Releases/) **|** [OVPN Profiles](https://cdn.wdh.gg/DBH-VPN/OVPN-Profiles/)\n\n*Files hosted by [**William Harrison**](https://discord.com/users/853158265466257448).*")
+        .setDescription("**URL**: https://cdn.wdh.gg/DBH-VPN/\n**Username**: `dbh`\n**Password**: `betatesters`\n\n**Important Links**:\n[Releases](https://cdn.wdh.gg/DBH-VPN/Releases/) **|** [OVPN Profiles](https://cdn.wdh.gg/DBH-VPN/OVPN-Profiles/)\n\n*Files hosted by [**William**](https://discord.com/users/853158265466257448).*")
         .setFooter("Do not share these details with anyone else!")
         .setColor("BLUE");
 
