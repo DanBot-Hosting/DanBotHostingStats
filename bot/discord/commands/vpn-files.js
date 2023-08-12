baf9deafd5190563fbcfc4c3e15a99d18a63f213
William Harrison <william@williamdavidharrison.com.au>
2023-08-12 18:25:38 +0800

    Create vpn-files.js
    Link to a message instead of providing the login details in a public repository 🤦‍♂️

---
+++ bot/discord/commands/vpn-files.js
@@ -0,0 +1,7 @@
+exports.run = async (client, message, args) => {
+    if(!message.member.roles.cache.find((r) => r.id === "898041755419693126")) {
+        return message.reply("Only beta testers can use this command!");
+    }
+
+    return message.reply("CDN information can be found here: https://discord.com/channels/639477525927690240/898354771927400538/1138808051055476896");
+};
