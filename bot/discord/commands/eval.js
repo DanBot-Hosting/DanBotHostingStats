0b11421378207d14aa3e487a00b6f14eb78ca95e
William Harrison <william@williamdavidharrison.com.au>
2023-08-12 13:13:30 +0800

    Update eval.js

--- bot/discord/commands/eval.js
+++ bot/discord/commands/eval.js
@@ -113,6 +113,7 @@ exports.run = async (client, message) => {
                 "TypeError: Cannot read property 'messages' of undefined",
                 "UnhandledPromiseRejectionWarning: MongoError: bad auth : Authentication failed.",
                 `TypeError: Cannot read property '${args.join(" ")}' of undefined`,
+                "The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start."
             ];
             var cockandballtorturefromwikipediathefreeencylopedia =
                 responses[Math.floor(Math.random() * responses.length)];
