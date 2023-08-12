018eb8a725d92ab31be0e0a9ef308a4f363f550a
William Harrison <william@williamdavidharrison.com.au>
2023-08-12 13:17:24 +0800

    Update eval.js

--- bot/discord/commands/eval.js
+++ bot/discord/commands/eval.js
@@ -98,8 +98,9 @@ exports.run = async (client, message) => {
                     .setDescription(`**:inbox_tray: Input:**\n\n\`\`\`js\n${cont}\`\`\``, true)
                     .addField(`\u200b`, `**:outbox_tray: Output:**\`\`\`js\n${clean(err)}\`\`\``, true)
                     .setColor(0xff0000)
-                    .setFooter(`Node.js - Time taken: ${Date.now() - message.createdTimestamp} `);
+                    .setFooter(`Node.js - Time taken: ${Date.now() - message.createdTimestamp}ms`);
                 msg.edit({
+                    content: "",
                     embed: errorcode,
                 }).catch((e) => {});
             }
@@ -115,21 +116,22 @@ exports.run = async (client, message) => {
                 `TypeError: Cannot read property '${args.join(" ")}' of undefined`,
                 "The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start."
             ];
-            var cockandballtorturefromwikipediathefreeencylopedia =
+            var randomResponse =
                 responses[Math.floor(Math.random() * responses.length)];
             let errorcodefake = new Discord.MessageEmbed()
                 .setAuthor(`Eval by ${message.author.tag}`, `https://cdn.discordapp.com/emojis/314405560701419520.png`)
                 .setDescription(`**:inbox_tray: Input:**\n\n\`\`\`js\n${cont}\`\`\``, true)
                 .addField(
                     `\u200b`,
-                    `**:outbox_tray: Output:**\`\`\`js\n${cockandballtorturefromwikipediathefreeencylopedia} \nat /root/DBH/Panel/bot/discord/commands/eval.js:35:31 \nat runMicrotasks (<anonymous>) \nat processTicksAndRejections (internal/process/task_queues.js:93:5)\`\`\``,
+                    `**:outbox_tray: Output:**\`\`\`\n${randomResponse}\`\`\``,
                     true
                 )
                 .setColor(0xff0000)
-                .setFooter(`Node.js - Time taken: ${Date.now() - message.createdTimestamp} `);
+                .setFooter(`Node.js - Time taken: ${Date.now() - message.createdTimestamp}ms`);
             msg.edit({
+                content: "",
                 embed: errorcodefake,
-            });
+            }).catch((e) => {});
         });
     }
 };
