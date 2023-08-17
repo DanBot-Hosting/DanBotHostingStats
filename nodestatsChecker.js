6937905640886f60f21444b83eb21363a229c764
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2023-08-17 15:56:02 -0400

    fix: node status checker so it doesn't override staff maintenance command

--- nodestatsChecker.js
+++ nodestatsChecker.js
@@ -126,65 +126,26 @@ if (enabled.nodestatsChecker === true) {
                 })
                     .then((response) => {
                         //Node is online and is working. Just checking if it's in maintenance or not.
-
-                        if (nodeStatus.fetch(node + ".maintenance")) {
-                            nodeStatus.set(node, {
-                                timestamp: Date.now(),
-                                status: true,
-                                is_vm_online: true,
-                                maintenance: true,
-                            });
-                        } else {
-                            nodeStatus.set(node, {
-                                timestamp: Date.now(),
-                                status: true,
-                                is_vm_online: true,
-                                maintenance: false,
-                            });
-                        }
+                        
+                        nodeStatus.set(`${node.timestamp}`, Date.now());
+                        nodeStatus.set(`${node.status}`, true);
+                        nodeStatus.set(`${node.is_vm_online}`, true);
                     })
                     .catch((error) => {
                         //Node is either offline or wings are offline. Checks if it's maintenance, and then checks for wings.
 
-                        if (nodeStatus.fetch(node + ".maintenance")) {
                             ping2
                                 .ping(data.IP, 22)
                                 .then(() => {
-                                    nodeStatus.set(node, {
-                                        timestamp: Date.now(),
-                                        status: false,
-                                        is_vm_online: true,
-                                        maintenance: true,
-                                    });
+                                    nodeStatus.set(`${node.timestamp}`, Date.now());
+                                    nodeStatus.set(`${node.status}`, false);
+                                    nodeStatus.set(`${node.is_vm_online}`, true);
                                 })
                                 .catch((e) => {
-                                    nodeStatus.set(node, {
-                                        timestamp: Date.now(),
-                                        status: false,
-                                        is_vm_online: false,
-                                        maintenance: true,
-                                    });
+                                    nodeStatus.set(`${node.timestamp}`, Date.now());
+                                    nodeStatus.set(`${node.status}`, false);
+                                    nodeStatus.set(`${node.is_vm_online}`, false);
                                 });
-                        } else {
-                            ping2
-                                .ping(data.IP, 22)
-                                .then(() => {
-                                    nodeStatus.set(node, {
-                                        timestamp: Date.now(),
-                                        status: false,
-                                        is_vm_online: true,
-                                        maintenance: false,
-                                    });
-                                })
-                                .catch((e) => {
-                                    nodeStatus.set(node, {
-                                        timestamp: Date.now(),
-                                        status: false,
-                                        is_vm_online: false,
-                                        maintenance: false,
-                                    });
-                                });
-                        }
                     });
 
                 setTimeout(() => {
