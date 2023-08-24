a7410ee734e244df894c5913c3b3a4c6f80e62c6
William Harrison <william@williamdavidharrison.com.au>
2023-08-24 08:55:37 +0800

    send message

--- bot/discord/events/message.js
+++ bot/discord/events/message.js
@@ -1,3 +1,5 @@
+const cap = require("../util/cap");
+
 module.exports = async (client, message) => {
     if (message.mentions.users.size >= 20) {
         message.member.ban({ reason: "Suspected raid. Pinging more than 20 users." });
@@ -27,8 +29,9 @@ module.exports = async (client, message) => {
             const args = message.content.trim().split(/ +/g);
 
             try {
-                await client.channels.cache.get(args[0]).send(message.content.split(" ").slice(1).join(" "));
+                await client.channels.cache.get(args[0]).send(cap(message.content.split(" ").slice(1).join(" "), 2048));
                 // message.react("<:Yes:768256004655677451>");
+                message.channel.send(`<:Yes:768256004655677451> Message sent to <#${args[0]}>`);
             } catch(err) {
                 message.channel.send(`<:No:768256005426511912> An error occurred\n\`\`\`${err.message}\`\`\``);
             }
