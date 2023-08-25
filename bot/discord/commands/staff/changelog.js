260a495237b87eaafefc7f0268eedbfb409444ee
William Harrison <william@williamdavidharrison.com.au>
2023-08-25 16:30:24 +0800

    Update changelog.js

--- bot/discord/commands/staff/changelog.js
+++ bot/discord/commands/staff/changelog.js
@@ -1,20 +1,16 @@
-exports.init = (client, message, args) => {
+exports.run = (client, message, args) => {
     // Check if user has the staff role
     if (!message.member.roles.cache.find((r) => r.id === "898041751099539497")) return;
 
-    try {
-        if(!args[1]) return message.reply("Please provide a message!");
+    if(!args[1]) return message.reply("Please provide a message!");
 
-        const msg = message.content.split(" ").slice(1).join(" ");
+    const msg = message.content.split(" ").slice(1).join(" ");
 
-        const embed = new Discord.MessageEmbed()
-            .setColor("GREEN")
-            .setAuthor(message.author.tag.endsWith("#0") ? message.author.username : message.author.tag, message.author.displayAvatarURL({ format: "png", dynamic: true }), `https://discord.com/users/${message.author.id}`)
-            .setDescription(msg)
-            .setTimestamp();
-        client.channels.cache.get("898041904208429116").send(embed);
-        message.reply("Changelog sent!");
-    } catch(err) {
-        message.channel.send(`\`\`\`${err.message}\`\`\``);
-    }
+    const embed = new Discord.MessageEmbed()
+        .setColor("GREEN")
+        .setAuthor(message.author.tag.endsWith("#0") ? message.author.username : message.author.tag, message.author.displayAvatarURL({ format: "png", dynamic: true }), `https://discord.com/users/${message.author.id}`)
+        .setDescription(msg)
+        .setTimestamp();
+    client.channels.cache.get("898041904208429116").send(embed);
+    message.reply("Changelog sent!");
 };
