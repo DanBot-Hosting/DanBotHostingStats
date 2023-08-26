ba11d331a1d1e72e0b5f3f18e3be3e72b8e24c5e
William Harrison <william@williamdavidharrison.com.au>
2023-08-26 09:41:30 +0800

    Update list.js

--- bot/discord/commands/server/list.js
+++ bot/discord/commands/server/list.js
@@ -2,69 +2,65 @@ const axios = require("axios");
 const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
-    try {
-        let userID = message.author.id;
+    let userID = message.author.id;
 
-        // Allow developers to lookup a user's server list
-        if (message.member.roles.cache.find((r) => r.id === "898041747597295667") || message.author.id === "853158265466257448") userID = args[1] || message.author.id;
+    // Allow developers to lookup a user's server list
+    if (message.member.roles.cache.find((r) => r.id === "898041747597295667") || message.author.id === "853158265466257448") userID = args[1] || message.author.id;
 
-        const userAccount = userData.get(userID);
+    const userAccount = userData.get(userID);
 
-        if (userAccount == null || userAccount.consoleID == null) {
-            message.reply(
-                "You do not have an panel account linked to your Discord account.\n" +
-                "If you have not made an account yet please check out `" +
-                config.DiscordBot.Prefix +
-                "user new` to create an account \nIf you already have an account link it using `" +
-                config.DiscordBot.Prefix +
-                "user link`"
-            );
-            return;
-        }
-
-        // List servers
-        var arr = [];
-
-        axios({
-            url: `https://panel.danbot.host/api/application/users/${userAccount.consoleID}?include=servers`,
-            method: "GET",
-            followRedirect: true,
-            maxRedirects: 5,
-            headers: {
-                Authorization: `Bearer ${config.Pterodactyl.apikey}`,
-                "Content-Type": "application/json",
-                Accept: "Application/vnd.pterodactyl.v1+json",
-            },
-        }).then((response) => {
-            const preoutput = response.data.attributes.relationships.servers.data;
-            arr.push(...preoutput);
-
-            const donoNodes = [34, 31, 33, 35, 39];
-
-            const clean = arr.map((server) => {
-                const emoji = donoNodes.includes(server.attributes.node) ? ":money_with_wings:" : ":free:";
-
-                return `${emoji} **${server.attributes.name}** (\`${server.attributes.identifier}\`)`;
-            });
-
-            if (clean.length == 0) {
-                message.reply("You do not have any servers.");
-            } else if (clean.length > 70) {
-                message.reply("You have too many servers to display!");
-            } else if (clean.length > 20) {
-                const serverListEmbed = new Discord.MessageEmbed()
-                    .setTitle("Server List (shortened)")
-                    .setDescription(arr.map((i) => `\`${i.attributes.identifier}\``).join("\n"));
-                message.reply(serverListEmbed);
-            } else {
-                const serverListEmbed = new Discord.MessageEmbed()
-                    .setTitle("Server List")
-                    .setDescription(clean);
-                message.reply(serverListEmbed);
-            }
-        });
-    } catch (error) {
-        console.log(error);
-        message.channel.send(`\`\`\`${error.message}\`\`\``);
+    if (userAccount == null || userAccount.consoleID == null) {
+        message.reply(
+            "You do not have an panel account linked to your Discord account.\n" +
+            "If you have not made an account yet please check out `" +
+            config.DiscordBot.Prefix +
+            "user new` to create an account.\nIf you already have an account link it using `" +
+            config.DiscordBot.Prefix +
+            "user link`"
+        );
+        return;
     }
+
+    // List servers
+    var arr = [];
+
+    axios({
+        url: `https://panel.danbot.host/api/application/users/${userAccount.consoleID}?include=servers`,
+        method: "GET",
+        followRedirect: true,
+        maxRedirects: 5,
+        headers: {
+            Authorization: `Bearer ${config.Pterodactyl.apikey}`,
+            "Content-Type": "application/json",
+            Accept: "Application/vnd.pterodactyl.v1+json",
+        },
+    }).then((response) => {
+        const preoutput = response.data.attributes.relationships.servers.data;
+        arr.push(...preoutput);
+
+        const donoNodes = [34, 31, 33, 35, 39];
+
+        const clean = arr.map((server) => {
+            const emoji = donoNodes.includes(server.attributes.node) ? ":money_with_wings:" : ":free:";
+
+            return `${emoji} **${server.attributes.name}** (\`${server.attributes.identifier}\`)`;
+        });
+
+        if (clean.length == 0) {
+            message.reply("You do not have any servers.");
+        } else if (clean.length > 70) {
+            message.reply("You have too many servers to display!");
+        } else if (clean.length > 20) {
+            const serverListEmbed = new Discord.MessageEmbed()
+                .setTitle("Server List (shortened)")
+                .setDescription(arr.map((i) => `\`${i.attributes.identifier}\``).join("\n"));
+            message.reply(serverListEmbed);
+        } else {
+            const serverListEmbed = new Discord.MessageEmbed()
+                .setTitle("Server List")
+                .setDescription(clean);
+            message.reply(serverListEmbed);
+        }
+    })
+    .catch(() => message.reply("An error occurred while loading servers."));
 };
