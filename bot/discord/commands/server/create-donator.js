9f6c9ae241069144c4debb842822dd66d2524a53
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2023-08-26 11:22:18 -0400

    feat: re-enable for testing

--- bot/discord/commands/server/create-donator.js
+++ bot/discord/commands/server/create-donator.js
@@ -2,7 +2,9 @@ const serverCreateSettings_Prem = require("../../../../createData_Prem");
 const axios = require("axios");
 
 exports.run = async (client, message, args) => {
-    return message.reply("This command is temporarily disabled.");
+
+    //return message.reply("This command is temporarily disabled.");
+
     let userP = userPrem.fetch(message.author.id) || {
         used: 0,
         donated: 0,
