0442728f42c46ace1e51cff083c9246c94e51998
William Harrison <william@williamdavidharrison.com.au>
2023-08-28 10:11:59 +0800

    chore(staff premiumfix): fix undefined

--- bot/discord/commands/staff/premiumfix.js
+++ bot/discord/commands/staff/premiumfix.js
@@ -4,7 +4,7 @@ exports.run = async (client, message, args) => {
     if (!message.member.roles.cache.find((r) => r.id === "898041751099539497")) return;
 
     if (!args[1]) {
-        return message.reply("Please provide a user id!");
+        return message.reply("Please specify a user!");
     } else {
         const replyMsg = await message.reply("Starting calculation...");
 
@@ -14,6 +14,7 @@ exports.run = async (client, message, args) => {
             message.client.users.cache.get(
                 args[1].match(/\d{17,19}/).length == 0 ? args[1] : args[1].match(/\d{17,19}/)[0]
             );
+
         selectedUser = await selectedUser;
 
         const response = await axios({
@@ -35,6 +36,7 @@ exports.run = async (client, message, args) => {
         const preoutput = response.data.attributes.relationships.servers.data;
 
         let actualPremiumServersUsed = 0;
+
         for (let index = 0; index < preoutput.length; index++) {
             if (premiumNodes.includes(preoutput[index].attributes.node)) ++actualPremiumServersUsed;
         }
@@ -44,11 +46,12 @@ exports.run = async (client, message, args) => {
         const storedPremiumServersUsed = userPremData.used;
 
         console.log({ actualPremiumServersUsed, storedPremiumServersUsed, userPremData, selectedUser });
+
         if (actualPremiumServersUsed != storedPremiumServersUsed) {
             userPrem.set(selectedUser.id, { used: actualPremiumServersUsed, donated: userPremData.donated });
-            replyMsg.edit(`${selectedUser.tag}'s premium server count has been fixed!`);
+            replyMsg.edit(`${selectedUser.user.tag}'s premium server count has been fixed!`);
         } else {
-            replyMsg.edit(`${selectedUser.tag} has the correct premium server count!`);
+            replyMsg.edit(`${selectedUser.user.tag} has the correct premium server count!`);
         }
     }
 };
