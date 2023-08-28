3d649d351f0ff0b9fcc8b633de276f82664987c3
William Harrison <william@williamdavidharrison.com.au>
2023-08-28 09:24:53 +0800

    Update membercount.js

--- bot/discord/commands/info/membercount.js
+++ bot/discord/commands/info/membercount.js
@@ -1,14 +1,10 @@
 const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
-    try {
-        const embed = new Discord.MessageEmbed()
-            .setAuthor(message.guild.name, message.guild.iconURL({ format: "png", dynamic: true }))
-            .setTitle("Member Count")
-            .setDescription(`**Total**: ${message.guild.memberCount}\n**Humans**: ${message.guild.members.cache.filter(member => !member.user.bot).size}\n**Bots**: ${message.guild.members.cache.filter(member => member.user.bot).size}`)
-    
-        message.reply(embed);
-    } catch(err) {
-        message.reply(err.message);
-    }
+    const embed = new Discord.MessageEmbed()
+        .setAuthor(message.guild.name, message.guild.iconURL({ format: "png", dynamic: true }))
+        .setTitle("Member Count")
+        .setDescription(`:busts_in_silhouette: **Total**: ${message.guild.memberCount}\n:bust_in_silhouette: **Humans**: ${message.guild.members.cache.filter(member => !member.user.bot).size}\n:robot: **Bots**: ${message.guild.members.cache.filter(member => member.user.bot).size}`)
+
+    message.reply(embed);
 };
