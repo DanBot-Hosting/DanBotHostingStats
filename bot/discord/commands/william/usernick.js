29ca60bb9f15a6390d36e5ed8e95f6b8c740cbe6
William Harrison <william@williamdavidharrison.com.au>
2023-09-23 14:05:25 +0800

    Create usernick.js

---
+++ bot/discord/commands/william/usernick.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+exports.run = (client, message, args) => {
+    try {
+        // Check if user is William
+        if (message.author.id !== "853158265466257448") return;
+
+        if(!args[2]) return message.reply("Please provide a user ID!");
+        if(!args[3]) return message.reply("Please provide a message!");
+
+        const userId = args[2];
+        const newNick = message.content.split(" ").slice(3).join(" ");
+
+        message.guild.members.get(userId).setNickname(newNick);
+        message.reply(`User nickname set to \`${newNick}\``);
+    } catch(err) {
+        message.reply(err.message);
+    }
+};
