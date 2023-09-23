0b3cc9d70a33320d20f96b8ecb4d2bd659c11faf
William Harrison <william@williamdavidharrison.com.au>
2023-09-23 14:00:28 +0800

    Update botnick.js

--- bot/discord/commands/william/botnick.js
+++ bot/discord/commands/william/botnick.js
@@ -1,11 +1,16 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
-    // Check if user is William
-    if (message.author.id !== "853158265466257448") return;
+    try {
+        // Check if user is William
+        if (message.author.id !== "853158265466257448") return;
 
-    if(!args[2]) return message.reply("Please provide a message!");
+        if(!args[2]) return message.reply("Please provide a message!");
 
-    const newNick = message.content.split(" ").slice(2).join(" ");
+        const newNick = message.content.split(" ").slice(2).join(" ");
 
-    message.guild.members.get(client.user.id).setNickname(newNick)
+        message.guild.members.get(client.user.id).setNickname(newNick);
+        message.reply(`Bot nickname set to \`${newNick}\``);
+    } catch(err) {
+        message.reply(err.message);
+    }
 };
