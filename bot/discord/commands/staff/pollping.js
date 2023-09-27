d1c958eef2fb0c822c37856a8032dec516747ed6
William Harrison <william@williamdavidharrison.com.au>
2023-09-27 19:23:34 +0800

    feat: cooldown

--- bot/discord/commands/staff/pollping.js
+++ bot/discord/commands/staff/pollping.js
@@ -4,6 +4,9 @@ exports.run = (client, message, args) => {
     if (!message.member.roles.cache.find((r) => r.id === "898041747597295667")) return;
     // Check the message is in the #dev-questions channel
     if(message.channel.id !== "1083142107977486389") return message.reply("This command can only be used in <#1083142107977486389>!");
+    // Command can only be used every 30 minutes
+    if(pollPingLastUsed + 1800000 > Date.now()) return message.reply(`This command can only be used every 30 minutes! Cooldown expires <t:${(pollPingLastUsed + 1800000).toString().slice(0, -3)}:R>`)
 
     message.reply("<@&898041781927682090>");
+    global.pollPingLastUsed = Date.now();
 };
