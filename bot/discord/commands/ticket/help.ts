d374b15ae3023aac19afbe440dc67d33ae835ac9
William Harrison <william@williamdavidharrison.com.au>
2023-10-14 18:38:01 +0800

    Create help.ts

---
+++ bot/discord/commands/ticket/help.ts
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args) => {
+    let embed = new Discord.MessageEmbed()
+        .setAuthor(`${client.user.username} | Tickets Help`, client.user.avatarURL())
+        .addField(
+            `🎫 | Ticket Commands`,
+            `> ~~${config.DiscordBot.Prefix}ticket new~~ Use the \`/open\` command instead as this ticket system has been deprecated.\n> \`${config.DiscordBot.Prefix}ticket close\`\n> \`${config.DiscordBot.Prefix}ticket add\`\n> \`${config.DiscordBot.Prefix}ticket remove\`\n> \`${config.DiscordBot.Prefix}ticket upgrade\`\n> \`${config.DiscordBot.Prefix}ticket downgrade\``
+        )
+        .setThumbnail("https://cdn.discordapp.com/emojis/860696559573663815.png?v=1")
+        .setColor(message.guild.me.displayHexColor)
+        .setTimestamp();
+    await message.reply(embed);
+};
