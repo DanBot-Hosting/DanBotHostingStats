8668858b333852bed7b89756e655fe72e5a3843c
danielpmc <danielpd93@gmail.com>
2023-11-04 01:55:30 +0000

    Update nodestatsChecker.js

--- nodestatsChecker.js
+++ nodestatsChecker.js
@@ -9,43 +9,43 @@ let pingLocals = {
 let stats = {
     dono01: {
         serverID: "bd9d3ad6",
-        IP: "63.141.249.43",
+        IP: config.Nodes.Dono1,
         ID: "34",
         Location: pingLocals.CA,
     },
     dono02: {
         serverID: "ca6dba5a",
-        IP: "192.95.42.70",
+        IP: config.Nodes.Dono2,
         ID: "31",
         Location: pingLocals.CA,
     },
     dono03: {
         serverID: "c23f92cb",
-        IP: "51.68.194.254",
+        IP: config.Nodes.Dono3,
         ID: "33",
         Location: pingLocals.UK,
     },
     pnode1: {
         serverID: "1f6b4ee2",
-        IP: "51.68.201.44",
+        IP: config.Nodes.PNode1,
         ID: "38",
         Location: pingLocals.UK,
     },
     pnode2: {
         serverID: "2358ca8e",
-        IP: "51.89.140.125",
+        IP: config.Nodes.PNode2,
         ID: "40",
         Location: pingLocals.UK,
     },
     pnode3: {
         serverID: "e611fc3f",
-        IP: "51.89.140.124",
+        IP: config.Nodes.PNode3,
         ID: "40",
         Location: pingLocals.UK,
     },
     pnode4: {
         serverID: "e611fc3f",
-        IP: "51.89.140.126",
+        IP: config.Nodes.PNode4,
         ID: "40",
         Location: pingLocals.UK,
     },
@@ -66,7 +66,7 @@ if (enabled.nodestatsChecker) {
                 }).then((response) => {
                     let pingData = response.data.ping;
 
-                    if(isNaN(pingData)){
+                    if (isNaN(pingData)) {
                         pingData = "0";
                     };
 
@@ -79,19 +79,19 @@ if (enabled.nodestatsChecker) {
                 });
 
                 axios({
-                    url: config.Pterodactyl.hosturl + "/api/client/servers/" + data.serverID + "/resources",
-                    method: "GET",
-                    followRedirect: true,
-                    maxRedirects: 5,
-                    headers: {
-                        Authorization: "Bearer " + config.Pterodactyl.apikeyclient,
-                        "Content-Type": "application/json",
-                        Accept: "Application/vnd.pterodactyl.v1+json",
-                    },
-                })
+                        url: config.Pterodactyl.hosturl + "/api/client/servers/" + data.serverID + "/resources",
+                        method: "GET",
+                        followRedirect: true,
+                        maxRedirects: 5,
+                        headers: {
+                            Authorization: "Bearer " + config.Pterodactyl.apikeyclient,
+                            "Content-Type": "application/json",
+                            Accept: "Application/vnd.pterodactyl.v1+json",
+                        },
+                    })
                     .then((response) => {
                         //Node is online and is working. Just checking if it's in maintenance or not.
-                        
+
                         nodeStatus.set(`${node}.timestamp`, Date.now());
                         nodeStatus.set(`${node}.status`, true);
                         nodeStatus.set(`${node}.is_vm_online`, true);
@@ -99,24 +99,23 @@ if (enabled.nodestatsChecker) {
                     .catch((error) => {
                         //Node is either offline or wings are offline. Checks if it's maintenance, and then checks for wings.
 
-                            ping
-                                .ping(data.IP, 22)
-                                .then(() => {
-                                    nodeStatus.set(`${node}.timestamp`, Date.now());
-                                    nodeStatus.set(`${node}.status`, false);
-                                    nodeStatus.set(`${node}.is_vm_online`, true);
-                                })
-                                .catch((e) => {
-                                    nodeStatus.set(`${node}.timestamp`, Date.now());
-                                    nodeStatus.set(`${node}.status`, false);
-                                    nodeStatus.set(`${node}.is_vm_online`, false);
-                                });
+                        ping
+                            .ping(data.IP, 22)
+                            .then(() => {
+                                nodeStatus.set(`${node}.timestamp`, Date.now());
+                                nodeStatus.set(`${node}.status`, false);
+                                nodeStatus.set(`${node}.is_vm_online`, true);
+                            })
+                            .catch((e) => {
+                                nodeStatus.set(`${node}.timestamp`, Date.now());
+                                nodeStatus.set(`${node}.status`, false);
+                                nodeStatus.set(`${node}.is_vm_online`, false);
+                            });
                     });
 
                 setTimeout(() => {
                     axios({
-                        url:
-                            config.Pterodactyl.hosturl +
+                        url: config.Pterodactyl.hosturl +
                             "/api/application/nodes/" +
                             data.ID +
                             "/allocations?per_page=5000",
@@ -129,10 +128,10 @@ if (enabled.nodestatsChecker) {
                             Accept: "Application/vnd.pterodactyl.v1+json",
                         },
                     }).then((response) => {
-                            const servercount = response.data.data.filter((m) => m.attributes.assigned == true).length;
-                            nodeServers.set(node, {
-                                servers: servercount,
-                            });
+                        const servercount = response.data.data.filter((m) => m.attributes.assigned == true).length;
+                        nodeServers.set(node, {
+                            servers: servercount,
+                        });
                     }).catch((err) => {});
                 }, 2000);
             }, 2000);
@@ -140,7 +139,7 @@ if (enabled.nodestatsChecker) {
 
         // AU 1 VPN Server
         ping
-            .ping("139.99.171.195", 22)
+            .ping(config.VPN.AU - 1, 22)
             .then(() =>
                 nodeStatus.set("au1", {
                     timestamp: Date.now(),
@@ -156,7 +155,7 @@ if (enabled.nodestatsChecker) {
 
         // FR 1 VPN Server
         ping
-            .ping("176.31.125.135", 22)
+            .ping(config.VPN.FR - 1, 22)
             .then(() =>
                 nodeStatus.set("fr1", {
                     timestamp: Date.now(),
@@ -172,7 +171,7 @@ if (enabled.nodestatsChecker) {
 
         // US 1 VPN Server
         ping
-            .ping("69.197.129.206", 22)
+            .ping(config.VPN.US - 1, 22)
             .then(() =>
                 nodeStatus.set("us1", {
                     timestamp: Date.now(),
