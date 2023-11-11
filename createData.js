860bc8656034341a029b9b5c0978655546e308ad
danielpmc <danielpd93@gmail.com>
2023-11-11 08:49:52 +0000

    Update createData.js

--- createData.js
+++ createData.js
@@ -9,7 +9,7 @@ Free Hosting forever!                                            /____/
 const axios = require("axios");
 
 global.gamingFREE = [14]; // Gaming nodes
-global.botswebdbFREE = [32]; // Bots, Websites and Databases nodes
+global.botswebdbFREE = [30]; // Bots, Websites and Databases nodes
 global.storageFREE = [13]; // Storage nodes
 
 /*
