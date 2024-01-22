2af295dfb0e01483e39ecbc98c5630a05ad5a057
danielpmc <danielpd93@gmail.com>
2024-01-22 19:56:00 +0000

    Update serverStatus.js

--- bot/discord/serverStatus.js
+++ bot/discord/serverStatus.js
@@ -3,12 +3,17 @@ let nstatus = {
         {
             name: "PNode 1",
             data: "pnode1",
-            maxCount: 5000,
+            maxCount: 6000,
         },
         {
             name: "PNode 2",
             data: "pnode2",
             maxCount: 6000,
+        },
+        {
+            name: "PNode 3",
+            data: "pnode3",
+            maxCount: 5000,
         }
     ],
 
