6b800b0727d9059dfe91853b273019fd59e9f296
danielpmc <danielpd93@gmail.com>
2024-04-18 07:24:10 +0100

    Update storage.js

--- create-premium/storage.js
+++ create-premium/storage.js
@@ -1,19 +1,19 @@
-createListPrem.storage = (serverName, userID) => ({
+createList.storage = (serverName, userID) => ({
     name: serverName,
     user: userID,
-    nest: 5,
-    egg: 46,
-    docker_image: "danielpmc/discordnode8",
+    nest: 22,
+    egg: 92,
+    docker_image: "ghcr.io/parkervcp/yolks:alpine",
     startup: "${STARTUP_CMD}",
     limits: {
-        memory: 1,
+        memory: 0,
         swap: -1,
-        disk: 1048576,
+        disk: 0,
         io: 500,
-        cpu: 1,
+        cpu: 0,
     },
     environment: {
-        STARTUP_CMD: "STORAGE NODE",
+        STARTUP_CMD: `echo -e "\n\n\033[0;31mThere is no need to start this server. The SFTP service is always running. Feel free to stop the server now.\n\t\033[0;36m(Sub-users can be added via the Users tab to add/remove access to the share in real time)\n\n"`,
     },
     feature_limits: {
         databases: 0,
