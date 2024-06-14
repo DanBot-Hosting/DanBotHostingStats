8f6cce784047879f708babf41697a0bbb782e041
danielpmc <danielpd93@gmail.com>
2024-06-14 04:00:41 +0100

    Create influxdb.js

---
+++ create-premium/influxdb.js
@@ -0,0 +1,29 @@
+createListPrem.influxdb = (serverName, userID) => ({
+    name: serverName,
+    user: userID,
+    nest: 19,
+    egg: 66,
+    docker_image: "ghcr.io/parkervcp/yolks:debian",
+    startup: `./influxd  --http-bind-address 0.0.0.0:{{SERVER_PORT}}`,
+    limits: {
+        memory: 0,
+        swap: -1,
+        disk: 0,
+        io: 500,
+        cpu: 0,
+    },
+    environment: {
+        VERSION: "v2.4",
+    },
+    feature_limits: {
+        databases: 2,
+        allocations: 1,
+        backups: 10,
+    },
+    deploy: {
+        locations: botswebdbPREM,
+        dedicated_ip: false,
+        port_range: [],
+    },
+    start_on_completion: false,
+});
