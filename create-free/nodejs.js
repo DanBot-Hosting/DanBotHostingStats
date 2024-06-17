33697fac93d08995218e71d1843a2ffe03de692b
DieDeNueva <168224695+DieDeNueva@users.noreply.github.com>
2024-06-17 20:30:10 +0700

    Update nodejs.js

--- create-free/nodejs.js
+++ create-free/nodejs.js
@@ -1,16 +1,16 @@
 createList.nodejs = (serverName, userID) => ({
-    name: serverName,
-    user: userID,
+    name: Purpur,
+    user: nueva,
     nest: 5,
     egg: 50,
     docker_image: "ghcr.io/parkervcp/yolks:nodejs_20",
     startup: `/usr/local/bin/npm i && /usr/local/bin/node /home/container/{{BOT_JS_FILE}}`,
     limits: {
-        memory: 0,
+        memory: 3,
         swap: -1,
         disk: 10240,
         io: 500,
-        cpu: 100,
+        cpu: 200,
     },
     environment: {
         BOT_JS_FILE: "index.js",
