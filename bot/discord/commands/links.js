50db5135677b3157b3848432a83e2315d9f42881
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2024-07-25 12:15:49 -0400

    chore: adding two more links

--- bot/discord/commands/links.js
+++ bot/discord/commands/links.js
@@ -5,6 +5,8 @@ exports.run = async (client, message, args) => {
         .setColor("BLUE")
         .addField("Website", "[danbot.host](https://danbot.host)", true)
         .addField("Panel", "[panel.danbot.host](https://panel.danbot.host)", true)
+        .addField("Service Status", "[service.danbot.host](https://service.danbot.host)", true)
+        .addField("Uptime Site", "[uptime.danbot.host](https://uptime.danbot.host)", true)
         .addField("Portal", "[danbot.app](https://danbot.app)", true);
 
     return message.reply(LinksEmbed);
