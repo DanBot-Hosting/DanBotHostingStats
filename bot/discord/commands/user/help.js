518fa811bc1846ab9ac13a069bdbba80ae33bab6
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2024-07-26 00:34:12 -0400

    refractor: changing how user help commands look

--- bot/discord/commands/user/help.js
+++ bot/discord/commands/user/help.js
@@ -1,19 +1,24 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const Configs = require('../../../../config.json');
+
+/**
+ * User help command. Shows the user the commands under this category.
+ * 
+ * @param {Discord.Client} client 
+ * @param {Discord.Message} message 
+ * @param {Array} args 
+ * @returns void
+ */
 exports.run = async (client, message, args) => {
-    const embed = new Discord.MessageEmbed().addField(
-        "__**Commands**__",
-        "`" +
-            config.DiscordBot.Prefix +
-            "user new` | Create an account \n`" +
-            config.DiscordBot.Prefix +
-            "user password` | Reset account password \n`" +
-            config.DiscordBot.Prefix +
-            "user link` | Link this account with console account \n`" +
-            config.DiscordBot.Prefix +
-            "user unlink` | Unlinks account from the console account \n`" +
-            config.DiscordBot.Prefix +
-            "user invites` | Shows the amount of people you have invited to this server \n`" +
-            config.DiscordBot.Prefix +
-            "user premium` | Check your premium server limit"
-    );
-    await message.reply(embed);
+
+    const Embed = new Discord.MessageEmbed();
+    Embed.setColor("BLUE");
+    Embed.setTitle("User Commands");
+
+    fs.readdirSync(__dirname).forEach((File) => {
+        Embed.addField('`' + Configs.DiscordBot.Prefix + 'user ' + File.split('.')[0] + '`', require('./' + File).description);
+    });
+
+    await message.reply(Embed);
 };
