e14fce0547a0fabc2e3e9429fa050aceb393b93c
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2024-07-26 18:19:12 -0400

    refractor!: some QoL changes
    - Remove unused function (cap.js).
    - Create commonly used function (generatePassword.js).
    - Rewrote user premium to include raw donation amount, as well as a validation check.
    - Rewrote password reset, noticed that the mail server might not actually be sending the password reset via the mail.

---
+++ bot/discord/util/generatePassword.js
@@ -0,0 +1,13 @@
+module.exports = function () {
+
+    const CAPSNUM = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+    var password = "";
+    
+    // Creating a 16 character password using the CAPSNUM string.
+    while (password.length < 16) {
+        password += CAPSNUM[Math.floor(Math.random() * CAPSNUM.length)];
+    }
+
+    return password;
+}
