8f0212d5b6da93f5b402b958b9796d178592e193
nin <95270582+ninsaccplayzyt@users.noreply.github.com>
2024-07-27 14:32:50 +1000

    how dare dib forget the capital d in discord

--- bot/discord/commands/user/password.js
+++ bot/discord/commands/user/password.js
@@ -63,7 +63,7 @@ exports.run = async (client, message, args) => {
                 Embed.setColor("BLUE");
                 Embed.setTitle("Password Reset Success");
                 Embed.setDescription(
-                    "The console account that is linked with the discord account has now been reset.\n" +
+                    "The console account that is linked with the Discord account has now been reset.\n" +
                     "Please check direct messages for the password. If you didn't recieve a message, you do not have direct messages enabled for this server.\n\n" +
                     "An email has also been sent to your email connected to the console account."
                 );
