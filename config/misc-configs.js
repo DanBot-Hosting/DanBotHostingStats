20c5ad8cd4c40fef3c6b01aef98b78d755781da5
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2024-08-04 12:09:02 -0400

    refractor: progress towards removing all hardcoded IDs

---
+++ config/misc-configs.js
@@ -0,0 +1,54 @@
+const misc = {
+    // Users who are authorized to process donations.
+    staffPremium: [
+        "137624084572798976", //Dan
+        "737603315722092544", //Sarah
+        "459025800633647116", //Avix
+        "853158265466257448", //William
+        "757296951925538856"  //DIBSTER
+    ],
+
+    // Staff who can make code drops.
+    codeDrops: [
+        "137624084572798976", //Dan
+        "737603315722092544", //Sarah
+        "405771597761216522"  //Mike
+    ],
+
+    //Staff who can run eval and exec.
+    botCommands: [
+        "137624084572798976", //Dan
+        "757296951925538856", //DIBSTER
+        "853158265466257448"  //William
+    ],
+
+    // Channels where the bot reacts with up and down.
+    suggestionChannels: [
+        "980595293768802327", // Staff Suggestions
+        "976371313901965373", // VPN Suggestions
+    ],
+
+    // Staff that can DM the bot.
+    dmAllowedUsers: [
+        "137624084572798976", // Dan
+        "853158265466257448", // William
+        "757296951925538856", // DIBSTER
+    ],
+
+    // Channels where users can run the bot commands.
+    allowedChannels: [
+        "898041850890440725", // #commands - Community
+        "898041866589700128", // #commands - Donators
+        "898041878447013948", // #commands - Beta Testers
+        "1217536336181854258", // #commands - Staff
+        "898041906599178240", // #private - Staff
+    ],
+
+    // Categories where users can run the bot commands.
+    allowedCategories: [
+        "1160713638743658577", // High Priority Tickets
+        "1160713549685989406", // Medium Priority Tickets
+        "1160710296986460171", // Low Priority Tickets
+        "1160716485065445406", // Unknown Priority Tickets
+    ],
+}
