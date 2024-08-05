b455f7e2d67530cccc2bb3c7bcfad974beec6b9c
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2024-08-04 20:24:13 -0400

    chore: removing an old tag

--- src/commands/tag.js
+++ src/commands/tag.js
@@ -33,12 +33,6 @@ exports.run = async (client, message, args) => {
                 "`Error 504` means that the wings are currently down. This is nothing you can change.\nPlease be patient and wait for Dan to fix it.",
             );
             break;
-        case "api":
-        case "dbh-api":
-            embed.setDescription(
-                "You can find API-Wrappers and there documentation here:\n[Python](https://pypi.org/project/danbot-hosting/)\nSupport for more languages is comming soon.",
-            );
-            break;
 
         default:
             return message.reply(helpEmbed.setDescription("**I could not find this tag.**"));
