701946ffca66d046128cd66a610241aaf9166de3
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2024-08-14 18:07:50 -0400

    chore: changes to staff help

--- src/commands/staff/help.js
+++ src/commands/staff/help.js
@@ -2,47 +2,6 @@ const Discord = require('discord.js');
 
 const Config = require('../../../config.json');
 
-const subcommands = {
-    mods: {
-        premiumfix: [
-            "Fixes a user's premium count if it's incorrect.",
-            "<USER_ID || USER_MENTION>",
-        ],
-        proxyfix: ["Attmpts to delete a proxy from one of the servers.", "<DOMAIN_NAME>"],
-        transfer: ["Transfer data from one user account to another.", "<OLDUSERID> <NEWUSERID>"],
-    },
-    admin: {
-        lockdown: ["Allows the channel to be locked or unlocked.", ""],
-        linked: ["Check the details for a user account.", ""],
-    },
-    botdev: {
-        linked: ["Shows if the users account is linked.", "<userid>"],
-        maintenance: ["Set a Node into maintenance for Node Status.", "<nodeName>"],
-        update: ["Pulls files from GitHub manaully.", ""],
-        changelog: ["Announce new changes to the changelog channel.", "<MESSAGE>"],
-    },
-    misc: {
-        premium: [
-            "Set, add, or remove premium servers from a user.",
-            "<set||add||remove> <user> <amount>",
-        ],
-        code: ["Create a code that is worth premium servers.", "<codename> <amount>"],
-        drop: ["Drops a code within the channel within a certain time.", "<time> <code>"],
-        help: ["Pulls up this help menu.", ""],
-    },
-};
-
-let desc = (object) => {
-    let description = [];
-    let entries = Object.entries(object);
-    for (const [subCommand, [desc, usage]] of entries) {
-        description.push(
-            `**${subCommand}** - ${desc}\n (\`${config.DiscordBot.Prefix + "staff " + subCommand + " " + usage}\`)`,
-        );
-    }
-    return description;
-};
-
 exports.description = "Shows staff commands."
 
 /**
@@ -56,12 +15,13 @@ exports.run = async (client, message, args) => {
 
     if (!message.member.roles.cache.find((r) => r.id === Config.DiscordBot.Roles.Staff)) return;
 
-    const embed = new Discord.MessageEmbed()
-        .setColor("BLUE")
-        .addField("**Moderator Commands:**", desc(subcommands.mods).join("\n"))
-        .addField("**Administrator Commands:**", desc(subcommands.admin).join("\n"))
-        .addField("**Bot Developer Commands:**", desc(subcommands.botdev).join("\n"))
-        .addField("**Misc Commands:**", desc(subcommands.misc).join("\n"));
-        
-    await message.reply(embed);
+    const Embed = new Discord.MessageEmbed();
+    Embed.setColor("BLUE");
+    Embed.setTitle("Staff Commands");
+
+    fs.readdirSync(__dirname).forEach((File) => {
+        Embed.addField("`" + Config.DiscordBot.Prefix + "user " + File.split(".")[0] + "`", require("./" + File).description);
+    });
+
+    await message.reply(Embed);
 };
