0f73d91f51f58d0525652627984b33f533f0aac7
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2024-10-22 13:20:22 -0400

    chore: debug

--- test.js
+++ test.js
@@ -1,10 +1,5 @@
 (async () => {
     const userAccounts = await userData.all();
 
-    for (const user of userAccounts) {
-
-        const userAccount = await userData.get(user.discordID);
-
-        console.log(typeof userAccount);
-    }
+    console.log(userAccounts);
 })();
