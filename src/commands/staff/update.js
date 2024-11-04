7549f7d25d795a4246eafb224a7048ad379adc19
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2024-11-04 18:47:50 -0500

    fix: staff update

--- src/commands/staff/update.js
+++ src/commands/staff/update.js
@@ -21,28 +21,26 @@ exports.run = async (client, message, args) => {
     // Checks if the user has the Bot Administrator Role.
     if (!message.member.roles.cache.find((r) => r.id === Config.DiscordBot.Roles.BotAdmin)) return;
 
-    console.log("Updating the bot from GitHub.");
+    //Automatic GitHub Update (30 seconds intervals).
+    try {
+        const { stdout } = await execPromise('git pull');
+            
+        if (!stdout.includes("Already up to date.")) {
+            await client.channels.cache
+                .get(MiscConfigs.github)
+                .send(
+                    `<t:${Math.floor(Date.now() / 1000)}:f> Automatic update from GitHub, pulling files.\n\`\`\`${stdout}\`\`\``,
+                );
 
-        //Automatic GitHub Update (30 seconds intervals).
-        setInterval(async () => {
-            try {
-                const { stdout } = await execPromise('git pull');
-                    
-                if (!stdout.includes("Already up to date.")) {
-                    await client.channels.cache
-                        .get(MiscConfigs.github)
-                        .send(
-                            `<t:${Math.floor(Date.now() / 1000)}:f> Automatic update from GitHub, pulling files.\n\`\`\`${stdout}\`\`\``,
-                        );
+            await message.reply("Pulling files from GitHub.");
 
-                    await message.reply("Pulling files from GitHub.");
-
-                    setTimeout(() => {
-                        process.exit();
-                    }, 1000);
-                }
-            } catch (error) {
-                console.error(`Error with git pull: ${error.message}`);
-            }
-        }, 30000);
+            setTimeout(() => {
+                process.exit();
+            }, 1000);
+        } else {
+            await message.reply("Discord bot is already up to date.");
+        }
+    } catch (error) {
+        console.error(`[STAFF UPDATE] Error with git pull: ${error.message}`);
+    }
 };
