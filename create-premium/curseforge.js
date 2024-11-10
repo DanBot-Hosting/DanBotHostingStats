d733eee83f98271a9f5280f423cef8dce462b5b5
danielpmc <danielpd93@gmail.com>
2024-11-10 14:12:06 +0000

    Update curseforge.js

--- create-premium/curseforge.js
+++ create-premium/curseforge.js
@@ -20,7 +20,7 @@ createListPrem.curseforge = (serverName, userID) => ({
     },
     feature_limits: {
         databases: 2,
-        allocations: 1,
+        allocations: 5,
         backups: 10,
     },
     deploy: {
