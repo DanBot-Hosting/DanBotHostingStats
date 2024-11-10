3b14ac31c202d15056155160227101526e833257
danielpmc <danielpd93@gmail.com>
2024-11-10 14:13:19 +0000

    Update lavalink.js

--- create-premium/lavalink.js
+++ create-premium/lavalink.js
@@ -19,7 +19,7 @@ createListPrem.lavalink = (serverName, userID) => ({
     },
     feature_limits: {
         databases: 2,
-        allocations: 1,
+        allocations: 5,
         backups: 10,
     },
     deploy: {
