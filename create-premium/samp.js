b1cc8d533010e6b22fef5e32b20f55db40f9187c
danielpmc <danielpd93@gmail.com>
2024-11-10 14:16:01 +0000

    Update samp.js

--- create-premium/samp.js
+++ create-premium/samp.js
@@ -16,7 +16,7 @@ createListPrem.samp = (serverName, userID) => ({
     },
     feature_limits: {
         databases: 2,
-        allocations: 1,
+        allocations: 5,
         backups: 10,
     },
     environment: {
