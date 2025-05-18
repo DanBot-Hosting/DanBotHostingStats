cbb2cd36a9172dfea4b107935149561e47e7e7fc
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2025-05-18 15:30:02 -0400

    chore: updating proxy IP

--- config/proxy-configs.js
+++ config/proxy-configs.js
@@ -5,7 +5,7 @@ const Proxies = [
         name: "United States 1",            //The name to be displayed on embeds.
         dbLocation: "US1",                  //The location within the database.
         url: Config.USProxy1.url,           //The URL for requests.
-        ip: "69.197.135.202",               //The IP for the proxy.
+        ip: "69.30.226.123",               //The IP for the proxy.
         email: Config.USProxy1.email,       //The email for the Administrator Account.
         pass: Config.USProxy1.pass,         //The password for the Administrator Account.
         premiumOnly: false,                 //If the proxy is only available to premium users.
@@ -14,7 +14,7 @@ const Proxies = [
         name: "United States 2",
         dbLocation: "US2",
         url: Config.USProxy2.url,
-        ip: "69.197.135.203",
+        ip: "69.30.226.124",
         email: Config.USProxy2.email,
         pass: Config.USProxy2.pass,
         premiumOnly: false
@@ -23,7 +23,7 @@ const Proxies = [
         name: "United States 3",
         dbLocation: "US3",
         url: Config.USProxy3.url,
-        ip: "69.197.135.204",
+        ip: "69.30.226.125",
         email: Config.USProxy3.email,
         pass: Config.USProxy3.pass,
         premiumOnly: false
@@ -32,7 +32,7 @@ const Proxies = [
         name: "United States 4",
         dbLocation: "US4",
         url: Config.USProxy4.url,
-        ip: "69.197.135.205",
+        ip: "69.30.226.126",
         email: Config.USProxy4.email,
         pass: Config.USProxy4.pass,
         premiumOnly: false
