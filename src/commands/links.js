3920df1bd7b1503fce2a3f4adae94bb08a48506b
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2025-09-28 17:12:19 -0400

    chore: removing uptime site

--- src/commands/links.js
+++ src/commands/links.js
@@ -15,7 +15,6 @@ exports.run = async (client, message, args) => {
             {name: "Website", value: "[danbot.host](https://danbot.host)", inline: true},
             {name: "Panel", value: "[panel.danbot.host](https://panel.danbot.host)", inline: true},
             {name: "Service Status", value: "[service.danbot.host](https://service.danbot.host)", inline: true},
-            {name: "Uptime Site", value: "[uptime.danbot.host](https://uptime.danbot.host)", inline: true},
             {name: "Docs Site", value: "[docs.danbot.host](https://docs.danbot.host)", inline: true},
             {name: "Billing Site", value: "[billing.danbot.host](https://billing.danbot.host)", inline: true}
         )
