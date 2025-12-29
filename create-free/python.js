73f078c9aeb688a62c167baf7f74bf63edfa233f
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2025-12-29 12:56:31 -0500

    fix: python egg

--- create-free/python.js
+++ create-free/python.js
@@ -11,9 +11,9 @@ function createServer(ServerName, UserID){
         user: UserID,
         nest: 5,
         egg: 22,
-        docker_image: "ghcr.io/parkervcp/yolks:python_3.9",
+        docker_image: "ghcr.io/ptero-eggs/yolks:python_3.13",
         startup:
-            'if [[ ! -z "{{PY_PACKAGES}}" ]]; then pip install -U --prefix .local {{PY_PACKAGES}}; fi; if [[ -f /home/container/${REQUIREMENTS_FILE} ]]; then pip install -U --prefix .local -r ${REQUIREMENTS_FILE}; fi; ${STARTUP_CMD}',
+            'if [[ -d .git ]] && [[ "{{AUTO_UPDATE}}" == "1" ]]; then git pull; fi; if [[ ! -z "{{PY_PACKAGES}}" ]]; then pip install -U --prefix .local {{PY_PACKAGES}}; fi; if [[ -f /home/container/${REQUIREMENTS_FILE} ]]; then pip install -U --prefix .local -r ${REQUIREMENTS_FILE}; fi; /usr/local/bin/python /home/container/{{PY_FILE}}',
         limits: {
             memory: 1024,
             swap: -1,
@@ -23,7 +23,9 @@ function createServer(ServerName, UserID){
         },
         environment: {
             REQUIREMENTS_FILE: "requirements.txt",
-            STARTUP_CMD: "bash",
+            USER_UPLOAD: "1",
+            AUTO_UPDATE: "0",
+            PY_FILE: "main.py"
         },
         feature_limits: {
             databases: 2,
